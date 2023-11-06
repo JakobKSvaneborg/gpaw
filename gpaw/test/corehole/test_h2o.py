e9ebd0b5c6a9c0e174937587dfcd30497f81e9a8
Michael Walter <mcoywalter@gmail.com>
2023-11-06 07:35:59 +0100

    switch logic to require one core

--- gpaw/test/corehole/test_h2o.py
+++ gpaw/test/corehole/test_h2o.py
@@ -9,28 +9,30 @@ from gpaw.xas import XAS
 
 @pytest.mark.later
 def test_corehole_h2o(in_tmp_dir, add_cwd_to_setup_paths, gpw_files):
+    if mpi.size != 1:  # 1 core only for now
+        return
+
     # Generate setup for oxygen with half a core-hole:
     calc = GPAW(gpw_files['h2o_xas'])
-    if mpi.size == 1:
-        xas = XAS(calc)
-        x, y = xas.get_spectra()
-        e1_n = xas.eps_n
-        de1 = e1_n[1] - e1_n[0]
 
-    if mpi.size == 1:
-        # calc = GPAW('h2o-xas.gpw')
-        # poissonsolver=FDPoissonSolver(use_charge_center=True))
-        # calc.initialize()
-        xas = XAS(calc)
-        x, y = xas.get_spectra()
-        e2_n = xas.eps_n
-        w_n = np.sum(xas.sigma_cn.real**2, axis=0)
-        de2 = e2_n[1] - e2_n[0]
+    xas = XAS(calc)
+    x, y = xas.get_spectra()
+    e1_n = xas.eps_n
+    de1 = e1_n[1] - e1_n[0]
 
-        equal(de2, 2.064, 0.005)
-        equal(w_n[1] / w_n[0], 2.22, 0.01)
+    # calc = GPAW('h2o-xas.gpw')
+    # poissonsolver=FDPoissonSolver(use_charge_center=True))
+    # calc.initialize()
+    xas = XAS(calc)
+    x, y = xas.get_spectra()
+    e2_n = xas.eps_n
+    w_n = np.sum(xas.sigma_cn.real**2, axis=0)
+    de2 = e2_n[1] - e2_n[0]
 
-        assert de1 == de2
+    equal(de2, 2.064, 0.005)
+    equal(w_n[1] / w_n[0], 2.22, 0.01)
+
+    assert de1 == de2
 
     if 0:
         import matplotlib.pyplot as plt
