cb0ac5316c47b31b48a966e2978ff53aad6f5527
Dario A. Leon <dalv.cuba@gmail.com>
2024-06-17 17:02:51 +0200

    flake8, but the test passes with a factor 3 for omega = 0

--- gpaw/test/response/test_mpa_poly_C.py
+++ gpaw/test/response/test_mpa_poly_C.py
@@ -7,8 +7,8 @@ def mpa_py(omega, f, omegat_nGG, W_nGG, eta, factor):
     x1_nGG = f / (omega + omegat_nGG - 1j * eta)
     x2_nGG = (1.0 - f) / (omega - omegat_nGG + 1j * eta)
 
-    x_GG = (2 * factor) * np.sum(W_nGG * (x1_nGG + x2_nGG),
-                                 axis=0)  # Why 2 here
+    x_GG = 2 * factor * np.sum(W_nGG * (x1_nGG + x2_nGG),
+                               axis=0)  # Why 2 here
 
     eps = 0.0001 / Ha
     xp_nGG = f / (omega + eps + omegat_nGG - 1j * eta)
@@ -18,7 +18,7 @@ def mpa_py(omega, f, omegat_nGG, W_nGG, eta, factor):
     dx_GG = 2 * factor * np.sum(W_nGG * (xp_nGG - xm_nGG) / (2 * eps),
                                 axis=0)  # Why 2 here
 
-    return x_GG, dx_GG  # Why do we have to do a conjugate
+    return x_GG, dx_GG
 
 
 def test_residues(in_tmp_dir):
@@ -29,18 +29,14 @@ def test_residues(in_tmp_dir):
     npols = 10
     omegat_nGG = np.empty((npols, nG, nG), dtype=np.complex128)
     W_nGG = np.empty((npols, nG, nG), dtype=np.complex128)
-    omega = 0. #1.5
+    omega = 0.
 
     rng = np.random.default_rng(seed=1)
     omegat_nGG = rng.random((npols, nG, nG)) * 0.05 + 5.5 - 0.01j
     W_nGG[:] = rng.random((npols, nG, nG))
     W_nGG = np.ascontiguousarray(W_nGG)
 
-
-    x_GG_py = np.empty(omegat_nGG.shape[1:], dtype=complex)
-    dx_GG_py = np.empty(omegat_nGG.shape[1:], dtype=complex)
     x_GG_py, dx_GG_py = mpa_py(omega, f, omegat_nGG, W_nGG, eta, factor)
-
 
     x_GG_C = np.empty(omegat_nGG.shape[1:], dtype=complex)
     dx_GG_C = np.empty(omegat_nGG.shape[1:], dtype=complex)
@@ -48,8 +44,6 @@ def test_residues(in_tmp_dir):
 
     print(x_GG_py)
     print(x_GG_C)
-    print(x_GG_py/x_GG_C)
+    print(x_GG_py / x_GG_C)
 
-
-    assert np.allclose(x_GG_py/(3.), x_GG_C, atol=1e-6)
-
+    assert np.allclose(x_GG_py / (3.), x_GG_C, atol=1e-6)
