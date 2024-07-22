30b0906b812ed0fc210232eaefe4bcb1a22dcfbb
Jens Jørgen Mortensen <jjmo@dtu.dk>
2024-07-22 10:48:54 +0200

    Simplify test

--- gpaw/test/xc/test_lxc_laplacian.py
+++ gpaw/test/xc/test_lxc_laplacian.py
@@ -1,4 +1,4 @@
-"""check if an error is raised if the laplacian is needed (mgga)"""
+"""check if an error is raised if the laplacian is needed (mgga)."""
 import pytest
 from gpaw.xc import LibXC
 from gpaw.xc.libxc import FunctionalNeedsLaplacianError
@@ -8,24 +8,10 @@ from gpaw.xc.libxc import FunctionalNeedsLaplacianError
 @pytest.mark.libxc
 def test_mgga_lxc_laplacian():
     """Check for raised error."""
-    laplacian_test = False
-    try:
+    with pytest.raises(FunctionalNeedsLaplacianError):
         LibXC('MGGA_X_BR89+MGGA_C_TPSS')
-    except FunctionalNeedsLaplacianError:
-        laplacian_test = True
-    assert laplacian_test
 
 
 def test_mgga_lxc_suppressed_laplacian():
     """Check for suppressed error."""
-    laplacian_test = True
-    try:
-        LibXC('MGGA_X_BR89+MGGA_C_TPSS', provides_laplacian=True)
-    except FunctionalNeedsLaplacianError:
-        laplacian_test = False
-    assert laplacian_test
-
-
-if __name__ == '__main__':
-    test_mgga_lxc_laplacian()
-    test_mgga_lxc_suppressed_laplacian()
+    LibXC('MGGA_X_BR89+MGGA_C_TPSS', provides_laplacian=True)
