a7aa726a0efe51e8eeac77cb41978683e9049dab
Jens Jørgen Mortensen <jjmo@dtu.dk>
2024-07-26 09:32:20 +0200

    Finish up

--- gpaw/doctools/agts_crontab.py
+++ gpaw/doctools/agts_crontab.py
@@ -5,34 +5,7 @@ import re
 import numpy as np
 
 
-def send_email(tasks):
-    import smtplib
-    from email.message import EmailMessage
-
-    txt = 'Hi!\n\n'
-    for task in tasks:
-        if task.state in {'FAILED', 'CANCELED', 'TIMEOUT', 'MEMORY'}:
-            id, dir, name, res, age, status, t, err = task.words()
-            txt += ('test: {}/{}@{}: {}\ntime: {}\nerror: {}\n\n'
-                    .format(dir.split('agts/gpaw')[1],
-                            name,
-                            res[:-1],
-                            status,
-                            t,
-                            err))
-    txt += 'Best regards,\nNiflheim\n'
-
-    msg = EmailMessage()
-    msg.set_content(txt)
-    msg['Subject'] = 'Failing Niflheim-tests!'
-    msg['From'] = 'agts@niflheim.dtu.dk'
-    msg['To'] = 'jjmo@dtu.dk'
-    s = smtplib.SMTP('smtp.ait.dtu.dk')
-    s.send_message(msg)
-    s.quit()
-
-
-def find_created_files(root: Path = Path()):
+def find_created_files(root: Path = Path()) -> None:
     names = set()
     for path in root.glob('**/*.py'):
         if path.parts[0] == 'build':
@@ -51,7 +24,7 @@ def find_created_files(root: Path = Path()):
             yield path.with_name(name)
 
 
-def collect_files_for_web_page():
+def collect_files_for_web_page() -> None:
     os.chdir('gpaw/doc')
     folder = Path('agts-files')
     if not folder.is_dir():
@@ -62,7 +35,8 @@ def collect_files_for_web_page():
 
 
 def compare_all_files(references: Path,
-                      root: Path):
+                      root: Path) -> None:
+    errors = []
     for path in find_created_files(root):
         if not path.is_file():
             continue
@@ -70,49 +44,54 @@ def compare_all_files(references: Path,
         if not ref.is_file():
             print('MISSING', ref)
             continue
-        ok = compare_files(path, ref)
-        if not ok:
-            print(path, references / path.name)
+        err = compare_files(path, ref)
+        if err:
+            print(err, path)
+            errors.append((err, path, ref))
+    for err, path, ref in sorted(errors):
+        print(err, path, ref)
 
 
-def compare_files(p1, p2):
+def compare_files(p1: Path, p2: Path) -> float:
     if p1.suffix in {'.png', '.svg'}:
         return compare_images(p1, p2)
     if p1.suffix == '.db':
-        return True
+        return 0.0
     return compare_text(p1, p2)
 
 
-def compare_images(p1, p2):
+def compare_images(p1: Path, p2: Path) -> float:
     import PIL.Image as pil
     a1, a2 = (np.asarray(pil.open(p)) for p in [p1, p2])
     if a1.shape != a2.shape:
         print(a1.shape, a2.shape)
-        return False
+        return 10.0
     error = abs(a1 - a2).mean() / 255 / 3
-    if error > 1e-3:
+    if error > 0.012:
         print(error)
-        return False
-    return True
+        return error
+    return 0.0
 
 
-def compare_text(p1, p2):
+def compare_text(p1: Path, p2: Path) -> float:
     t1 = p1.read_text()
     t2 = p2.read_text()
     if t1 == t2:
-        return True
+        return 0.0
+
+    # (?s:...) means match also newlines
     for r in [r'(?s:User: .*OMP_NUM_THREADS)',
               r'(?s:Timing:.*)',
               r'Lattice=".*pbc="',
               r'Process memory now:.*',
               r'iter: .*',
-              r'20..-..-.. .*:..:..\.\d*',
+              r'20..-..-.. .*:..:..\.\d*',  # date
               r'Calculating spectrum .*',
               r'Spectrum calculated .*']:
         t1 = re.sub(r, '', t1)
         t2 = re.sub(r, '', t2)
     if t1 == t2:
-        return True
+        return 0.0
     lines1 = t1.splitlines()
     lines2 = t2.splitlines()
     sep = ',' if p1.suffix == '.csv' else None
@@ -123,6 +102,7 @@ def compare_text(p1, p2):
         atol = 1.5
     elif p1.name == 'TS.xyz':
         atol = 0.02
+
     for l1, l2 in zip(lines1, lines2):
         if l1 == l2:
             continue
@@ -130,21 +110,21 @@ def compare_text(p1, p2):
         words2 = l2.split(sep)
         if len(words1) != len(words2):
             print(l1, l2, words1, words2, sep)
-            return False
+            return 1.0
         for w1, w2 in zip(words1, words2):
             try:
                 f2 = float(w2)
             except ValueError:
                 if w1 != w2:
                     print(l1, l2, w1, w2)
-                    return False
+                    return 1.0
             else:
                 f1 = float(w1)
                 error = abs(f1 - f2)
                 if error > atol and error / abs(f2) > rtol:
                     print(l1, l2, f1, f2)
-                    return False
-    return True
+                    return 1.0
+    return 0.0
 
 
 if __name__ == '__main__':
