60c40e72a86f8fe0b5a5974bb86aed8d865f277a
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2024-08-07 08:30:57 +0200

    Adjust AGTS scripts

--- doc/documentation/xc/agts.py
+++ doc/documentation/xc/agts.py
@@ -18,7 +18,7 @@ def workflow():
     run(script='rsf_gamma.py', cores=8)
     run(script='ivo_hft.py')
     run(script='rsf_ivo_nacl.py')
-    run(script='rsf_lrtddft.py')
+    run(script='rsf_lrtddft.py', cores=24, tmax='20m')
     run(script='rsf_setup_poisson.py')
     run(script='rsf_simple.py')
 
