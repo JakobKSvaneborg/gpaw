ff0eda902a2c8dd20df05a6270bec375067dfa05
Jens Jørgen Mortensen <jj@smoerhul.dk>
2024-08-07 12:30:15 +0200

    Skip test for new GPAW

--- gpaw/test/lrtddft/test_rpa.py
+++ gpaw/test/lrtddft/test_rpa.py
@@ -1,6 +1,8 @@
+import pytest
 from gpaw.lrtddft import LrTDDFT
 
 
+@pytest.mark.lrtddft
 def test_rpa(H2, in_tmp_dir):
     lr = LrTDDFT(H2.calc, xc='RPA', restrict={'to': [1]})
     assert len(lr) == 1
