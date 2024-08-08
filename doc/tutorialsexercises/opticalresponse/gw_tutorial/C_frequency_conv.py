c58b5789720b9330f3e8dc46d54df1600190b8ff
Jens Jørgen Mortensen <jjmo@dtu.dk>
2024-08-08 15:54:29 +0200

    Clean up + fix y-axis in plot

--- doc/tutorialsexercises/opticalresponse/gw_tutorial/C_frequency_conv.py
+++ doc/tutorialsexercises/opticalresponse/gw_tutorial/C_frequency_conv.py
@@ -12,26 +12,25 @@ calc = GPAW(mode=PW(600),
             kpts={'size': (4, 4, 4), 'gamma': True},
             xc='LDA',
             occupations=FermiDirac(0.001),
-            txt='C_groundstate_freq2.txt')
+            txt='C_groundstate_freq.txt')
 
 atoms.calc = calc
 atoms.get_potential_energy()
 
 calc.diagonalize_full_hamiltonian()
-calc.write('C_groundstate_freq2.gpw', 'all')
+calc.write('C_groundstate_freq.gpw', 'all')
 
-for i, domega0 in enumerate([0.01, 0.02, 0.025, 0.03, 0.04, 0.05]):
+for i, domega0 in enumerate([0.0125, 0.025, 0.0375, 0.05]):
     for j, omega2 in enumerate([1, 5, 10, 15, 20, 25]):
         frequencies = {'type': 'nonlinear',
                        'domega0': domega0,
                        'omega2': omega2}
-        gw = G0W0(calc='C_groundstate_freq2.gpw',
-                  # nbands=30,
+        gw = G0W0(calc='C_groundstate_freq.gpw',
                   bands=(3, 5),
                   kpts=[0],
                   ecut=200,
                   integrate_gamma='WS',
                   frequencies=frequencies,
-                  filename=f'C_g0w0_domega0_{domega0}_omega2_{omega2}_2')
+                  filename=f'C_g0w0_domega0_{domega0}_omega2_{omega2}')
 
         results = gw.calculate()
