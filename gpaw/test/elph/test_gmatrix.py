61e59bbf8df8eb82a08bee5aecedbb3af19647df
Robert Warmbier <rwarmbier@uj.ac.za>
2024-08-12 19:55:43 +0200

    lint

--- gpaw/test/elph/test_gmatrix.py
+++ gpaw/test/elph/test_gmatrix.py
@@ -17,10 +17,10 @@ def test_gmatrix(module_tmp_path, supercell_cache):
     elph = ElectronPhononMatrix(atoms, 'supercell', 'elph')
     q = [[0, 0, 0], [0.5, 0.5, 0.5]]
 
-    phonon = Phonons(atoms, name='elph', supercell=(2,1,1),
-                     center_refcell=True)
-    phonon.read()
-    w_ql, u_ql = phonon.band_structure(q, modes=True)
+    # phonon = Phonons(atoms, name='elph', supercell=(2, 1, 1),
+    #                  center_refcell=True)
+    # phonon.read()
+    # w_ql, u_ql = phonon.band_structure(q, modes=True)
     # print(w_ql)
     # print(u_ql)
     # [[0.00124087  0.00044091  0.00132004  0.0421112   0.04212737  0.04218485]
@@ -61,7 +61,6 @@ def test_gmatrix(module_tmp_path, supercell_cache):
     print(g_abs)
     assert g_abs == pytest.approx(0.023668, rel=1e-4)
 
-
     # q = 1
     print("q1 checks")
     g_knn = np.sum(g_sqklnn[0, 1, :, 4:6], axis=1)  # modes 5-6
@@ -75,4 +74,3 @@ def test_gmatrix(module_tmp_path, supercell_cache):
     g_abs = np.sum(np.abs(g_sqklnn[0, 1, 0, 4:6, 0, 1])**2)
     print(g_abs)
     assert g_abs == pytest.approx(0.29253, rel=1e-4)
-
