01e864eb1c4d3fa95f77c5c4690385995b1136e1
Jens Jørgen Mortensen <jjmo@dtu.dk>
2024-08-29 12:31:15 +0200

    Units

--- gpaw/wannier/overlaps.py
+++ gpaw/wannier/overlaps.py
@@ -3,6 +3,7 @@ from typing import Dict, List, Tuple, Union
 
 import numpy as np
 from ase import Atoms
+from ase.units import Bohr
 
 from gpaw.new.ase_interface import ASECalculator as GPAW
 from gpaw.kpt_descriptor import KPointDescriptor
@@ -281,15 +282,18 @@ class BZRealSpaceWaveFunctions:
         bzwfs = {}
         for ibz_index in range(kd.nibzkpts):
             for n in range(n1, n2):
-                # u_nR[n - n1] = wfs.get_wave_function_array(n=n,
-                #                                            k=ibz_index,
-                #                                            s=spin,
-                #                                            periodic=True)
-                u_nR[n - n1] = calc.get_pseudo_wave_function(band=n,
-                                                             kpt=ibz_index,
-                                                             spin=spin,
-                                                             periodic=True,
-                                                             pad=False)
+                if 0:
+                    u_nR[n - n1] = wfs.get_wave_function_array(n=n,
+                                                               k=ibz_index,
+                                                               s=spin,
+                                                               periodic=True)
+                else:
+                    u_nR[n - n1] = calc.get_pseudo_wave_function(
+                        band=n,
+                        kpt=ibz_index,
+                        spin=spin,
+                        periodic=True,
+                        pad=False) * Bohr**1.5
             P_nI = wfs.collect_projections(ibz_index, spin)
             if P_nI is not None:
                 P_nI = P_nI[n1:n2]
