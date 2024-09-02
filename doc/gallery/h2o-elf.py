33d0b7fa712968937f29b30eff5011ec8e332893
Jens Jørgen Mortensen <jjmo@dtu.dk>
2024-09-02 11:12:37 +0200

    Update plot

--- doc/gallery/h2o-elf.py
+++ doc/gallery/h2o-elf.py
@@ -1,3 +1,4 @@
+# creates: h2o-elf.png
 import plotly.graph_objects as go
 from ase.build import molecule
 from gpaw.new.ase_interface import GPAW
@@ -9,8 +10,13 @@ h2o.calc = GPAW(mode={'name': 'pw', 'ecut': 1000},
                 txt='h2o.txt')
 h2o.get_potential_energy()
 elf_R = elf_from_dft_calculation(h2o.calc.dft, ncut=0.001)
+
+# Plot:
 surf = elf_R.isosurface(show=False, isomin=0.8, isomax=0.8)
 fig = go.Figure(data=[surf])
+fig.update_traces(showscale=False)
 fig.update_layout(
-    scene=dict(xaxis_visible=False, yaxis_visible=False, zaxis_visible=False))
+    scene=dict(xaxis_visible=False, yaxis_visible=False, zaxis_visible=False,
+               camera=dict(up=dict(x=0.8, y=0.4, z=0.8),
+                           eye=dict(x=0.5, y=0.3, z=0.1))))
 fig.write_image('h2o-elf.png')
