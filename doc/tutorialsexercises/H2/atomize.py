f7d9ceef70425d861f8577431682ace577538117
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2024-09-10 14:51:59 +0200

    H+H2 fixes

--- doc/tutorialsexercises/H2/atomize.py
+++ doc/tutorialsexercises/H2/atomize.py
@@ -11,29 +11,28 @@ c = a / 2
 atom = Atoms('H',
              positions=[(c, c, c)],
              magmoms=[0],
-             cell=(a, a + 0.0001, a + 0.0002))  # Break cell symmetry
+             cell=(a, a + 0.0001, a + 0.0002))  # break cell symmetry
 
 # gpaw calculator:
 calc = GPAW(mode=PW(),
             xc='PBE',
             hund=True,
-            eigensolver='rmm-diis',  # This solver can parallelize over bands
-            occupations=FermiDirac(0.0, fixmagmom=True),
-            txt='H.out',
-            )
+            # eigensolver='rmm-diis',  # This solver can parallelize over bands
+            # occupations=FermiDirac(0.0, fixmagmom=True),
+            txt='H.out')
 atom.calc = calc
 
 e1 = atom.get_potential_energy()
 calc.write('H.gpw')
 
 # Hydrogen molecule:
-d = 0.74  # Experimental bond length
+d = 0.74  # experimental bond length
 molecule = Atoms('H2',
                  positions=([c - d / 2, c, c],
                             [c + d / 2, c, c]),
                  cell=(a, a, a))
 
-calc = calc.new(hund=False,  # No hund rule for molecules
+calc = calc.new(hund=False,  # no hund rule for molecules
                 txt='H2.out')
 
 molecule.calc = calc
