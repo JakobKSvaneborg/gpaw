cb07266c9366b9d6485aba3b3f80a1d5379031b5
Jens Jørgen Mortensen <jjmo@dtu.dk>
2024-09-13 13:12:12 +0200

    ...

--- gpaw/new/pw/paw_poisson.py
+++ gpaw/new/pw/paw_poisson.py
@@ -2,6 +2,7 @@ from math import pi
 
 import numpy as np
 from scipy.special import erf
+from ase.neighborlist import primitive_neighbor_list
 from gpaw.core import PWDesc, PWArray
 from gpaw.core.atom_arrays import AtomDistribution, AtomArrays
 from gpaw.gpu import cupy as cp
@@ -37,53 +38,42 @@ class PAWPoissonSolver:
                  poisson_solver,
                  fracpos_ac: np.ndarray,
                  atomdist: AtomDistribution,
-                 xp=np,
-                 simple=False):
+                 xp=np):
         self.xp = xp
         self.pwg = pwg
         self.pwg0 = pwg.new(comm=None)  # not distributed
         self.poisson_solver = poisson_solver
         cutoff_a = np.asarray(cutoff_a)
-
         rcut = cutoff_a.max() * 2
         d = 0.01
         rgd = RGD(d, int(rcut * 5 / d))
-
-        if simple:
-            # For testing
-            cache = {}
-            ghat_al = []
-            for rc in cutoff_a:
-                if rc in cache:
-                    ghat_l = cache[rc]
-                else:
-                    g_lg = shape_functions(rgd, 'gauss', rc, lmax=2)
-                    ghat_l = [rgd.spline(g_g, l=1)
-                              for l, g_g in enumerate(g_lg)]
-                    cache[rc] = ghat_l
-                ghat_al.append(ghat_l)
-            vhat_al = [rgd.spline(np.zeros_like(rgd.r_g), l=l)
-                       for l in range(3)] * len(cutoff_a)
-        else:
-            g_lg = shape_functions(rgd, 'gauss', rcut, lmax=2)
-            ghat_l = [rgd.spline(g_g, l=1) for l, g_g in enumerate(g_lg)]
-            ghat_al = [ghat_l] * len(cutoff_a)
-            cache = {}
-            vhat_al = []
-            for rc in cutoff_a:
-                if rc in cache:
-                    vhat_l = cache[rc]
-                else:
-                    v_lg = dvl(rgd, rc, rcut, lmax=2)
-                    vhat_l = [rgd.spline(v_g, l=1)
-                              for l, v_g in enumerate(v_lg)]
-                    cache[rc] = vhat_l
-                vhat_al.append(vhat_l)
+        g_lg = shape_functions(rgd, 'gauss', rcut, lmax=2)
+        ghat_l = [rgd.spline(g_g, l=1) for l, g_g in enumerate(g_lg)]
+        ghat_al = [ghat_l] * len(cutoff_a)
+        cache = {}
+        vhat_al = []
+        for rc in cutoff_a:
+            if rc in cache:
+                vhat_l = cache[rc]
+            else:
+                v_lg = dvl(rgd, rc, rcut, lmax=2)
+                vhat_l = [rgd.spline(v_g, l=1)
+                          for l, v_g in enumerate(v_lg)]
+                cache[rc] = vhat_l
+            vhat_al.append(vhat_l)
 
         self.ghat_aLg = pwg.atom_centered_functions(
             ghat_al, fracpos_ac, atomdist=atomdist, xp=xp)
         self.vhat_aLg = pwg.atom_centered_functions(
             vhat_al, fracpos_ac, atomdist=atomdist, xp=xp)
+
+        self._neighbors = None
+
+    def get_neighbors(self):
+        if self._neighbors is None:
+            self._neighbors = primitive_neighbor_list(
+                'ijdD', )
+        return self._neighbors
 
     def dipole_layer_correction(self):
         return self.poisson_solver.dipole_layer_correction()
@@ -100,7 +90,16 @@ class PAWPoissonSolver:
         if vHt_g is None:
             vHt_g = pwg.zeros(xp=self.xp)
 
-        e_coulomb = self.poisson_solver.solve(vHt_g, charge_g)
+        e_coulomb1 = self.poisson_solver.solve(vHt_g, charge_g)
+
+        vhat_g = pwg.zeros()
+        self.vhat_aLg.add_to(vhat_g, Q_aL)
+        vHt_g.data += vhat_g.data
+        e_coulomb2 = vhat_g.integrate(nt_g)
+
+        e_coulomb3 = 0.0
+        for i, j, d, D in self.get_neighbors():
+            print(i, j, d, D)
 
         vHt0_g = vHt_g.gather()
         if pwg.comm.rank == 0:
@@ -108,6 +107,42 @@ class PAWPoissonSolver:
 
         V_aL = self.ghat_aLg.integrate(vHt_g)
 
+        return e_coulomb1 + e_coulomb2 + e_coulomb3, vHt_g, V_aL
+
+
+class SimplePAWPoissonSolver:
+    def __init__(self,
+                 pwg: PWDesc,
+                 setups: Setups,
+                 poisson_solver,
+                 fracpos_ac: np.ndarray,
+                 atomdist: AtomDistribution,
+                 xp=np):
+        self.xp = xp
+        self.pwg = pwg
+        self.pwg0 = pwg.new(comm=None)  # not distributed
+        self.poisson_solver = poisson_solver
+        self.ghat_aLg = setups.create_compensation_charges(
+            pwg, fracpos_ac, atomdist, xp)
+
+    def dipole_layer_correction(self):
+        return self.poisson_solver.dipole_layer_correction()
+
+    def solve(self,
+              nt_g: PWArray,
+              Q_aL: AtomArrays,
+              vt0_g: PWArray,
+              vHt_g: PWArray | None = None):
+        charge_g = nt_g.copy()
+        self.ghat_aLg.add_to(charge_g, Q_aL)
+        pwg = self.pwg
+        if vHt_g is None:
+            vHt_g = pwg.zeros(xp=self.xp)
+        e_coulomb = self.poisson_solver.solve(vHt_g, charge_g)
+        vHt0_g = vHt_g.gather()
+        if pwg.comm.rank == 0:
+            vt0_g.data += vHt0_g.data
+        V_aL = self.ghat_aLg.integrate(vHt_g)
         return e_coulomb, vHt_g, V_aL
 
 
