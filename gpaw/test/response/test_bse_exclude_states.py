3d4170b4cf23547d4ee87a37412931cee089148b
JakobKSvaneborg <jakobksvaneborg@protonmail.com>
2024-10-14 14:51:54 +0200

    small cleanup of test. still fails for 4 processes...

--- gpaw/test/response/test_bse_exclude_states.py
+++ gpaw/test/response/test_bse_exclude_states.py
@@ -2,7 +2,6 @@ import pytest
 from ase.build import bulk
 from gpaw import GPAW, FermiDirac
 from gpaw.response.bse import BSE
-import numpy as np
 
 
 @pytest.mark.response
@@ -26,18 +25,15 @@ def test_bse_exclude_states(in_tmp_dir):
               deps_max=6,
               eshift=eshift,
               nbands=8)
-    bse_matrix = bse.calculate(optical=True)
-    excludedeps_S = np.where(bse_matrix.deps_S > bse_matrix.deps_max)[0]
-    excludef_S = np.where(np.abs(bse_matrix.df_S) < 0.001)[0]
-    exclude_S = np.unique(np.concatenate((excludef_S, excludedeps_S)))
-    w_T, v_Rt, exclude_S = bse_matrix.diagonalize_tammdancoff(bse)
-    nk = (calc.wfs.kd.nbzkpts)
+    bse_matrix = bse.get_bse_matrix()
+    w_T, v_Rt, exclude_S = bse.diagonalize_bse_matrix(bse_matrix)
+
+    nk = calc.wfs.kd.nbzkpts
     nval = 3
     ncond = 2
     n_pairs = nk * nval * ncond
     assert len(exclude_S) == 14
     assert len(w_T) == n_pairs - len(exclude_S)
-    print(w_T)
     assert w_T[0] == pytest.approx(0.1004, abs=0.001)
     assert w_T[11] == pytest.approx(0.1287, abs=0.001)
     assert w_T[29] == pytest.approx(0.1924, abs=0.001)
