b978634a73498944175abb284b4300f5629da537
Mikael Kuisma <kuisma@dtu.dk>
2024-11-04 10:50:25 +0100

    Move ground state to a fixture

--- gpaw/test/gpwfile.py
+++ gpaw/test/gpwfile.py
@@ -745,6 +745,20 @@ class GPWFiles(CachedFilesHandler):
         scalapack = atoms.calc.wfs.bd.comm.size
         atoms.calc.diagonalize_full_hamiltonian(nbands=8, scalapack=scalapack)
         return atoms.calc
+   
+    @gpwfile 
+    def c2_gw_more_bands(self):
+        a = 3.567
+        atoms = bulk('C', 'diamond', a=a)
+        atoms.calc = GPAW(mode=PW(400),
+                          parallel={'domain': 1},
+                          kpts={'size': (2, 2, 2), 'gamma': True},
+                          xc='LDA',
+                          occupations=FermiDirac(0.001))
+        atoms.get_potential_energy()
+        atoms.calc.diagonalize_full_hamiltonian(nbands=128)
+        return atoms.calc
+
 
     @gpwfile
     def na_pw(self):
--- gpaw/test/response/test_gw_mpa_C.py
+++ gpaw/test/response/test_gw_mpa_C.py
@@ -7,30 +7,13 @@ from gpaw import PW
 from gpaw.response.g0w0 import G0W0
 
 
-def test_diamond_mpa(in_tmp_dir):
-    a = 3.567
-    atoms = bulk('C', 'diamond', a=a)
-
-    k = 2
-
-    calc = GPAW(mode=PW(400),
-                parallel={'domain': 1},
-                kpts={'size': (k, k, k), 'gamma': True},
-                xc='LDA',
-                occupations=FermiDirac(0.001))
-
-    atoms.calc = calc
-    atoms.get_potential_energy()
-
-    calc.diagonalize_full_hamiltonian()
-    calc.write('C_converged_mpa.gpw', 'all')
-
+def test_diamond_mpa(in_tmp_dir, gpw_files):
     ref_results_mp1 = np.array([[[11.48389, 18.685187]]])
     ref_results_mp8 = np.array([[[11.239777, 18.591851]]])
     ref_results = {1: ref_results_mp1, 8: ref_results_mp8}
 
     for npols in [1, 8]:
-        gw = G0W0(calc='C_converged_mpa.gpw',
+        gw = G0W0(calc=gpw_files['c2_gw_more_bands'],
                   kpts=[0],
                   bands=(3, 5),
                   ecut=200,
