f47826ce7ca212b9695a224aba3440f577bb36a8
Ask Hjorth Larsen <asklarsen@gmail.com>
2024-11-05 15:39:35 +0100

    test now blazing fast

--- gpaw/test/test_nosave_projections.py
+++ gpaw/test/test_nosave_projections.py
@@ -1,13 +1,22 @@
 import pytest
 from ase.build import bulk
 from gpaw.new.ase_interface import GPAW
+from gpaw.mpi import world
 
+# Prevent grid-dependent crash:
+parallel = dict(band=1 if world.size < 8 else 2)
 
 @pytest.fixture(scope='module', params=['fd', 'lcao', 'pw'])
 def noprojs_gpw(module_tmp_path, request):
     mode = request.param
-    atoms = bulk('Si')
-    atoms.calc = GPAW(mode=mode, kpts=[2, 2, 2], txt=None)
+    atoms = bulk('Al')
+    if mode in {'fd', 'lcao'}:
+        kwargs = dict(mode=mode, gpts=(8, 8, 8))
+    else:
+        kwargs = dict(mode={'name': 'pw', 'ecut': 200.0})
+    atoms.calc = GPAW(kpts=[2, 2, 2], txt=None, parallel=parallel,
+                      convergence={'density': 1e6, 'eigenstates': 1e6},
+                      **kwargs)
     atoms.get_potential_energy()
     gpw_path = module_tmp_path / 'gs_noprojs_{mode}.gpw'
     atoms.calc.write(gpw_path, include_projections=False)
@@ -15,7 +24,7 @@ def noprojs_gpw(module_tmp_path, request):
 
 
 def test_no_save_projections(noprojs_gpw):
-    calc = GPAW(noprojs_gpw)
+    calc = GPAW(noprojs_gpw, parallel=parallel)
     ibzwfs = list(calc.dft.ibzwfs)
     assert len(ibzwfs) > 0
     for wfs in ibzwfs:
@@ -26,7 +35,7 @@ def test_nice_error_message(noprojs_gpw):
     # We want there to be a good error message when we do not have
     # projections.  This only tests the most obvious case of .P_ani access,
     # but there could be code paths that will crash less controllably.
-    calc = GPAW(noprojs_gpw)
+    calc = GPAW(noprojs_gpw, parallel=parallel)
 
     wfs = next(iter(calc.dft.ibzwfs))
     with pytest.raises(RuntimeError, match='There are no proj'):
@@ -34,9 +43,10 @@ def test_nice_error_message(noprojs_gpw):
 
 
 def test_fixed_density_bandstructure(tmp_path, noprojs_gpw):
-    calc = GPAW(noprojs_gpw)
+    calc = GPAW(noprojs_gpw, parallel=parallel)
 
     fixed_calc = calc.fixed_density(
+        parallel=parallel,
         kpts=[[0., 0., 0.], [0., 0., 0.5]], symmetry='off')
 
     bs = fixed_calc.band_structure()
