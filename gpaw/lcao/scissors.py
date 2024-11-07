86de5279675df4c608ff30d511d862b7c5f14f88
Jens Jørgen Mortensen <jjmo@dtu.dk>
2024-11-07 10:32:33 +0100

    Add one_shot feature

--- gpaw/lcao/scissors.py
+++ gpaw/lcao/scissors.py
@@ -1,102 +1,20 @@
 """Scissors operator for LCAO."""
-from typing import Sequence, Tuple
+from __future__ import annotations
+
+from typing import Sequence
 
 import numpy as np
 from ase.units import Ha
 
-from gpaw.hamiltonian import Hamiltonian
-from gpaw.typing import Array2D, Array3D
-from gpaw.kpoint import KPoint
-from gpaw.wavefunctions.base import WaveFunctions
-
 from gpaw.lcao.eigensolver import DirectLCAO
+from gpaw.new.calculation import DFTCalculation
 from gpaw.new.lcao.eigensolver import LCAOEigensolver
-
-
-class Scissors(DirectLCAO):
-    def __init__(self, shifts: Sequence[Tuple[float, float, int]]):
-        """Scissors-operator eigensolver.
-
-        The *shifts* are given as a sequence of tuples::
-
-            [(<shift for occupied states>,
-              <shift for unoccupied states>,
-              <number of atoms>),
-             ...]
-
-        Here we open a gap for states on atoms with indices 3, 4 and 5:
-
-        >>> eigensolver = Scissors([(0.0, 0.0, 3),
-        ...                         (-0.5, 0.5, 3)])
-
-        """
-        DirectLCAO.__init__(self)
-        self.shifts = []
-        for homo, lumo, natoms in shifts:
-            self.shifts.append((homo / Ha, lumo / Ha, natoms))
-
-    def write(self, writer):
-        writer.write(name='lcao')
-
-    def __repr__(self):
-        txt = DirectLCAO.__repr__(self)
-        txt += '\n    Scissors operators:\n'
-        a1 = 0
-        for homo, lumo, natoms in self.shifts:
-            a2 = a1 + natoms
-            txt += (f'      Atoms {a1}-{a2 - 1}: '
-                    f'VB: {homo * Ha:+.3f} eV, '
-                    f'CB: {lumo * Ha:+.3f} eV\n')
-            a1 = a2
-        return txt
-
-    def calculate_hamiltonian_matrix(self,
-                                     ham: Hamiltonian,
-                                     wfs: WaveFunctions,
-                                     kpt: KPoint,
-                                     Vt_xMM: Array3D = None,
-                                     root: int = -1,
-                                     add_kinetic: bool = True) -> Array2D:
-        """Add scissors operator."""
-        H_MM = DirectLCAO.calculate_hamiltonian_matrix(
-            self, ham, wfs, kpt, Vt_xMM, root, add_kinetic)
-        if kpt.C_nM is None:
-            return H_MM
-
-        S_MM = wfs.S_qMM[kpt.q]
-        assert abs(S_MM - S_MM.T.conj()).max() < 1e-10
-
-        nocc = ham.setups.nvalence // 2
-        C_nM = kpt.C_nM
-        iC_Mn = np.linalg.inv(C_nM)
-        Co_nM = C_nM.copy()
-        Co_nM[nocc:] = 0.0
-        Cu_nM = C_nM.copy()
-        Cu_nM[:nocc] = 0.0
-
-        M1 = 0
-        a1 = 0
-        for homo, lumo, natoms in self.shifts:
-            a2 = a1 + natoms
-            M2 = M1 + sum(setup.nao for setup in ham.setups[a1:a2])
-
-            D_MM = np.zeros_like(S_MM)
-            D_MM[M1:M2, M1:M2] = S_MM[M1:M2, M1:M2]
-
-            H_MM += iC_Mn @ (
-                Co_nM @ D_MM @ Co_nM.T.conj() * homo +
-                Cu_nM @ D_MM @ Cu_nM.T.conj() * lumo) @ iC_Mn.T.conj()
-
-            a1 = a2
-            M1 = M2
-
-        return H_MM
 
 
 class ScissorsLCAOEigensolver(LCAOEigensolver):
     def __init__(self,
                  basis,
-                 shifts: Sequence[Tuple[float, float, int]]):
+                 shifts: Sequence[tuple[float, float, int]]):
         """Scissors-operator eigensolver.
 
         The *shifts* are given as a sequence of tuples::
@@ -149,42 +67,31 @@ class MyMatCalc:
         S_MM = wfs.S_MM.data
         assert abs(S_MM - S_MM.T.conj()).max() < 1e-10
 
-        new = 1
-
         C_nM = wfs.C_nM.data
-        if new:
-            M1 = 0
-            a1 = 0
-            for homo, lumo, natoms in self.shifts:
-                a2 = a1 + natoms
-                M2 = M1 + sum(setup.nao for setup in wfs.setups[a1:a2])
-                A_nM = C_nM[:nocc, M1:M2].conj() @ S_MM[M1:M2]
-                H_MM.data += homo * A_nM.T.conj() @ A_nM
-                A_nM = C_nM[nocc:, M1:M2].conj() @ S_MM[M1:M2]
-                H_MM.data += lumo * A_nM.T.conj() @ A_nM
-                a1 = a2
-                M1 = M2
-        else:
-            iC_Mn = np.linalg.inv(C_nM)
-            Co_nM = C_nM.copy()
-            Co_nM[nocc:] = 0.0
-            Cu_nM = C_nM.copy()
-            Cu_nM[:nocc] = 0.0
-
-            M1 = 0
-            a1 = 0
-            for homo, lumo, natoms in self.shifts:
-                a2 = a1 + natoms
-                M2 = M1 + sum(setup.nao for setup in wfs.setups[a1:a2])
-
-                D_MM = np.zeros_like(S_MM)
-                D_MM[M1:M2, M1:M2] = S_MM[M1:M2, M1:M2]
-
-                H_MM.data += iC_Mn @ (
-                    Co_nM @ D_MM @ Co_nM.T.conj() * homo +
-                    Cu_nM @ D_MM @ Cu_nM.T.conj() * lumo) @ iC_Mn.T.conj()
-
-                a1 = a2
-                M1 = M2
-
+        M1 = 0
+        a1 = 0
+        for homo, lumo, natoms in self.shifts:
+            a2 = a1 + natoms
+            M2 = M1 + sum(setup.nao for setup in wfs.setups[a1:a2])
+            A_nM = C_nM[:nocc, M1:M2].conj() @ S_MM[M1:M2]
+            H_MM.data += homo * A_nM.T.conj() @ A_nM
+            A_nM = C_nM[nocc:, M1:M2].conj() @ S_MM[M1:M2]
+            H_MM.data += lumo * A_nM.T.conj() @ A_nM
+            a1 = a2
+            M1 = M2
         return H_MM
+
+
+def one_shot(shifts, dft: DFTCalculation) -> np.ndarray:
+    shifts = [(homo / Ha, lumo / Ha, natoms)
+              for homo, lumo, natoms in shifts]
+    matcalc = dft.scf_loop.hamiltonian.create_hamiltonian_matrix_calculator(
+        dft.potential)
+    matcalc = MyMatCalc(matcalc, shifts)
+    eig_uM = []
+    for wfs in dft.ibzwfs:
+        H_MM = matcalc.calculate_matrix(wfs)
+        eig_M = H_MM.eighg(wfs.L_MM, wfs.domain_comm)
+        eig_uM.append(eig_M)
+    shape = (dft.ibzwfs.nspins, -1, len(eig_M))
+    return np.array(eig_uM).reshape(shape) * Ha
