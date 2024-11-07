a2ce5eccf32b135fe77687ff659c4c3de83a1ec8
Jens Jørgen Mortensen <jjmo@dtu.dk>
2024-11-07 15:58:55 +0100

    A-B stacking + ... (WIP)

--- doc/documentation/scissors/mos2ws2.py
+++ doc/documentation/scissors/mos2ws2.py
@@ -6,7 +6,8 @@ a12 = mx2(formula='MoS2', kind='2H', a=3.184, thickness=3.13,
 a12 += mx2(formula='WS2', kind='2H', a=3.184, thickness=3.15,
            size=(1, 1, 1))
 a12.positions[3:, 2] += 3.6 + (3.13 + 3.15) / 2
-a12.center(vacuum=3.0, axis=2)
+a12.positions[3:] += [-1 / 3, 1 / 3, 0] @ a12.cell
+a12.center(vacuum=6.0, axis=2)
 
 k = 6
 a12.calc = GPAW(mode='lcao',
@@ -16,12 +17,15 @@ a12.calc = GPAW(mode='lcao',
                 eigensolver={'name': 'scissors',
                              'shifts': ([(-0.5, 0.5, 3),
                                          (-0.3, 0.3, 3)])},
-                txt='12.txt')
+                txt='12B.txt')
 a12.get_potential_energy()
-a12.calc.write('12.gpw')
+# a12.get_forces()
 
-bp = a12.cell.bandpath('GMKG', npoints=80)
+if 1:
+    a12.calc.write('12B.gpw')
 
-c12 = a12.calc.fixed_density(kpts=bp, symmetry='off')
-bs = c12.band_structure()
-bs.write('12bs.json')
+    bp = a12.cell.bandpath('GMKG', npoints=50)
+
+    c12 = a12.calc.fixed_density(kpts=bp, symmetry='off')
+    bs = c12.band_structure()
+    bs.write('12Bbs.json')
