4f177fe06981b86e4f03d7dd67a80bc40253a9d5
giale <gianl.levi@gmail.com>
2024-11-20 14:24:54 +0000

    Use np.sinc(x/np.pi) in calculation of matrix exponential for unitary invariant functionals in LCAO direct optimization

--- gpaw/directmin/tools.py
+++ gpaw/directmin/tools.py
@@ -61,11 +61,8 @@ def expm_ed_unit_inv(a_upp_r, oo_vo_blockonly=False):
     eigval[eigval.real < 1.0e-13] = 1.0e-13
     sqrt_eval = np.sqrt(eigval)
 
-    sin_sqrt_p = matrix_function(sqrt_eval, evec, np.sin)
     cos_sqrt_p = matrix_function(sqrt_eval, evec, np.cos)
-    sqrt_inv_p = matrix_function(1.0 / sqrt_eval, evec)
-
-    psin = sqrt_inv_p @ sin_sqrt_p
+    psin = matrix_function(sqrt_eval / np.pi, evec, np.sinc)
     u_oo = cos_sqrt_p
     u_vo = - a_upp_r.T.conj() @ psin
 
