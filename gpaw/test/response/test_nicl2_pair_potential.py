e6b792a52820927b5b1dd468c99f33208b48a5ab
Thorbjørn Skovhus <s134264@student.dtu.dk>
2025-01-27 11:53:29 +0100

    add kspair markers on all remaining tests which rely on the kspair-based response code

--- gpaw/test/response/test_nicl2_pair_potential.py
+++ gpaw/test/response/test_nicl2_pair_potential.py
@@ -13,6 +13,7 @@ from gpaw.test.response.test_parallel_kptpair_extraction import \
 
 
 @pytest.mark.response
+@pytest.mark.kspair
 @pytest.mark.old_gpaw_only
 def test_nicl2_pair_potential(gpw_files):
     """Test that the transverse pair potential vanishes in vacuum."""
