60841804bd94f3b8b2c0ed1abd95570682531570
amalie <you@example.com>
2025-01-30 12:00:25 +0100

    moved n1, n2 to be before m1, m2 except in update_chi0()

--- gpaw/response/chi0.py
+++ gpaw/response/chi0.py
@@ -129,14 +129,14 @@ class Chi0Calculator:
         """
         if n2 is None:
             n2 = self.gs.nocc2
-        self.chi0_body_calc.update_chi0_body(chi0.body, m1, m2, spins, n1, n2)
+        self.chi0_body_calc.update_chi0_body(chi0.body, n1, n2, m1, m2, spins)
         if chi0.optical_limit:
             if self.chi0_body_calc.eshift is not None:
                 raise NotImplementedError("No wings eshift available")
             assert chi0.optical_extension is not None
             # Update the head and wings
             self.chi0_opt_ext_calc.update_chi0_optical_extension(
-                chi0.optical_extension, m1, m2, spins, n1, n2)
+                chi0.optical_extension, n1, n2, m1, m2, spins)
         return chi0
 
 
@@ -193,17 +193,16 @@ class Chi0BodyCalculator(Chi0ComponentPWCalculator):
 
         # Integrate all transitions into partially filled and empty bands
         n1, n2, m1, m2 = self.get_band_transitions()
-        self.update_chi0_body(chi0_body, m1, m2,
-                              spins=range(self.gs.nspins),
-                              n1=n1, n2=n2)
+        self.update_chi0_body(chi0_body, n1, n2, m1, m2,
+                              spins=range(self.gs.nspins))
 
         return chi0_body
 
     def update_chi0_body(self,
                          chi0_body: Chi0BodyData,
+                         n1: int, n2: int,
                          m1: int, m2: int,
-                         spins: list | range,
-                         n1: int, n2: int):
+                         spins: list | range):
         """In-place calculation of the body.
 
         Parameters
@@ -229,7 +228,7 @@ class Chi0BodyCalculator(Chi0ComponentPWCalculator):
         symmetries, generator, domain, prefactor = self.get_integration_domain(
             qpd.q_c, spins)
         integrand = Chi0Integrand(self, qpd=qpd, generator=generator,
-                                  optical=False, m1=m1, m2=m2, n1=n1, n2=n2)
+                                  optical=False, n1=n1, n2=n2, m1=m1, m2=m2)
 
         chi0_body.data_WgG[:] /= prefactor
         if self.hilbert:
@@ -372,9 +371,8 @@ class Chi0OpticalExtensionCalculator(Chi0ComponentPWCalculator):
         n1, n2, m1, m2 = self.get_band_transitions()
 
         # Perform the actual integration
-        self.update_chi0_optical_extension(chi0_opt_ext, m1, m2,
-                                           spins=range(self.gs.nspins),
-                                           n1=n1, n2=n2)
+        self.update_chi0_optical_extension(chi0_opt_ext, n1, n2, m1, m2,
+                                           spins=range(self.gs.nspins))
 
         if self.drude_calc is not None:
             # Add intraband contribution
@@ -388,9 +386,9 @@ class Chi0OpticalExtensionCalculator(Chi0ComponentPWCalculator):
     def update_chi0_optical_extension(
             self,
             chi0_optical_extension: Chi0OpticalExtensionData,
+            n1: int, n2: int,
             m1: int, m2: int,
-            spins: list | range,
-            n1: int, n2: int):
+            spins: list | range):
         """In-place calculation of the chi0 head and wings.
 
         Parameters
@@ -409,7 +407,7 @@ class Chi0OpticalExtensionCalculator(Chi0ComponentPWCalculator):
         symmetries, generator, domain, prefactor = self.get_integration_domain(
             qpd.q_c, spins)
         integrand = Chi0Integrand(self, qpd=qpd, generator=generator,
-                                  optical=True, m1=m1, m2=m2, n1=n1, n2=n2)
+                                  optical=True, n1=n1, n2=n2, m1=m1, m2=m2)
 
         # We integrate the head and wings together, using the combined index P
         # index v = (x, y, z)
