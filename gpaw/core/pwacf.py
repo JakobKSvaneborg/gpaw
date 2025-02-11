56f0a79656933731347940c1860c3cd0315d66d8
Mikael Kuisma <kuisma@dtu.dk>
2025-02-11 14:42:47 +0100

    lets not change blocksize in this MR

--- gpaw/core/pwacf.py
+++ gpaw/core/pwacf.py
@@ -85,7 +85,7 @@ class PWLFC:  # (BaseLFC)
                  *,
                  xp,
                  integrals: ArrayLike1D | float | None = None,
-                 blocksize: int | None = 10000):
+                 blocksize: int | None = 5000):
         """Reciprocal-space plane-wave localized function collection.
 
         spline_aj: list of list of spline objects
