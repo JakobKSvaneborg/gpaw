83225518dca8ed9677fe08aef4d6dc5e417cb873
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-02-14 13:29:36 +0100

    instead of opening /dev/null, which looks like a resource leak to pytest even though we registered its closing using atexit, use a fake file inside a real wrapper

--- gpaw/utilities/__init__.py
+++ gpaw/utilities/__init__.py
@@ -3,7 +3,7 @@
 
 """Utility functions and classes."""
 
-import atexit
+import io
 import os
 import re
 import sys
@@ -422,8 +422,12 @@ def file_barrier(path: Union[str, Path], world=None):
     world.barrier()
 
 
-devnull = open(os.devnull, 'w')
-atexit.register(devnull.close)
+class _NullIO(io.TextIOBase):
+    def writable(self):
+        return True
+
+
+devnull = io.TextIOWrapper(_NullIO())
 
 
 def convert_string_to_fd(name, world=None):
