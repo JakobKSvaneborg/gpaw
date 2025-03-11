4427696f9846412cd531a21c34209fb3e0a0a05c
Thorbjørn Skovhus <s134264@student.dtu.dk>
2025-03-11 15:11:46 +0100

    check all IBZ k-points for gamma-centered ground states

--- gpaw/test/response/test_symmetry.py
+++ gpaw/test/response/test_symmetry.py
@@ -14,24 +14,31 @@ def test_qsymmetries(gpw_files, identifier):
     context = ResponseContext()
     qsymmetry = QSymmetryAnalyzer()
 
+    # Count all symmetries:
+    symmetry = gs.kd.symmetry
+    ndirect = len(symmetry.op_scc)
+    nindirect = ndirect * (1 - symmetry.has_inversion)
+    nsymmetries = ndirect + nindirect
+
     # Test symmetry analysis
     kpts = gs._calc.parameters.get('kpts', {})
     if 'gamma' in kpts and kpts['gamma']:
         # If the ground state is Γ-centered, all IBZ k-points are valid
         # q-points as well (autocommensurate) and we check that the q-point
         # symmetry analyzer reproduces the symmetries of the ground state.
-        pass  # XXX
+        for k, k_c in enumerate(gs.kd.ibzk_kc):
+            # Add a bit of numerical noise:
+            q_c = k_c + (np.random.rand(3) - 0.5) * 1e-15
+            qsymmetries, _ = qsymmetry.analyze(q_c, gs.kpoints, context)
+            # The number of q -> G + q symmetries is reduced by the
+            # multiplicity of the corresponding k-point
+            bzk_K = np.where(gs.kd.bz2ibz_k == k)[0]
+            assert nsymmetries % len(bzk_K) == 0
+            assert len(qsymmetries) == nsymmetries // len(bzk_K)
     else:
         # If the ground state isn't Γ-centered, we simply check that a "noisy"
         # Γ-point q vector recovers all symmetries of the system
-        # All symmetries:
-        symmetry = gs.kd.symmetry
-        ndirect = len(symmetry.op_scc)
-        nindirect = ndirect * (1 - symmetry.has_inversion)
-        # "Noisy" q-point:
-        q_c = (np.random.rand(3) - 0.5) * 1e-15
+        q_c = (np.random.rand(3) - 0.5) * 1e-15  # "Noisy" Γ-point
         qsymmetries, _ = qsymmetry.analyze(q_c, gs.kpoints, context)
-        assert qsymmetries.ndirect == ndirect, \
-            f'{q_c}: {qsymmetries.ndirect} / {ndirect}'
-        assert qsymmetries.nindirect == nindirect, \
-            f'{q_c}: {qsymmetries.nindirect} / {nindirect}'
+        assert qsymmetries.ndirect == ndirect, f'{q_c}'
+        assert qsymmetries.nindirect == nindirect, f'{q_c}'
