d3f11f45cb519eb71b7a33fe07da271331b1b714
Jens Jørgen Mortensen <jj@smoerhul.dk>
2025-03-11 17:25:37 +0100

    Make sure potential is zero at boundary above slab

--- gpaw/test/solvation/test_fixed_fermi_level.py
+++ gpaw/test/solvation/test_fixed_fermi_level.py
@@ -1,23 +1,24 @@
 from gpaw.new.ase_interface import GPAW
 from ase import Atoms
 from gpaw.jellium import JelliumSlab
+import numpy as np
 
 
 def test_ffl():
     a = 1.4
-    atoms = Atoms('H', cell=[a, a, 8.0], pbc=(1, 1, 0))
-    atoms.positions[0, 2] = 3.0
+    atoms = Atoms('H', cell=[a, a, 11.0], pbc=(1, 1, 0))
+    atoms.positions[0, 2] = 4.0
     k = 2
     atoms.calc = GPAW(
         mode='pw',
         kpts=(k, k, 1),
         poissonsolver={'dipolelayer': 'xy'},
-        background_charge=JelliumSlab(0.01, 4.0, 6.0))
+        background_charge=JelliumSlab(0.01, 7.0, 9.0))
     atoms.get_potential_energy()
     v = atoms.calc.get_electrostatic_potential()
     import matplotlib.pyplot as plt
     print(v.shape)
-    plt.plot(v[0, 0])
+    plt.plot(np.linspace(0, 11, v.shape[2], 0), v[0, 0])
     plt.show()
 
 
