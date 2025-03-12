99b6f7ba541f51c724c371496dbe9c87ddf6ffe9
Mikael Kuisma <kuisma@dtu.dk>
2025-03-12 16:44:41 +0100

    cpupy needs rtol as kwargs

--- gpaw/test/core/test_pw.py
+++ gpaw/test/core/test_pw.py
@@ -97,7 +97,7 @@ def test_pw_integrate(xp, grid):
     gg = g.new()
     gg.scatter_from(f.gather(broadcast=True)
                     .ifft(grid=g.desc.new(comm=None)))
-    assert xp.allclose(g.data, gg.data, 1e-15)
+    assert xp.allclose(g.data, gg.data, rtol=1e-14)
 
     i1 = g.integrate()
     i2 = f.integrate()
