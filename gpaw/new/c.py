396c23ded91fbfde9e9ac3f8508d87780cc02a3a
the-ohm <mikhik@yahoo.dk>
2025-03-18 10:32:08 +0100

    Flakes.

--- gpaw/new/c.py
+++ gpaw/new/c.py
@@ -31,11 +31,10 @@ if not TYPE_CHECKING and not GPAW_NO_C_EXTENSION:
 else:
     from gpaw.purepython import (add_to_density, pw_insert, pw_precond,  # noqa
                                  pwlfc_expand, symmetrize_ft)
-    from gpaw.purepython import (add_to_density_gpu,
-                                 calculate_residuals_gpu,  # noqa
+    from gpaw.purepython import (add_to_density_gpu,  # noqa
+                                 calculate_residuals_gpu,
                                  dH_aii_times_P_ani_gpu, evaluate_lda_gpu,
                                  evaluate_pbe_gpu,
                                  pw_amend_insert_realwf_gpu,
                                  pw_insert_gpu, pwlfc_expand_gpu,
                                  pw_norm_kinetic_gpu, pw_norm_gpu)
-
