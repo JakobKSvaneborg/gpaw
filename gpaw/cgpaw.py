0efa586e67d5293e4883c0a9009ffce51bab09b4
Mikael Kuisma <kuisma@dtu.dk>
2025-03-20 09:13:41 +0100

    Do not force users to recompile due to merging magma support to master

--- gpaw/cgpaw.py
+++ gpaw/cgpaw.py
@@ -2,8 +2,12 @@ from gpaw import GPAW_NO_C_EXTENSION
 
 if GPAW_NO_C_EXTENSION:
     import gpaw.pp as _gpaw
+    have_magma = False
 else:
     import _gpaw  # type: ignore[no-redef]
+
+    # Do not force users to recompile due to merging magma support to master
+    have_magma = getattr(_gpaw, 'have_magma', False)
 
 
 def __getattr__(name):
