01af270ff289519964f10374fbac69531d83d5ab
the-ohm <mikhik@yahoo.dk>
2025-03-27 12:51:10 +0100

    Cleaning

--- gpaw/new/c.py
+++ gpaw/new/c.py
@@ -20,7 +20,7 @@ if not TYPE_CHECKING and not GPAW_NO_C_EXTENSION:
                                 evaluate_pbe_gpu, pw_amend_insert_realwf_gpu,
                                 pw_insert_gpu, pwlfc_expand_gpu,
                                 pw_norm_kinetic_gpu, pw_norm_gpu)
-        # This is somewhat useless to trace the kernels in CPU
+
         w = trace(gpu=True)
         add_to_density_gpu = w(add_to_density_gpu)
         calculate_residuals_gpu = w(calculate_residuals_gpu)
@@ -32,7 +32,6 @@ if not TYPE_CHECKING and not GPAW_NO_C_EXTENSION:
         pwlfc_expand_gpu = w(pwlfc_expand_gpu)
         pw_norm_kinetic_gpu = w(pw_norm_kinetic_gpu)
         pw_norm_gpu = w(pw_norm_gpu)
-
     else:
         from gpaw.purepython import (add_to_density_gpu,
                                      calculate_residuals_gpu,  # noqa
@@ -51,15 +50,3 @@ else:
                                  pw_amend_insert_realwf_gpu,
                                  pw_insert_gpu, pwlfc_expand_gpu,
                                  pw_norm_kinetic_gpu, pw_norm_gpu)
-    # This is somewhat useless to trace the kernels in CPU
-    w = trace(gpu=True)
-    add_to_density_gpu = w(add_to_density_gpu)
-    calculate_residuals_gpu = w(calculate_residuals_gpu)
-    dH_aii_times_P_ani_gpu = w(dH_aii_times_P_ani_gpu)
-    evaluate_lda_gpu = w(evaluate_lda_gpu)
-    evaluate_pbe_gpu = w(evaluate_pbe_gpu)
-    pw_amend_insert_realwf_gpu = w(pw_amend_insert_realwf_gpu)
-    pw_insert_gpu = w(pw_insert_gpu)
-    pwlfc_expand_gpu = w(pwlfc_expand_gpu)
-    pw_norm_kinetic_gpu = w(pw_norm_kinetic_gpu)
-    pw_norm_gpu = w(pw_norm_gpu)
