ec519a70b98cd8de723cb515cf834ea07a1ed24d
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-03-31 16:15:03 +0200

    New test

---
+++ gpaw/test/solvation/test_sjm_h.py
@@ -0,0 +1,73 @@
+from ase import Atoms
+from ase.data.vdw import vdw_radii
+from gpaw import FermiDirac
+from gpaw.new.sjm import SJM
+from gpaw.solvation import (EffectivePotentialCavity, GradientSurface,
+                            LinearDielectric, SurfaceInteraction)
+from gpaw.solvation.sjm import SJM as OldSJM, SJMPower12Potential
+from gpaw.new.ase_interface import GPAW
+
+
+def test_sjm(gpaw_new):
+    # Solvent parameters
+    u0 = 0.180  # eV
+    epsinf = 78.36  # Dielectric constant of water at 298 K
+    gamma = 0.00114843767916  # 18.4*1e-3 * Pascal* m
+    T = 298.15   # K
+
+    def atomic_radii(atoms):
+        return [vdw_radii[n] for n in atoms.numbers]
+
+    atoms = Atoms('H', [[0, 0, 4]], pbc=(1, 1, 0), cell=[1.3, 1.3, 10])
+
+    potential = 3.1
+    tol = 0.02
+    sj = {'target_potential': None,  # potential,
+          'excess_electrons': -0.045,
+          'jelliumregion': {'top': 9.0},
+          'tol': tol}
+
+    convergence = {
+        'energy': 0.05 / 8.,
+        'density': 1e-4,
+        'eigenstates': 1e-4}
+
+    params = dict(
+        mode='fd',
+        # gpts=(8, 8, 48),
+        kpts=(2, 2, 1),
+        xc='PBE',
+        convergence=convergence,
+        occupations=FermiDirac(0.1))
+
+    solvation = dict(
+        cavity=EffectivePotentialCavity(
+            effective_potential=SJMPower12Potential(atomic_radii, u0),
+            temperature=T,
+            surface_calculator=GradientSurface()),
+        dielectric=LinearDielectric(epsinf=epsinf),
+        interactions=[SurfaceInteraction(surface_tension=gamma)])
+
+    if not gpaw_new:
+        atoms.calc = OldSJM(**params, sj=sj, **solvation)
+        atoms.get_potential_energy()
+        pot = atoms.calc.get_electrode_potential()
+    else:
+        atoms.calc = GPAW(
+            **params,
+            environment=SJM(**sj, **solvation))
+        atoms.get_potential_energy()
+        pot = -atoms.calc.get_fermi_level()
+
+    assert abs(pot - potential) < tol
+    if 1:
+        v = atoms.calc.get_electrostatic_potential()
+        import matplotlib.pyplot as plt
+        import numpy as np
+        plt.plot(np.linspace(0, atoms.cell[2, 2], v.shape[2], 0), v[0, 0])
+        plt.show()
+
+
+if __name__ == '__main__':
+    import sys
+    test_sjm(int(sys.argv[1]))
