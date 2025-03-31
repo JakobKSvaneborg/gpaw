5bb2b2be07ef7d0445cd4a92dacae4cdd6e10380
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-03-31 09:21:08 +0200

    Allow for no target potential

--- gpaw/new/sjm.py
+++ gpaw/new/sjm.py
@@ -2,9 +2,8 @@ from __future__ import annotations
 
 import numpy as np
 from ase.units import Bohr
-
 from gpaw.jellium import create_background_charge
-from gpaw.new.environment import Environment, FixedPotentialJellium
+from gpaw.new.environment import Environment, FixedPotentialJellium, Jellium
 from gpaw.new.poisson import PoissonSolverWrapper
 from gpaw.new.solvation import Solvation
 
@@ -16,7 +15,7 @@ class SJM:
                  dielectric,
                  interactions,
                  jelliumregion,
-                 target_potential: float,  # eV
+                 target_potential: float | None,  # eV
                  excess_electrons: float = 0.0,
                  tol: float = 0.001):  # eV
         self.cavity = cavity
@@ -43,25 +42,30 @@ class SJM:
         h = grid.cell_cv[2, 2] * Bohr
         z1 = relpos_ac[:, 2].max() * h + 3.0
         z2 = self.jelliumregion.get('top', h - 1.0)
-        jellium = create_background_charge(charge=self.excess_electrons,
-                                           z1=z1,
-                                           z2=z2)
-        jellium.set_grid_descriptor(grid._gd)
-        fixed_pot = FixedPotentialJellium(
-            jellium,
-            natoms=len(relpos_ac),
-            grid=grid,
-            workfunction=self.target_potential)
-        return SJMEnvironment(solvation, fixed_pot)
+        background = create_background_charge(charge=self.excess_electrons,
+                                              z1=z1,
+                                              z2=z2)
+        background.set_grid_descriptor(grid._gd)
+        if self.target_potential is None:
+            jellium = Jellium(background,
+                              natoms=len(relpos_ac),
+                              grid=grid)
+        else:
+            jellium = FixedPotentialJellium(
+                jellium,
+                natoms=len(relpos_ac),
+                grid=grid,
+                workfunction=self.target_potential)
+        return SJMEnvironment(solvation, jellium)
 
 
 class SJMEnvironment(Environment):
     def __init__(self,
                  solvation: Solvation,
-                 fixed_potential: FixedPotentialJellium):
+                 jellium: Jellium):
         super().__init__(solvation.natoms)
         self.solvation = solvation
-        self.fixed_potential = fixed_potential
+        self.jellium = jellium
         self.charge = self.fixed_potential.charge
 
     def create_poisson_solver(self, **kwargs):
@@ -73,14 +77,14 @@ class SJMEnvironment(Environment):
                              occ_calc,
                              mixer,
                              log) -> bool:
-        converged = self.fixed_potential.post_scf_convergence(
+        converged = self.jellium.post_scf_convergence(
             ibzwfs, occ_calc, mixer, log)
         self.charge = self.fixed_potential.charge
         return converged
 
     def update1(self, nt_r):
         self.solvation.update1(nt_r)
-        self.fixed_potential.update1(nt_r)
+        self.jellium.update1(nt_r)
 
     def update2(self, nt_r, vHt_r, vt_sr) -> float:
         return self.solvation.update2(nt_r, vHt_r, vt_sr)
