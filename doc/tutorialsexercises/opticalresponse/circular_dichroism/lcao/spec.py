caad602885652adc6eae917d9bd055bfce936cd4
Maria J. Weseloh <merchandise@neue-dateien.de>
2025-03-31 17:16:25 +0200

    Edited forgotten f-string.

--- doc/tutorialsexercises/opticalresponse/circular_dichroism/lcao/spec.py
+++ doc/tutorialsexercises/opticalresponse/circular_dichroism/lcao/spec.py
@@ -1,8 +1,8 @@
 from gpaw.tddft.spectrum import rotatory_strength_spectrum
 
 for gauge in ['length', 'velocity']:
-    rotatory_strength_spectrum(['mm-{gauge}_x.dat', 'mm-{gauge}_y.dat',
-                               'mm-{gauge}_z.dat'],
-                               'rot_spec-{gauge}.dat',
+    rotatory_strength_spectrum([f'mm-{gauge}_x.dat', f'mm-{gauge}_y.dat',
+                               f'mm-{gauge}_z.dat'],
+                               f'rot_spec-{gauge}.dat',
                                folding='Gauss', width=0.2,
                                e_min=0.0, e_max=10.0, delta_e=0.01)
