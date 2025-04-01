41825691bb1d82e86f21b313d3e76b0cc4cdbde5
the-ohm <mikhik@yahoo.dk>
2025-04-01 11:58:34 +0200

    Flakes.

--- gpaw/test/new/test_calc_info.py
+++ gpaw/test/new/test_calc_info.py
@@ -8,11 +8,11 @@ from gpaw.new.builder import get_calculation_info
 def test_calc_info():
     atoms = bulk('Si')
     calc_info = get_calculation_info(atoms,
-                                      h=0.15,
-                                      xc='PBE',
-                                      kpts={'density': 2, 'gamma': True},
-                                      mode='lcao',
-                                      spinpol=True,)
+                                     h=0.15,
+                                     xc='PBE',
+                                     kpts={'density': 2, 'gamma': True},
+                                     mode='lcao',
+                                     spinpol=True,)
     assert len(calc_info.ibz) == 10
     assert (calc_info.grid.size == np.array([20, 20, 20])).all()
     assert calc_info.nspins == 2
