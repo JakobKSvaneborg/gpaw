5d9aa56dc6f70a29112901b0e71702a48f484353
the-ohm <mikhik@yahoo.dk>
2025-04-02 14:00:11 +0200

    Flakes.

--- gpaw/new/gpw.py
+++ gpaw/new/gpw.py
@@ -359,8 +359,8 @@ def read_gpw(filename: Union[str, Path, IO[str]],
 
     ibzwfs = builder.read_ibz_wave_functions(reader, log)
 
-    dft = DFTCalculation(atoms,
-        ibzwfs, density, potential,
+    dft = DFTCalculation(
+        atoms, ibzwfs, density, potential,
         builder.setups,
         builder.create_scf_loop(),
         pot_calc=builder.create_potential_calculator(log),
