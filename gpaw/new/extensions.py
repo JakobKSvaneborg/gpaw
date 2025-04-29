42845e8741fa8133e1953f5f22173041a26590a2
Mikael Kuisma <kuisma@dtu.dk>
2025-04-29 11:22:06 +0200

    D3 correction extension

--- gpaw/new/extensions.py
+++ gpaw/new/extensions.py
@@ -1,3 +1,7 @@
+from gpaw.new.input_parameters import register
+from ase.units import Hartree, Bohr
+
+
 class Extension:
     name = 'unnamed extension'
 
@@ -22,6 +26,9 @@ class D3(ExtensionParameter):
         self.xc = xc
         self.kwargs = kwargs
 
+    def todict(self) -> dict:
+        return {'xc': self.xc, **self.kwargs}
+
     def build(self, atoms, domain_comm):
         atoms = atoms.copy()
         class D3Extension(Extension):
@@ -29,16 +36,16 @@ class D3(ExtensionParameter):
                 super().__init__()
                 self._calculate(atoms)
 
-            def _calculate(self, atoms):
+            def _calculate(_self, atoms):
                 from ase.calculators.dftd3 import PureDFTD3
-                atoms.calc = PureDFTD3(xc=self.params.xc, **self.params.kwargs)
+                atoms.calc = PureDFTD3(xc=self.xc, **self.kwargs)
                 # XXX params.xc should be taken directly from the calculator.
                 # XXX What if this is changed via set?
-                self.F_av = atoms.get_forces()
-                self.E = atoms.get_potential_energy()
+                _self.F_av = atoms.get_forces() / Hartree * Bohr
+                _self.E = atoms.get_potential_energy() / Hartree
 
             def get_energy_contributions(_self) -> dict[str, float]:
-                return {f'D3 (xc={self.xc})': self.E}
+                return {f'D3 (xc={self.xc})': _self.E}
 
             def force_contribution(self):
                 if domain_comm.rank == 0:
@@ -48,5 +55,6 @@ class D3(ExtensionParameter):
 
             def move_atoms(self, relpos_ac) -> None:
                 atoms.set_scaled_positions(relpos_ac)
+                self._calculate(atoms)
 
         return D3Extension()
