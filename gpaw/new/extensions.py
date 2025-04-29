c83de172e57e4439d1228b5a7af5e80fe5134b69
Mikael Kuisma <kuisma@dtu.dk>
2025-04-29 07:37:31 +0200

    move extensions to own file

---
+++ gpaw/new/extensions.py
@@ -0,0 +1,47 @@
+class ExtensionParameter:
+    def build(self, atoms):
+        raise NotImplementedError
+
+class Extension:
+    name = 'unnamed extension'
+
+    def __init__(self, atoms, domain_comm):
+        ...
+
+    def get_energy_contribution(self) -> float:
+        raise NotImplementedError
+
+    def force_contribution(self):
+        raise NotImplementedError
+
+    def move_atoms(self, atoms) -> None:
+        raise NotImplementedError
+
+"""
+class D3Extension:
+    def __init__(self, params, atoms):
+        self.params = params
+        self.atoms = atoms
+
+    def update_forces_postscf(self, F_av):
+        from ase.calculators.dftd3 import PureDFTD3
+        atoms = self.atoms.copy()
+        atoms.calc = PureDFTD3(xc=self.params.xc, **self.params.kwargs)
+        F_av += self.atoms.get_forces()
+
+    def update_energy_postscf(self, energies):
+        from ase.calculators.dftd3 import PureDFTD3
+        atoms = atoms.copy()
+        atoms.calc = PureDFTD3(xc=self.params.xc, **self.params.kwargs)
+        energies['D3'] += atoms.get_potential_energy()
+
+@register
+class D3(ExtensionParameter):
+    def __init__(self, *, xc, **kwargs):
+        self.xc = xc
+        self.kwargs = kwargs
+
+    def build(self, atoms):
+        return D3Extension(self, atoms)
+"""
+
--- gpaw/test/test_extensions.py
+++ gpaw/test/test_extensions.py
@@ -1,57 +1,8 @@
 import pytest
 from gpaw.new.input_parameters import register
+from gpaw.new.extensions import ExtensionParameter, Extension
 from ase.units import Hartree, Bohr
 import numpy as np
-
-
-class ExtensionParameter:
-    def build(self, atoms):
-        raise NotImplementedError
-
-
-"""
-class D3Extension:
-    def __init__(self, params, atoms):
-        self.params = params
-        self.atoms = atoms
-
-    def update_forces_postscf(self, F_av):
-        from ase.calculators.dftd3 import PureDFTD3
-        atoms = self.atoms.copy()
-        atoms.calc = PureDFTD3(xc=self.params.xc, **self.params.kwargs)
-        F_av += self.atoms.get_forces()
-
-    def update_energy_postscf(self, energies):
-        from ase.calculators.dftd3 import PureDFTD3
-        atoms = atoms.copy()
-        atoms.calc = PureDFTD3(xc=self.params.xc, **self.params.kwargs)
-        energies['D3'] += atoms.get_potential_energy()
-
-@register
-class D3(ExtensionParameter):
-    def __init__(self, *, xc, **kwargs):
-        self.xc = xc
-        self.kwargs = kwargs
-
-    def build(self, atoms):
-        return D3Extension(self, atoms)
-"""
-
-
-class Extension:
-    name = 'unnamed extension'
-
-    def __init__(self, atoms, domain_comm):
-        ...
-
-    def get_energy_contribution(self) -> float:
-        raise NotImplementedError
-
-    def force_contribution(self):
-        raise NotImplementedError
-
-    def move_atoms(self, atoms) -> None:
-        raise NotImplementedError
 
 
 @register
