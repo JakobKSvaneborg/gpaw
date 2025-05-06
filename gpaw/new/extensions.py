360783b9d4d82ecc0e7a6e4e529be9437d557a6c
Mikael Kuisma <kuisma@dtu.dk>
2025-05-06 14:10:03 +0200

    Various workarounds to ASE issues

--- gpaw/new/extensions.py
+++ gpaw/new/extensions.py
@@ -1,6 +1,11 @@
 from gpaw.new.input_parameters import register
 from ase.units import Hartree, Bohr
 from ase.calculators.calculator import PropertyNotImplementedError
+import numpy as np
+from gpaw.mpi import broadcast, serial_comm, broadcast_exception, broadcast_float
+import uuid
+from pathlib import Path
+import os
 
 
 class Extension:
@@ -30,26 +35,84 @@ class D3(ExtensionParameter):
     def todict(self) -> dict:
         return {'xc': self.xc, **self.kwargs}
 
-    def build(self, atoms, domain_comm):
+    def build(self, atoms, communicators):
         atoms = atoms.copy()
+        world = communicators['w']
+        from ase.calculators.dftd3 import PureDFTD3
+
+        # Since DFTD3 is filesystem based, and GPAW has no such requirements
+        # we need to be absolutely sure that there are no race-conditions
+        # in files. label cannot be used, because dftd3 executable still
+        # writes gradients to fixed files, thus a unique folder needs to be
+        # created.
 
         class D3Extension(Extension):
             def __init__(self):
                 super().__init__()
+                self.stress_vv = np.zeros((3, 3)) * np.nan
+                self.F_av = np.zeros_like(atoms.positions) * np.nan
+                self.E = np.nan
                 self._calculate(atoms)
 
             def _calculate(_self, atoms):
-                from ase.calculators.dftd3 import PureDFTD3
-                atoms.calc = PureDFTD3(xc=self.xc, **self.kwargs)
+                # Circumvent a DFTD3 bug for an isolated atom ASE #1672
+                if len(atoms) == 1 and not atoms.pbc.any():
+                    _self.stress_vv = np.zeros((3, 3)) * np.nan
+                    _self.F_av = np.zeros_like(atoms.positions)
+                    _self.E = 0.0
+                    return
+
+                cwd = Path.cwd()
+                assert atoms.calc is None
+                # Call DFTD3 only with single core due to #1671
+                with broadcast_exception(world):
+                    if world.rank == 0:
+                        try:
+                            _self.calculate_single_core()
+                        finally:
+                            os.chdir(cwd)
+                    _self.E = broadcast_float(_self.E)
+                    world.broadcast(_self.F_av)
+                    world.broadcast(_self.stress_vv)
+
+            def calculate_single_core(_self):
+                """Single core method to calculate D3 forces and stresses
+                
+                """
+                label = uuid.uuid4().hex[:8]
+                directory = Path('dftd3-ext-' + label).absolute()
+                directory.mkdir()
+
+                # Due to ase #1673, relative folders are not supported
+                # neither are absolute folders due to 80 character limit.
+                # The only way out, is to chdir to a temporary folder here.
+                os.chdir(directory)
+              
+                atoms.calc = PureDFTD3(xc=self.xc,
+                                       directory='.',
+                                       comm=serial_comm,
+                                       **self.kwargs)
+
                 # XXX params.xc should be taken directly from the calculator.
                 # XXX What if this is changed via set?
                 _self.F_av = atoms.get_forces() / Hartree * Bohr
+
                 try:
                     _self.stress_vv = atoms.get_stress(voigt=False) \
                         / Hartree * Bohr**3
                 except PropertyNotImplementedError as e:
-                    _self.stress_vv = e
+                    _self.stress_vv = np.zeros((3, 3)) * np.nan
+
                 _self.E = atoms.get_potential_energy() / Hartree
+                try:
+                    os.unlink('ase_dftd3.out')
+                    os.unlink('ase_dftd3.POSCAR')
+                    os.unlink('dftd3_cellgradient')
+                    os.unlink('dftd3_gradient')
+                    os.rmdir(directory.absolute())
+                except OSError as e:
+                    print(e)
+                atoms.calc = None 
 
             def get_energy_contributions(_self) -> dict[str, float]:
                 return {f'D3 (xc={self.xc})': _self.E}
@@ -58,8 +121,8 @@ class D3(ExtensionParameter):
                 return self.F_av
 
             def stress_contribution(self):
-                if isinstance(self.stress_vv, Exception):
-                    raise self.stress_vv
+                if np.isnan(self.stress_vv).all():
+                    raise PropertyNotImplementedError
                 return self.stress_vv
 
             def move_atoms(self, relpos_ac) -> None:
