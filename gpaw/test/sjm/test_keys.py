06a33fc0f6f8cd368a8d0d3f556ef6c3147410d0
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-05-08 15:47:13 +0200

    use pytest in the intended way (no __name__==__main__), use a fixture to unduplicate code

--- gpaw/test/sjm/test_keys.py
+++ gpaw/test/sjm/test_keys.py
@@ -1,44 +1,34 @@
 import pytest
-from .base_calc import calculator
-from ase.build import fcc111
 
 
 @pytest.mark.old_gpaw_only
 @pytest.mark.ci
 @pytest.mark.serial
-def test_keys():
-    atoms = fcc111('H', size=(1, 1, 1), a=2.5)
-    atoms.center(axis=2, vacuum=5)
-    atoms.cell[2][2] = 10
+def test_keys(atoms):
+    atoms.calc.set(
+        sj={'excess_electrons': 1.,
+            'jelliumregion': {'top': -2.,
+                              'bottom': -4.,
+                              'thickness': None,
+                              'fix_bottom': False},
+            'target_potential': 3,
+            'pot_ref': 'wf',
+            'tol': 0.01,
+            'always_adjust': True,
+            'grand_output': False,
+            'max_iters': 100,
+            'max_step': 3.,
+            'slope': 5,
+            'mixer': 1,
+            'fdt': False,
+            'slope_regression_depth': 4,
+            'dirichlet': False,
+            'cip': {'autoinner': {'nlayers': None,
+                                  'threshold': 0.0001},
+                    'inner_region': None,
+                    'mu_pzc': None,
+                    'phi_pzc': None,
+                    'filter': 10}})
 
-    calc = calculator()
-    calc.set(sj={'excess_electrons': 1.,
-                 'jelliumregion': {'top': -2.,
-                                   'bottom': -4.,
-                                   'thickness': None,
-                                   'fix_bottom': False},
-                 'target_potential': 3,
-                 'pot_ref': 'wf',
-                 'tol': 0.01,
-                 'always_adjust': True,
-                 'grand_output': False,
-                 'max_iters': 100,
-                 'max_step': 3.,
-                 'slope': 5,
-                 'mixer': 1,
-                 'fdt': False,
-                 'slope_regression_depth': 4,
-                 'dirichlet': False,
-                 'cip': {'autoinner': {'nlayers': None,
-                                       'threshold': 0.0001},
-                         'inner_region': None,
-                         'mu_pzc': None,
-                         'phi_pzc': None,
-                         'filter': 10}})
-    atoms.calc = calc
     atoms.calc.initialize(atoms)
     atoms.calc.initialize_positions(atoms)
-
-
-if __name__ == '__main__':
-    test_keys()
