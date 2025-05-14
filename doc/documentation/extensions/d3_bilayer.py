9064f9ac2bb0ab60562de9ec7b9e721a25e52eca
mnygp <markus.markusnp@gmail.com>
2025-05-14 18:16:37 +0200

    Added usage and external py file for example

---
+++ doc/documentation/extensions/d3_bilayer.py
@@ -0,0 +1,23 @@
+from gpaw.new.ase_interface import GPAW
+from gpaw import PW
+from gpaw.new.extensions import D3
+from ase.build import mx2
+
+MoS2 = mx2('MoS2', a=3.2)
+WSe2 = mx2('WSe2', a=3.2)
+
+# 6.6Å of initial distance between layers
+MoS2.positions[:, 2] += 3.3
+WSe2.positions[:, 2] -= 3.3
+
+bilayer = WSe2 + MoS2
+bilayer.center(vacuum=6.0, axis=2)
+bilayer.pbc = True
+
+calc = GPAW(mode=PW(400),
+            xc='PBE',
+            extensions=[D3(xc='PBE')],
+            txt='out.txt')
+
+bilayer.calc = calc
+energy = bilayer.get_potential_energy()
