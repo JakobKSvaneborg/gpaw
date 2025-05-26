75140491b524d03785a5daa089fdc338a8e34525
Amin Akhound <aminakhound@gmail.com>
2025-05-26 11:12:23 +0200

    write a new test

---
+++ gpaw/test/noncollinear/test_GGA.py
@@ -0,0 +1,17 @@
+import pytest
+from ase import Atoms
+
+from gpaw import GPAW
+from gpaw.mpi import size
+
+
+def test_noncollinear_o2(in_tmp_dir, gpaw_new):
+    a = Atoms('H', [[0, 0, 0]], magmoms=[1])
+    a.center(vacuum=2.5)
+    a.calc = GPAW(mode='pw',
+                  xc = 'PBE',
+                  symmetry = 'off',
+                  experimental={'magmoms': [[0, 0.5, 0.5]]})
+    with pytest.raises(ValueError):
+        a.get_potential_energy()
+
