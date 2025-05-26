f14e3095c08af1feb4543b9f0ce595c9edcfdc6e
Mikael Kuisma <kuisma@dtu.dk>
2025-05-26 11:29:36 +0200

    separate systems to systems.py, add more carbon to the test suite:D

--- gpaw/benchmark/__init__.py
+++ gpaw/benchmark/__init__.py
@@ -7,6 +7,8 @@ from time import time
 from json import dumps
 from pathlib import Path
 
+from gpaw.benchmarks.systems import parse_system
+
 # A parameter set is a 2-tuple with dictionary for gpaw-parameters,
 # and additional dictionary with named sub parameter sets
 
@@ -21,83 +23,35 @@ lcao_default_parameters = {'mode': {'name': 'lcao'}}
 lcao_parameter_subsets = {'sz': {'basis': 'sz(dzp)'},
                           'dzp': {'basis': 'dzp'}}
 
-general_parameter_sets = {'gamma': ({'kpts': (1, 1, 1)}, {}),
-                          'kpts': ({'kpts': {'density': 6}}, {}),
-                          'parallel': ({},
-                                       {'scalapack':
-                                        {'parallel': {'sl_auto': True}}})}
+kpts_parameter_sets = {'gamma': {'kpts': (1, 1, 1)},
+                       'density6': {'kpts': {'density': 6}},
+                       '411': ({'kpts': (4, 1, 1)})}
+
+xc_parameters_sets = {'PBE': {'xc': 'PBE'},
+                      'LDA': {'xc': 'LDA'}}
 
 gpu_default_parameters = {'parallel': {'gpu': True}, 'random': True}
+
+parallel_parameters_sets =  {'parallel': {'scalapack':
+                                         {'parallel': {'sl_auto': True}}}
 
 gpaw_parameter_sets = {'pw': (pw_default_parameters, pw_parameter_subsets),
                        'lcao': (lcao_default_parameters,
                                 lcao_parameter_subsets),
+                       'kpts': ({}, kpts_parameter_sets),
                        'gpu': (gpu_default_parameters, {}),
+                       'xc': ({}, xc_parameter_sets),
+                       'parallel': ({}, parallel_parameter_sets),
                        **general_parameter_sets}
-
-
-def system_magic_graphene():
-    from gpaw.benchmark.generate_twisted import make_heterostructure
-    from ase.build import graphene
-    atoms = graphene(vacuum=5)
-    transa_cc = np.array([[29, -30, 0], [59, 29, 0], [0, 0, 1]])
-    transb_cc = np.array([[30, -29, 0], [59, 30, 0], [0, 0, 1]])
-    atoms = make_heterostructure(atoms, atoms,
-                                 transa_cc=transa_cc,
-                                 transb_cc=transb_cc,
-                                 straina_vv=np.eye(3),
-                                 interlayer_dist=3.35)
-    return atoms
-
-
-def system_6000_bl_graphene():
-    from gpaw.benchmark.generate_twisted import make_heterostructure
-    from ase.build import graphene
-    atoms = graphene(vacuum=5)
-    transa_cc = np.array([[23, 45, 0], [-22, 23, 0], [0, 0, 1]])
-    transb_cc = np.array([[22, 45, 0], [-23, 22, 0], [0, 0, 1]])
-    atoms = make_heterostructure(atoms, atoms,
-                                 transa_cc=transa_cc,
-                                 transb_cc=transb_cc,
-                                 straina_vv=np.eye(3),
-                                 interlayer_dist=3.35)
-    return atoms
-
-
-def system_H2():
-    from ase.build import molecule
-    atoms = molecule('H2')
-    atoms.center(vacuum=3)
-    return atoms
-
-
-def system_C60():
-    from ase.build import molecule
-    atoms = molecule('C60')
-    atoms.center(vacuum=5)
-    return atoms
-
-
-def system_diamond():
-    from ase.build import bulk
-    atoms = bulk('C')
-    return atoms
-
-
-systems = {'C60': system_C60,
-           'diamond': system_diamond,
-           'H2': system_H2}
 
 
 benchmarks = {'C60_pw': 'C60-pw.high:gamma',
               'C60_lcao': 'C60-lcao.dzp',
               'C60_lowpw_gpu': 'C60-pw.low:gamma:gpu',
               'C60_lowpw_float_gpu': 'C60-pw.low.float32:gamma:gpu',
+              'MoS2_tube': 'MoS2_tube-pw.high:kpts.411:xc.PBE:parallel.scalapack',
+              '676_graphene': '676_graphene-pw:kpts.gamma:xc.PBE:parallel.scalapack',
               'diamond_pw': 'diamond-pw.high:kpts'}
-
-
-def parse_system(name):
-    return systems[name]()
 
 
 def recursive_update(d, u):
