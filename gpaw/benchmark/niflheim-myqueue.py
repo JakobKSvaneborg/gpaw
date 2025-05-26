35b1586cd1479807ca0be479e5175e5756068535
Mikkel Ohm Sauer <mikhik@yahoo.dk>
2025-05-26 17:06:34 +0200

    Make sub-directories

--- gpaw/benchmark/niflheim-myqueue.py
+++ gpaw/benchmark/niflheim-myqueue.py
@@ -1,6 +1,7 @@
 from myqueue.workflow import run
 
 from gpaw.benchmark import benchmark_main, get_benchmarks
+from os import makedirs
 
 platforms = [('xeon24el8_test', 24, 24, 0, '10m'),
              ('a100', 8, 1, 1, '10m'),
@@ -13,7 +14,9 @@ def workflow():
     for partition, ncores, nprocs, ngpus, time in platforms:
         for benchmark in get_benchmarks(
                 cores=nprocs, memory='10000G', gpus=ngpus):
+            makedirs(f'./{partition}-{nprocs}', exist_ok=True)
             run(function=benchmark_main, args=(benchmark,),
                 nodename=partition, cores=ncores, processes=nprocs,
                 gpus=ngpus, tmax=time,
-                name=f'{benchmark}-{partition}-{nprocs}')
+                name=f'{benchmark}-{partition}-{nprocs}',
+                folder=f'{partition}-{nprocs}')
