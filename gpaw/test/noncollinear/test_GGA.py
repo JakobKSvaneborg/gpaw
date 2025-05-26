ae699c441814e049ecd792fbc9ac08571c43ab22
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-05-26 11:15:42 +0200

    Turn off text output

--- gpaw/test/noncollinear/test_GGA.py
+++ gpaw/test/noncollinear/test_GGA.py
@@ -5,13 +5,13 @@ from gpaw import GPAW
 from gpaw.mpi import size
 
 
-def test_noncollinear_o2(in_tmp_dir, gpaw_new):
+def test_noncollinear_o2(gpaw_new):
     a = Atoms('H', [[0, 0, 0]], magmoms=[1])
     a.center(vacuum=2.5)
     a.calc = GPAW(mode='pw',
-                  xc = 'PBE',
-                  symmetry = 'off',
+                  xc='PBE',
+                  symmetry='off',
+                  txt=None,
                   experimental={'magmoms': [[0, 0.5, 0.5]]})
     with pytest.raises(ValueError):
         a.get_potential_energy()
-
