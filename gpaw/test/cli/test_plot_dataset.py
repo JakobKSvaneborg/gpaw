cf43e2a74376b593a86210693ad5aa5528a238d0
Terence S.-C. Tsang <t.tsang.sci@gmail.com>
2025-05-27 15:31:24 +0200

    Refactored `gpaw/test/cli/test_plot_dataset.py`
    gpaw/test/cli/test_plot_dataset.py
        - Moved computations in the module-global namespace into fixtures
          to avoid potential test-collection failures
        - Added separate fixtures `new_setup()` (resp. `old_setup()`) to
          test cases where the setup file has (resp. doesn't have) enough
          `.generatordata` for reconstructing the `PAWSetupGenerator`
        - Refactored `test_gpaw_plot_dataset()`:
          - Changed call signature
          - Now using `request` to load the aforementioned fixtures
          - Cleaned up the construction of the executed command lines to be
            more readable

--- gpaw/test/cli/test_plot_dataset.py
+++ gpaw/test/cli/test_plot_dataset.py
@@ -1,9 +1,11 @@
 import argparse
 import contextlib
+import dataclasses
 import os
 import shlex
 import subprocess
 import sys
+from typing import ContextManager
 
 import pytest
 
@@ -11,47 +13,79 @@ from gpaw.setup_data import search_for_file
 from gpaw.atom.plot_dataset import CLICommand, read_setup_file
 
 
-setup_file = 'Ti.LDA'
-installed_setup = read_setup_file(search_for_file(setup_file)[0])
-if '=' in installed_setup.generatordata:
-    full_fig_nplots = 4
-    run_main_ctx = None
-else:
-    # Legacy setup files, no info on which to reconstruct the generator
-    # -> cannot plot the log derivatives
-    full_fig_nplots = 3
-    run_main_ctx = pytest.warns(match='cannot reconstruct')
+@dataclasses.dataclass
+class SetupInfo:
+    filename: str
+    nplots: int
+    ctx: ContextManager = dataclasses.field(
+        default_factory=contextlib.nullcontext)
+
+
+@pytest.fixture(scope='module')
+def old_setup():
+    """
+    Legacy setup file, no info on which to reconstruct the generator
+    -> cannot plot the log derivatives, expect warnings
+    """
+    setup_file = 'Ti.LDA'
+    installed_setup = read_setup_file(search_for_file(setup_file)[0])
+    assert ('=' not in installed_setup.generatordata), (
+        f'Setup {setup_file!r} has been updated to include more '
+        'generator data, update the test')
+    return SetupInfo(
+        setup_file, 3, pytest.warns(match='cannot reconstruct'))
+
+
+@pytest.fixture(scope='module')
+def new_setup():
+    """
+    New setup file, reconstruction possible -> all plots can be plotted
+    """
+    setup_file = 'Cr.14.LDA'
+    installed_setup = read_setup_file(search_for_file(setup_file)[0])
+    assert ('=' in installed_setup.generatordata), (
+        f'Setup {setup_file!r} does\'t have the expected generator data')
+    return SetupInfo(setup_file, 4)
 
 
 @pytest.mark.serial
+@pytest.mark.parametrize('setup', ['old_setup', 'new_setup'])
 @pytest.mark.parametrize(
-    ('flags', 'search', 'use_cli', 'expected_nplots', 'ctx'),
-    [('', False, False, 2, None),  # Minimal plot
-     ('-s', False, False, 2, None),  # --separate-figures (ignored)
+    ('flags', 'search', 'use_cli', 'expected_nplots'),
+    [('', False, False, 2),  # Minimal plot
+     ('-s', False, False, 2),  # --separate-figures (ignored)
      ('-p -l spd,-1:1:.05',  # Reconstruct gen and make the full fig
-      False, False, full_fig_nplots, run_main_ctx),
+      False, False, None),
      ('-p -l spd,-1:1:.05',  # Same as the above, but...
       True, False,  # ... also test the dataset file searching
-      full_fig_nplots, run_main_ctx),
+      None),
      ('-p -l spd,-1:1:.05',  # Same as the above, but...
       True, True,  # ... also test the CLI
-      full_fig_nplots, None)])  # Running in subproc, no warnings
+      None)])  # Running in subproc, no warnings
 def test_gpaw_plot_dataset(
-        flags, search, use_cli, expected_nplots, ctx, in_tmp_dir):
+        setup, flags, search, use_cli, expected_nplots, request, in_tmp_dir):
     """
-    Test for `gpaw plot-dataset`.
+    Test for `gpaw plot-dataset`. For the cases where
+    `expected_nplots = None`, try to make as many subplots as the
+    dataset permits, potentially emitting a warning.
     """
+    info = request.getfixturevalue(setup)
+    setup_file = info.filename
     old_files = set(os.listdir(os.curdir))
     outfile = 'output.png'
     expected_files = {outfile}
-
-    if use_cli:
-        argv = [sys.executable, '-m', 'gpaw', '-T', 'plot-dataset']
+    if expected_nplots is None:
+        # Plot as many plots as possible, which may result in warnings
+        # (to be caught by `info.ctx`)
+        expected_nplots = info.nplots
+        ctx = info.ctx
     else:
-        argv = []
-    argv.extend([f'--write={outfile}', *shlex.split(flags), setup_file])
+        # Plot a definite number of plots, expect no warnings
+        ctx = contextlib.nullcontext()
+
+    args = [f'--write={outfile}', *shlex.split(flags)]
     if search:
-        argv.insert(-1, '--search')
+        args.append('--search')
     else:
         _, content = search_for_file(setup_file)
         with open(setup_file, mode='wb') as fobj:
@@ -59,13 +93,16 @@ def test_gpaw_plot_dataset(
             # `search_for_file()` because it may be zipped
             fobj.write(content)
         expected_files.add(setup_file)
+    args.append(setup_file)
+
     if use_cli:
-        subprocess.check_call(argv)
+        gpaw_plot_cmd = [sys.executable, '-m', 'gpaw', '-T', 'plot-dataset']
+        subprocess.check_call(gpaw_plot_cmd + args)
     else:
         parser = argparse.ArgumentParser()
         CLICommand.add_arguments(parser)
-        with contextlib.nullcontext() if ctx is None else ctx:
-            axs = CLICommand.run(parser.parse_args(argv))
+        with ctx:
+            axs = CLICommand.run(parser.parse_args(args))
         # All plots should be on the same figure
         assert len({id(ax.get_figure()) for ax in axs}) == 1
 
