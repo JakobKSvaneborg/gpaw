fc9821ed87d1b8cafa730f449bc97db8fa4792c1
Jens Jørgen Mortensen <jj@smoerhul.dk>
2025-06-17 07:57:13 +0200

    Fix X-ticks + add GPU info

--- doc/benchmarks/table.py
+++ doc/benchmarks/table.py
@@ -13,9 +13,13 @@ for path in Path().glob('*.json'):
         versions.append(version)
         for d in json.loads(path.read_text()):
             if d['calcinfo'] == age:
-                id = (d['longname'],
+                cpus = str(d['mpi-ranks'])
+                if d['mpi-ranks'] <= 4:
+                    cpus += 'G'
+                id = (d['shortname'],
+                      d['longname'],
                       d['processor'],
-                      str(d['mpi-ranks']))
+                      cpus)
                 data[id][version] = (d['First step'],
                                      d['Second step'],
                                      d['max_rss'])
@@ -25,15 +29,15 @@ rows = [['number',
          'step 1 [s]', 'step 2 [s]', 'max rss [GB]']]
 for i, id in enumerate(sorted(data)):
     d = data[id]
-    row = [str(i)] + list(id)
-    print(id)
-    print(d)
+    row = [str(i)] + list(id[1:])
     t1, t2, m = d['25.1.0-new']
     row += [f'{t1:.0f}', f'{t2:.0f}', f'{m * 1e-9:.1f}']
     rows.append(row)
 Path('table.csv').write_text(
     '\n'.join(', '.join(row) for row in rows) + '\n')
 
+X = [id[0] + ((' ' + id[3]) if id[3].endswith('G') else '')
+     for id in sorted(data)]
 fig, axs = plt.subplots(3, 1, sharex=True, figsize=(8, 8))
 for n, (name, ax) in enumerate(zip(['First step',
                                     'Second step',
@@ -51,6 +55,8 @@ for n, (name, ax) in enumerate(zip(['First step',
         ax.plot(Y, 'x-', label=version)
     ax.set_ylabel(name + ' [%]')
     if n == 2:
+        ax.set_xticks(range(len(X)), X, rotation=60, ha='right')
         ax.legend()
-plt.savefig('table.png')
+
+# plt.savefig('table.png')
 plt.show()
