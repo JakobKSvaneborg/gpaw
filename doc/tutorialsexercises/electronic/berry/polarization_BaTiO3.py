fc4fe9f818d14fe8838ce7cc68623db4189cc3f8
Silvan Kretschmer <silvan_kretschmer@gmx.de>
2025-06-19 14:50:23 +0200

    Refactored polarization_BaTiO3.py

--- doc/tutorialsexercises/electronic/berry/polarization_BaTiO3.py
+++ doc/tutorialsexercises/electronic/berry/polarization_BaTiO3.py
@@ -2,19 +2,21 @@ import numpy as np
 from ase.units import _e
 from ase.parallel import paropen
 from gpaw import GPAW
+from gpaw.mpi import serial_comm
 from gpaw.berryphase import polarization_phase
 from pathlib import Path
 
-gpw_wfs = Path('BaTiO3+wfs.gpw')
+gpw_gs = Path("BaTiO3.gpw")
+gpw_wfs = Path("BaTiO3+wfs.gpw")
 
-# Create gpw-file with wave functions for all k-points in the BZ:
-calc = GPAW('BaTiO3.gpw').fixed_density(symmetry='off')
-calc.write(gpw_wfs, mode='all')
+# create gpw-file with wave functions for all k-points in the BZ
+calc = GPAW(gpw_gs).fixed_density(symmetry="off")
+calc.write(gpw_wfs, mode="all")
 
-phases_c = polarization_phase(gpw_wfs)
-phi_c = phases_c['phase_c']
+phases_c = polarization_phase(gpw_wfs, comm=serial_comm)
+phi_c = phases_c["phase_c"]
 
-cell_cv = calc.atoms.cell * 1e-10      # in m
+cell_cv = calc.atoms.cell * 1e-10  # in m
 vol = calc.atoms.get_volume() * 1e-30  # in m^3
 
 # phase defined modulo 2 pi
@@ -23,5 +25,5 @@ phi_c -= (phi_c / pi2) * pi2
 
 # polarization in C/m^2
 px, py, pz = phi_c @ cell_cv / vol * _e
-with paropen('polarization_BaTiO3.out', 'w') as fd:
-    fd.wrie(f'P: {pz} C/m^2\n')
+with paropen("polarization_BaTiO3.out", "w") as fd:
+    fd.write(f"P: {pz} C/m^2\n")
