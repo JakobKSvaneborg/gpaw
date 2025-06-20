e4305e833cc9aacd70ac7f453b76732938a3b285
Silvan Kretschmer <silvan_kretschmer@gmx.de>
2025-06-20 18:46:35 +0200

    Modified get_borncharges.py script to display all the available information

--- doc/tutorialsexercises/electronic/berry/get_borncharges.py
+++ doc/tutorialsexercises/electronic/berry/get_borncharges.py
@@ -1,7 +1,10 @@
 from ase.io.jsonio import read_json
 import numpy as np
 
-Z_avv = read_json('born_charges.json')['Z_avv']
-for a, Z_vv in enumerate(Z_avv):
-    print(a)
+results = read_json('born_charges.json')
+Z_avv = results['Z_avv']
+sym_a = results['sym_a']
+
+for sym, Z_vv in zip(sym_a, Z_avv):
+    print(sym)
     print(np.round(Z_vv, 2))
