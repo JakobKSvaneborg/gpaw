662583dcf68ba7e212f4f0f80d4582cb60e0901b
SianK <silkr@dtu.dk>
2025-06-20 20:24:12 +0200

    Implemented write_csv_file

--- doc/tutorialsexercises/electronic/berry/get_borncharges.py
+++ doc/tutorialsexercises/electronic/berry/get_borncharges.py
@@ -16,13 +16,28 @@ for sym, Z_vv in zip(sym_a, Z_avv):
 csv_file = 'born_charges_BaTiO3.csv'
 
 
-def write_csv(csv_file, Z_avv, sym_a):
+def write_csv(csv_file, Z_avv, sym_a, tol=1e-2):
     from ase.parallel import paropen
 
+    table = []
+    for sym, Z_vv in zip(sym_a, Z_avv):
+        subtable = []
+        for row in Z_vv:
+            trow = ['']
+            for cell in row:
+                if np.abs(cell) > tol:
+                    tcell = f'{cell:4.2f}'
+                else:
+                    tcell = '0'
+                trow.append(tcell)
+            subtable.append(trow)
+        table.append(subtable)
+
+    table = np.transpose(np.array(table), (1, 0, 2))
+    table = table.reshape((3, -1))
     with paropen(csv_file, 'w') as fd:
-        for sym, Z_vv in zip(sym_a, Z_avv):
-            for row in Z_vv:
-                fd.write(' ,{:4.2f},{:4.2f},{:4.2f}\n'.format(*row))
+        for row in table:
+            fd.write(','.join(row) + '\n')
 
 
 write_csv(csv_file, Z_avv, sym_a)
