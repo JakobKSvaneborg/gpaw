fc4596211c3fc52e18f680181bfd6c7cf1eb292c
SianK <silkr@dtu.dk>
2025-06-23 13:11:33 +0200

    Updated doc born charges

--- doc/tutorialsexercises/electronic/berry/born_BaTiO3.py
+++ doc/tutorialsexercises/electronic/berry/born_BaTiO3.py
@@ -2,7 +2,7 @@ from gpaw import GPAW
 from pathlib import Path
 from gpaw.borncharges import born_charges_wf
 
-gpw_file = Path('BaTiO3.gpw')
+gpw_file = Path('BaTiO3+wfs.gpw')
 calc = GPAW(gpw_file, txt=None)
 atoms = calc.get_atoms()
 
