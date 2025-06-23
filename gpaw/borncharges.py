f806c91603c7b70149e3c32afffc60ebb17bc4d2
Silvan Kretschmer <silvan_kretschmer@gmx.de>
2025-06-23 12:53:30 +0200

    Removed main() MoS2 example from borncharges

--- gpaw/borncharges.py
+++ gpaw/borncharges.py
@@ -179,37 +179,3 @@ def distance_to_non_pbc_boundary(atoms):
 
 class AtomsTooCloseToBoundary(Exception):
     pass
-
-
-def main():
-    # example workflow
-
-    from gpaw import GPAW
-    from ase.build import mx2
-
-    calc_params = {
-        'mode': {'name': 'pw', 'ecut': 400},
-        'xc': 'PBE',
-        'kpts': {'density': 3.0},
-        'occupations': {'name': 'fermi-dirac', 'width': 0.05},
-        'symmetry': 'off',
-        'convergence': {'density': 1e-4},
-    }
-
-    atoms = mx2('MoS2', vacuum=5.0)
-    atoms.center()
-
-    gpw_file = Path('MoS2.gpw')
-
-    calc = GPAW(**calc_params, txt='gs.txt')
-    atoms.calc = calc
-    atoms.get_potential_energy()
-    atoms.calc.write(gpw_file, mode='all')
-
-    results = born_charges_wf(atoms, gpw_file=gpw_file, cleanup=True)
-
-    parprint(results)
-
-
-if __name__ == "__main__":
-    main()
