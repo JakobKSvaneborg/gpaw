45026d350e9763a76b245ec890c0673f8a789682
Amalie Sondersted <amhso@dtu.dk>
2025-06-25 09:13:05 +0200

    bse+ calculation on tio2 for tutorial

---
+++ doc/tutorialsexercises/opticalresponse/bse_plus_tutorial/bsep_calc.py
@@ -0,0 +1,43 @@
+from gpaw import GPAW
+import numpy as np
+from gpaw.response.chi0 import get_frequency_descriptor
+from gpaw.response.pair import get_gs_and_context
+from gpaw.response.bse import BSE_Plus
+from gpaw.mpi import world
+from ase.dft.bandgap import bandgap
+
+calc_bse = 'fixed_density_calc_BSE_Plus.gpw'
+calc_rpa = 'fixed_density_calc_rpa.gpw'
+
+gs, context = get_gs_and_context(calc_rpa, txt=None, world=world, timer=None)
+ecut = 80
+eta = 0.1
+q_c = [0.0, 0.0, 0.0]
+bse_valence_bands = range(18, 24)
+bse_conduction_bands = range(24, 30)
+bse_nbands = 60
+rpa_nbands = 130
+w_w = np.linspace(0, 50, 5001)
+wd = get_frequency_descriptor(w_w, gs=gs)
+
+gap, _, _ = bandgap(GPAW(calc_rpa), direct=True)
+eshift = 3.3 - gap
+
+bse_plus = BSE_Plus(bse_gpw=calc_bse,
+                    bse_valence_bands=bse_valence_bands,
+                    bse_conduction_bands=bse_conduction_bands,
+                    bse_nbands=bse_nbands,
+                    rpa_gpw=calc_rpa,
+                    rpa_nbands=rpa_nbands,
+                    w_w=w_w,
+                    eshift=eshift,
+                    eta=eta,
+                    q_c=q_c,
+                    ecut=ecut)
+
+bse_plus.get_chi_wGG(optical=True,
+                     chi_BSE=True,
+                     chi_RPA=True,
+                     bsep_name='chi_BSE_Plus',
+                     bse_name='chi_BSE',
+                     rpa_name='chi_RPA')
