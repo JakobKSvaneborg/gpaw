8502dcca1fafe2804e67ffd5afd5ed2d22dbc7a0
Amalie Sondersted <amhso@dtu.dk>
2025-06-25 09:25:00 +0200

    some bugs

--- doc/tutorialsexercises/opticalresponse/bse_plus_tutorial/submit.agts.py
+++ doc/tutorialsexercises/opticalresponse/bse_plus_tutorial/submit.agts.py
@@ -2,8 +2,8 @@ from myqueue.workflow import run
 
 
 def workflow():
-    with run(script='gs_Si.py', cores=4, tmax='20m'):
-        run(script='bse_plus_Si.py', cores=40, tmax='1h')
+    with run(script='gs_TiO2.py', cores=80, tmax='20m'):
+        run(script='bse_plus_TiO2.py', cores=80, tmax='4h')
 
-    with run(script='gs_MoS2.py', cores=4, tmax='20m'):
-        run(script='bse_plus_MoS2.py', cores=40, tmax='1h')
+   # with run(script='gs_MoS2.py', cores=4, tmax='20m'):
+   #     run(script='bse_plus_MoS2.py', cores=40, tmax='1h')
