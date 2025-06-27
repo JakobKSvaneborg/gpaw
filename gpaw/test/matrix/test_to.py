e23c413622f8fb11b252323270310641a3da8961
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-06-27 11:53:47 +0200

    Add as experimental feature + finish test

--- gpaw/test/matrix/test_to.py
+++ gpaw/test/matrix/test_to.py
@@ -1,15 +1,17 @@
 import pytest
 from gpaw.core.matrix import Matrix
-from gpaw.mpi import broadcast_exception, world
+from gpaw.mpi import world
 import numpy as np
 import gpaw.gpu.cupy as cp
 
 
-@pytest.mark.parametrize('xp', [np, cp]):
+@pytest.mark.parametrize('xp', [np, cp])
 def test_to_xp_dtype(xp):
     N = 21
     m1 = Matrix(N, N, dist=world)
+    m1.data[:] = world.rank
     xp2 = np if xp is cp else cp
-    m2 = m1.to_xp(xp2).to_xp(xp2)
+    m2 = m1.to_xp(xp2).to_xp(xp)
+    assert (m2.data == m1.data).all()
     m3 = m1.to_dtype(np.float32).to_dtype(float)
-    assert ...
+    assert (m3.data == m1.data).all()
