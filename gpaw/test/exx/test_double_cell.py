2cd799196a876aacb0182e95eb95147e6c0137d5
Jens Jørgen Mortensen <jj@smoerhul.dk>
2025-07-01 10:28:46 +0200

    Enable double-cell test

--- gpaw/test/exx/test_double_cell.py
+++ gpaw/test/exx/test_double_cell.py
@@ -5,7 +5,8 @@ from gpaw import GPAW, PW
 
 @pytest.mark.libxc
 @pytest.mark.hybrids
-def test_exx_double_cell(in_tmp_dir):
+@pytest.mark.new_gpaw_ready
+def test_exx_double_cell(in_tmp_dir, gpaw_new):
     L = 2.6
     a = Atoms('H2',
               [[0, 0, 0], [0.5, 0.5, 0]],
@@ -13,18 +14,26 @@ def test_exx_double_cell(in_tmp_dir):
               pbc=1)
     a.center()
 
-    a.calc = GPAW(
+    kwargs = dict(
         mode=PW(400),
         symmetry='off',
-        kpts={'size': (1, 1, 4), 'gamma': True},
         convergence={'density': 1e-6},
         spinpol=True,
-        txt='H2.txt',
         xc='HSE06')
+    if gpaw_new:
+        kwargs |= dict(spinpol=False,
+                       setups='ae',
+                       eigensolver='rmm-diis')
+
+    a.calc = GPAW(
+        kpts={'size': (1, 1, 4), 'gamma': True},
+        txt='H2.txt',
+        **kwargs)
     e1 = a.get_potential_energy()
     eps1 = a.calc.get_eigenvalues(1)[0]
-    f1 = a.get_forces()
-    assert abs(f1[1, 0] - 9.60644) < 0.0005
+    if not gpaw_new:
+        f1 = a.get_forces()
+        assert abs(f1[1, 0] - 9.60644) < 0.0005
     if 0:
         # To check against numeric calculation of the forces, but it takes
         # more time
@@ -34,22 +43,20 @@ def test_exx_double_cell(in_tmp_dir):
 
     a *= (1, 1, 2)
     a.calc = GPAW(
-        mode=PW(400),
         kpts={'size': (1, 1, 2), 'gamma': True},
-        symmetry='off',
-        convergence={'density': 1e-6},
         txt='H4.txt',
-        xc='HSE06')
+        **kwargs)
     e2 = a.get_potential_energy()
     eps2 = a.calc.get_eigenvalues(0)[0]
-    f2 = a.get_forces()
+    if not gpaw_new:
+        f2 = a.get_forces()
 
-    f2[:2] -= f1
-    f2[2:] -= f1
+        f2[:2] -= f1
+        f2[2:] -= f1
+        assert abs(f2).max() < 0.00085
 
     assert abs(e2 - 2 * e1) < 0.002
     assert abs(eps1 - eps2) < 0.001
-    assert abs(f2).max() < 0.00085
 
 
 if __name__ == '__main__':
