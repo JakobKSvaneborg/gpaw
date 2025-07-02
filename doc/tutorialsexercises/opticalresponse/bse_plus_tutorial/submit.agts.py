73bd22e32c89f7f9595e31371054b4fc7bb6d1f9
Amalie Sondersted <amhso@dtu.dk>
2025-07-02 14:45:23 +0200

    corrected workflow

--- doc/tutorialsexercises/opticalresponse/bse_plus_tutorial/submit.agts.py
+++ doc/tutorialsexercises/opticalresponse/bse_plus_tutorial/submit.agts.py
@@ -2,10 +2,12 @@ from myqueue.workflow import run
 
 
 def workflow():
-    with run(script='gs_TiO2.py', cores=80, tmax='1h'):
-        run(script='bse_plus_TiO2.py', cores=80, tmax='4h')
-        run(script='plot_TiO2.py', cores=1, tmax='1h')
+    with run(script='gs_TiO2.py', cores=80, tmax='2h'):
+        with run(script='bse_plus_TiO2.py', cores=80, tmax='4h'):
+            run(script='plot_TiO2.py', cores=1, tmax='1h')
+            run(script='test_TiO2.py')
 
-    with run(script='gs_MoS2.py', cores=80, tmax='1h'):
-        run(script='bse_plus_MoS2.py', cores=80, tmax='4h')
-        run(script='plot_MoS2.py', cores=1, tmax='1h')
+    with run(script='gs_MoS2.py', cores=80, tmax='2h'):
+        with run(script='bse_plus_MoS2.py', cores=80, tmax='4h'):
+            run(script='plot_MoS2.py', cores=1, tmax='1h')
+            run(script='test_MoS2.py')
