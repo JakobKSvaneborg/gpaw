abe76f40f1a923f9326629c152df3cf4aa3958f1
Amalie Sondersted <amhso@dtu.dk>
2025-07-07 11:09:48 +0200

    changed refractive index function

--- doc/tutorialsexercises/opticalresponse/bseplus_tutorial/plot_TiO2.py
+++ doc/tutorialsexercises/opticalresponse/bseplus_tutorial/plot_TiO2.py
@@ -2,13 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def refractive_index(Realpart, Impart):
-    n = (np.sqrt(np.pi * 4 * (Realpart + Impart * 1j) + 1)).real
-    return n
+def refractive_index(chi):
+    eps = 1 + np.pi * 4 * chi
+    eps1 = eps.real
+    eps2 = eps.imag
+    N = np.sqrt(0.5 * (np.sqrt(eps1**2 + eps2**2) + eps1))
+    return N
 
 
-def eels(chi_real, chi_im):
-    eps = np.pi * 4 * (chi_real.real + chi_im * 1j) + 1
+def eels(chi):
+    eps = np.pi * 4 * chi + 1
     return (-eps**(-1)).imag
 
 
@@ -21,9 +24,9 @@ exp_data = np.loadtxt('tio2_n_rutile_inplane.csv', delimiter=',')
 freq = exp_data[:, 0]
 exp_n = exp_data[:, 1]
 
-n_bsep = refractive_index(-(chi_bsep[:, 0, 0]).real, -(chi_bsep[:, 0, 0]).imag)
-n_bse = refractive_index(-(chi_bse[:, 0, 0]).real, -(chi_bse[:, 0, 0]).imag)
-n_rpa = refractive_index(-(chi_rpa[:, 0, 0]).real, -(chi_rpa[:, 0, 0]).imag)
+n_bsep = refractive_index(-chi_bsep[:, 0, 0])
+n_bse = refractive_index(-chi_bse[:, 0, 0])
+n_rpa = refractive_index(-chi_rpa[:, 0, 0])
 
 plt.plot(x, n_bsep, label='BSE+')
 plt.plot(x, n_bse, label='BSE')
@@ -38,9 +41,9 @@ plt.ylim(0, 5)
 plt.savefig('n_TiO2.png')
 plt.close()
 
-eels_bsep = eels(-(chi_bsep[:, 0, 0]).real, -(chi_bsep[:, 0, 0]).imag)
-eels_bse = eels(-(chi_bse[:, 0, 0]).real, -(chi_bse[:, 0, 0]).imag)
-eels_rpa = eels(-(chi_rpa[:, 0, 0]).real, -(chi_rpa[:, 0, 0]).imag)
+eels_bsep = eels(-chi_bsep[:, 0, 0])
+eels_bse = eels(-chi_bse[:, 0, 0])
+eels_rpa = eels(-chi_rpa[:, 0, 0])
 exp_data = np.loadtxt('eels_tio2_rutile.csv', delimiter=',')
 freq = exp_data[:, 0]
 exp_eels = exp_data[:, 1]
