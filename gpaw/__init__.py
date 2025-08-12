09cce5cd50a63b78580eeb12e0a0d5eeb8d1123d
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-08-12 15:57:04 +0200

    Introduce GPAW_DEBUG envvar

--- gpaw/__init__.py
+++ gpaw/__init__.py
@@ -28,7 +28,8 @@ boolean_envvars = {
     'GPAW_USE_GPUS',
     'GPAW_TRACE',
     'GPAW_NO_C_EXTENSION',
-    'GPAW_MPI4PY'}
+    'GPAW_MPI4PY',
+    'GPAW_DEBUG'}
 allowed_envvars = {
     *boolean_envvars,
     'GPAW_MPI_OPTIONS',
@@ -38,15 +39,17 @@ allowed_envvars = {
 is_gpaw_python = '_gpaw' in sys.builtin_module_names
 dry_run = 0
 
-# When type-checking or running pytest, we want the debug-wrappers enabled:
-debug: bool = (TYPE_CHECKING or
-               'pytest' in sys.modules or
-               bool(sys.flags.debug))
 
-if debug:
-    for var in os.environ:
-        if var.startswith('GPAW') and var not in allowed_envvars:
-            warnings.warn(f'Unknown GPAW environment varable: {var}')
+def _get_gpaw_env_vars(attr: str) -> bool | str:
+    if attr in boolean_envvars:
+        return bool(int(os.environ.get(attr) or 0))
+    if attr in allowed_envvars and attr in os.environ:
+        return os.environ[attr]
+    raise _module_attr_error(attr)
+
+
+# When type-checking, we want the debug-wrappers enabled:
+debug = TYPE_CHECKING or _get_gpaw_env_vars('GPAW_DEBUG')
 
 
 @contextlib.contextmanager
@@ -204,14 +207,6 @@ def _lazy_import(attr: str) -> Any:
     assert sep
     assert target.isidentifier()
     return getattr(importlib.import_module(module), target)
-
-
-def _get_gpaw_env_vars(attr: str) -> bool | str:
-    if attr in boolean_envvars:
-        return bool(int(os.environ.get(attr) or 0))
-    if attr in allowed_envvars and attr in os.environ:
-        return os.environ[attr]
-    raise _module_attr_error(attr)
 
 
 all_lazy_imports = dict(
