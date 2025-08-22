d7fcb597e33edc09dab099d9812113a1912ee960
JakobKSvaneborg <jakobksvaneborg@protonmail.com>
2025-08-22 14:32:02 +0200

    flakes

--- gpaw/response/bse.py
+++ gpaw/response/bse.py
@@ -93,7 +93,7 @@ class BSEMatrix:
             bse.context.print('  Using mkl...')
             bse.context.print('You are using a general eigensolver '
                               'to solve a Hermitian eigenvalue problem. '
-                              'You may get better performance by using ' 
+                              'You may get better performance by using '
                               '`backend = \'scalapack\'`')
             w_T = np.empty(nR, dtype=complex)
             mkl_scalapack_diagonalize_non_symmetric(desc, H_rr, v_rt, w_T)
