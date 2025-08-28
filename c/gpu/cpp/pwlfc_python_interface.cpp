7f966d8acf827af9ec973df7ec6b27fe2a89b483
Lauri Niemi <lauri.niemi@csc.fi>
2025-08-28 15:03:39 +0300

    use pyarray_utils.hpp instead of defines from array.h

--- c/gpu/cpp/pwlfc_python_interface.cpp
+++ c/gpu/cpp/pwlfc_python_interface.cpp
@@ -1,13 +1,15 @@
-#include "../extensions.h"
+//#include "../extensions.h"
 
-#define GPAW_ARRAY_DISABLE_NUMPY
-#define GPAW_ARRAY_ALLOW_CUPY
-#include "../array.h"
-#undef GPAW_ARRAY_DISABLE_NUMPY
+// #define GPAW_ARRAY_DISABLE_NUMPY
+// #define GPAW_ARRAY_ALLOW_CUPY
+// #include "../array.h"
+// #undef GPAW_ARRAY_DISABLE_NUMPY
 
-#include "gpu.h"
-#include "gpu-complex.h"
-#include <stdio.h>
+#include "../gpu.h"
+#include "../gpu-complex.h"
+//#include <stdio.h>
+
+#include "pyarray_utils.hpp"
 
 void calculate_residual_launch_kernel(int dtypenum,
                                       int nG,
@@ -93,19 +95,19 @@ void evaluate_lda_launch_kernel(int nspin, int ng,
                                 double* v,
                                 double* e);
 
-static int get_dtype(void* array)
+static int get_dtype(PyObject* array)
 {
     // Only these combinations are allowed. Make it so.
     // dtype.num 11      14        12      15
     // array     float32 complex64 float64 complex128
 
-    int dtypenum = Array_TYPE(array);
+    int dtypenum = gpaw::Array_TYPE(array);
     assert(dtypenum == NP_FLOAT || dtypenum == NP_DOUBLE ||
            dtypenum == NP_FLOAT_COMPLEX || dtypenum == NP_DOUBLE_COMPLEX);
     return dtypenum;
 }
 
-static void assert_corresponding_real(int dtypenum, void* array)
+static void assert_corresponding_real(int dtypenum, PyObject* array)
 {
     // Only these combinations are allowed. Make it so.
     // dtypenum  11      14        12      15
@@ -113,7 +115,7 @@ static void assert_corresponding_real(int dtypenum, void* array)
     //
     // realdtype 11      11        12      12
     // array     float32 float32   float64
-    int realdtype = Array_TYPE(array);
+    int realdtype = gpaw::Array_TYPE(array);
     assert((realdtype == NP_FLOAT && (dtypenum == NP_FLOAT || dtypenum == NP_FLOAT_COMPLEX)) ||
            (realdtype == NP_DOUBLE && (dtypenum == NP_DOUBLE || dtypenum == NP_DOUBLE_COMPLEX)));
     return;
@@ -127,20 +129,20 @@ PyObject* evaluate_lda_gpu(PyObject* self, PyObject* args)
     if (!PyArg_ParseTuple(args, "OOO",
                           &n_obj, &v_obj, &e_obj))
         return NULL;
-    int nspin = Array_DIM(n_obj, 0);
+    int nspin = gpaw::Array_DIM(n_obj, 0);
     if ((nspin != 1) && (nspin != 2))
     {
         PyErr_Format(PyExc_RuntimeError, "Expected 1 or 2 spins. Got %d.", nspin);
         return NULL;
     }
     int ng = 1;
-    for (int d=1; d<Array_NDIM(n_obj); d++)
+    for (int d=1; d<gpaw::Array_NDIM(n_obj); d++)
     {
-        ng *= Array_DIM(n_obj, d);
+        ng *= gpaw::Array_DIM(n_obj, d);
     }
-    double* n_ptr = Array_DATA(n_obj);
-    double* v_ptr = Array_DATA(v_obj);
-    double* e_ptr = Array_DATA(e_obj);
+    double* n_ptr = gpaw::Array_DATA<double>(n_obj);
+    double* v_ptr = gpaw::Array_DATA<double>(v_obj);
+    double* e_ptr = gpaw::Array_DATA<double>(e_obj);
     if (PyErr_Occurred())
     {
         return NULL;
@@ -160,22 +162,22 @@ PyObject* evaluate_pbe_gpu(PyObject* self, PyObject* args)
     if (!PyArg_ParseTuple(args, "OOOOO",
                           &n_obj, &v_obj, &e_obj, &sigma_obj, &dedsigma_obj))
         return NULL;
-    int nspin = Array_DIM(n_obj, 0);
+    int nspin = gpaw::Array_DIM(n_obj, 0);
     if ((nspin != 1) && (nspin != 2))
     {
         PyErr_Format(PyExc_RuntimeError, "Expected 1 or 2 spins. Got %d.", nspin);
         return NULL;
     }
     int ng = 1;
-    for (int d=1; d<Array_NDIM(n_obj); d++)
+    for (int d=1; d<gpaw::Array_NDIM(n_obj); d++)
     {
-        ng *= Array_DIM(n_obj, d);
+        ng *= gpaw::Array_DIM(n_obj, d);
     }
-    double* n_ptr = Array_DATA(n_obj);
-    double* v_ptr = Array_DATA(v_obj);
-    double* e_ptr = Array_DATA(e_obj);
-    double* sigma_ptr = Array_DATA(sigma_obj);
-    double* dedsigma_ptr = Array_DATA(dedsigma_obj);
+    double* n_ptr = gpaw::Array_DATA<double>(n_obj);
+    double* v_ptr = gpaw::Array_DATA<double>(v_obj);
+    double* e_ptr = gpaw::Array_DATA<double>(e_obj);
+    double* sigma_ptr = gpaw::Array_DATA<double>(sigma_obj);
+    double* dedsigma_ptr = gpaw::Array_DATA<double>(dedsigma_obj);
     if (PyErr_Occurred())
     {
         return NULL;
@@ -201,30 +203,30 @@ PyObject* dH_aii_times_P_ani_gpu(PyObject* self, PyObject* args)
         return NULL;
 
 
-    if (Array_DIM(ni_a_obj, 0) == 0)
+    if (gpaw::Array_DIM(ni_a_obj, 0) == 0)
     {
         Py_RETURN_NONE;
     }
 
-    void* dH_aii_dev = Array_DATA(dH_aii_obj);
+    void* dH_aii_dev = gpaw::Array_DATA<void>(dH_aii_obj);
     if (!dH_aii_dev)
     {
 	PyErr_SetString(PyExc_RuntimeError, "Error in input dH_aii.");
         return NULL;
     }
-    void* P_ani_dev = Array_DATA(P_ani_obj);
+    void* P_ani_dev = gpaw::Array_DATA<void>(P_ani_obj);
     if (!P_ani_dev)
     {
         PyErr_SetString(PyExc_RuntimeError, "Error in input P_ani.");
         return NULL;
     }
-    void* outP_ani_dev = Array_DATA(outP_ani_obj);
+    void* outP_ani_dev = gpaw::Array_DATA<void>(outP_ani_obj);
     if (!outP_ani_dev)
     {
         PyErr_SetString(PyExc_RuntimeError, "Error in output outP_ani.");
         return NULL;
     }
-    npy_int32* ni_a = Array_DATA(ni_a_obj);
+    npy_int32* ni_a = gpaw::Array_DATA<npy_int32>(ni_a_obj);
     if (!ni_a)
     {
         PyErr_SetString(PyExc_RuntimeError, "Error in input ni_a.");
@@ -235,11 +237,11 @@ PyObject* dH_aii_times_P_ani_gpu(PyObject* self, PyObject* args)
     assert_corresponding_real(dtypenum, dH_aii_obj);
     assert(dtypenum == get_dtype(outP_ani_obj));
 
-    assert(Array_ITEMSIZE(ni_a_obj) == 4);
+    assert(gpaw::Array_ITEMSIZE(ni_a_obj) == 4);
 
-    int nA = Array_DIM(ni_a_obj, 0);
-    int nn = Array_DIM(P_ani_obj, 0);
-    int nI = Array_DIM(P_ani_obj, 1);
+    int nA = gpaw::Array_DIM(ni_a_obj, 0);
+    int nn = gpaw::Array_DIM(P_ani_obj, 0);
+    int nI = gpaw::Array_DIM(P_ani_obj, 1);
     if (PyErr_Occurred())
     {
         return NULL;
@@ -270,22 +272,22 @@ PyObject* pwlfc_expand_gpu(PyObject* self, PyObject* args)
                           &l_s_obj, &a_J_obj, &s_J_obj,
                           &cc, &f_GI_obj, &I_J_obj))
         return NULL;
-    void *f_Gs = (void*)Array_DATA(f_Gs_obj);
-    void *Y_GL = (void*)Array_DATA(Y_GL_obj);
-    int *l_s = (int*)Array_DATA(l_s_obj);
-    int *a_J = (int*)Array_DATA(a_J_obj);
-    int *s_J = (int*)Array_DATA(s_J_obj);
-    void *f_GI = (void*)Array_DATA(f_GI_obj);
-    int nG = Array_DIM(Gk_Gv_obj, 0);
-    int *I_J = (int*)Array_DATA(I_J_obj);
-    int nJ = Array_DIM(a_J_obj, 0);
-    int nL = Array_DIM(Y_GL_obj, 1);
-    int nI = Array_DIM(f_GI_obj, 1);
-    int natoms = Array_DIM(pos_av_obj, 0);
-    int nsplines = Array_DIM(f_Gs_obj, 1);
-    void* Gk_Gv = (void*)Array_DATA(Gk_Gv_obj);
-    void* pos_av = (void*)Array_DATA(pos_av_obj);
-    void* eikR_a = (void*)Array_DATA(eikR_a_obj);
+    void *f_Gs = gpaw::Array_DATA<void>(f_Gs_obj);
+    void *Y_GL = gpaw::Array_DATA<void>(Y_GL_obj);
+    int *l_s = gpaw::Array_DATA<int>(l_s_obj);
+    int *a_J = gpaw::Array_DATA<int>(a_J_obj);
+    int *s_J = gpaw::Array_DATA<int>(s_J_obj);
+    void *f_GI = gpaw::Array_DATA<void>(f_GI_obj);
+    int nG = gpaw::Array_DIM(Gk_Gv_obj, 0);
+    int *I_J = gpaw::Array_DATA<int>(I_J_obj);
+    int nJ = gpaw::Array_DIM(a_J_obj, 0);
+    int nL = gpaw::Array_DIM(Y_GL_obj, 1);
+    int nI = gpaw::Array_DIM(f_GI_obj, 1);
+    int natoms = gpaw::Array_DIM(pos_av_obj, 0);
+    int nsplines = gpaw::Array_DIM(f_Gs_obj, 1);
+    void* Gk_Gv = gpaw::Array_DATA<void>(Gk_Gv_obj);
+    void* pos_av = gpaw::Array_DATA<void>(pos_av_obj);
+    void* eikR_a = gpaw::Array_DATA<void>(eikR_a_obj);
     int dtype = get_dtype(f_GI_obj);
     if (PyErr_Occurred())
     {
@@ -307,24 +309,24 @@ PyObject* pw_insert_gpu(PyObject* self, PyObject* args)
     if (!PyArg_ParseTuple(args, "OOdOiii",
                           &c_nG_obj, &Q_G_obj, &scale, &tmp_nQ_obj, &rx, &ry, &rz))
         return NULL;
-    npy_int32 *Q_G = Array_DATA(Q_G_obj);
-    void *c_nG = Array_DATA(c_nG_obj);
-    void *tmp_nQ = Array_DATA(tmp_nQ_obj);
+    npy_int32 *Q_G = gpaw::Array_DATA<npy_int32>(Q_G_obj);
+    void *c_nG = gpaw::Array_DATA<void>(c_nG_obj);
+    void *tmp_nQ = gpaw::Array_DATA<void>(tmp_nQ_obj);
     int nG = 0;
     int nQ = 0;
     int nb = 0;
-    assert(Array_NDIM(c_nG_obj) == Array_NDIM(tmp_nQ_obj));
-    if (Array_NDIM(c_nG_obj) == 1)
+    assert(gpaw::Array_NDIM(c_nG_obj) == gpaw::Array_NDIM(tmp_nQ_obj));
+    if (gpaw::Array_NDIM(c_nG_obj) == 1)
     {
-        nG = Array_DIM(c_nG_obj, 0);
+        nG = gpaw::Array_DIM(c_nG_obj, 0);
         nb = 1;
-        nQ = Array_DIM(tmp_nQ_obj, 0);
+        nQ = gpaw::Array_DIM(tmp_nQ_obj, 0);
     }
     else
     {
-        nG = Array_DIM(c_nG_obj, 1);
-        nb = Array_DIM(c_nG_obj, 0);
-        nQ = Array_DIM(tmp_nQ_obj, 1);
+        nG = gpaw::Array_DIM(c_nG_obj, 1);
+        nb = gpaw::Array_DIM(c_nG_obj, 0);
+        nQ = gpaw::Array_DIM(tmp_nQ_obj, 1);
     }
     if (PyErr_Occurred())
     {
@@ -350,21 +352,21 @@ PyObject* pw_norm_gpu(PyObject* self, PyObject* args)
                           &result_x_obj, &C_xG_obj))
         return NULL;
 
-    void *result_x = Array_DATA(result_x_obj);
-    void *C_xG = Array_DATA(C_xG_obj);
+    void *result_x = gpaw::Array_DATA<void>(result_x_obj);
+    void *C_xG = gpaw::Array_DATA<void>(C_xG_obj);
     int dtypenum = get_dtype(C_xG_obj);
 
     // Make sure number of dimensions are correct
-    assert(Array_NDIM(C_xG_obj) == 2);
-    assert(Array_NDIM(result_x_obj) == 1);
+    assert(gpaw::Array_NDIM(C_xG_obj) == 2);
+    assert(gpaw::Array_NDIM(result_x_obj) == 1);
 
     // Make sure dtypes are correct
     assert_corresponding_real(dtypenum, result_x_obj);
 
     // Make sure dimensions match
-    int nx = Array_DIM(result_x_obj, 0);
-    int nG = Array_DIM(C_xG_obj, 1);
-    assert(Array_DIM(C_xG_obj, 0) == nx);
+    int nx = gpaw::Array_DIM(result_x_obj, 0);
+    int nG = gpaw::Array_DIM(C_xG_obj, 1);
+    assert(gpaw::Array_DIM(C_xG_obj, 0) == nx);
 
     if (PyErr_Occurred())
     {
@@ -384,25 +386,25 @@ PyObject* pw_norm_kinetic_gpu(PyObject* self, PyObject* args)
                           &result_x_obj, &C_xG_obj, &kin_G_obj))
         return NULL;
 
-    void *result_x = Array_DATA(result_x_obj);
-    void *C_xG = Array_DATA(C_xG_obj);
-    void *kin_G = Array_DATA(kin_G_obj);
+    void *result_x = gpaw::Array_DATA<void>(result_x_obj);
+    void *C_xG = gpaw::Array_DATA<void>(C_xG_obj);
+    void *kin_G = gpaw::Array_DATA<void>(kin_G_obj);
     int dtypenum = get_dtype(C_xG_obj);
 
     // Make sure number of dimensions are correct
-    assert(Array_NDIM(C_xG_obj) == 2);
-    assert(Array_NDIM(result_x_obj) == 1);
-    assert(Array_NDIM(kin_G_obj) == 1);
+    assert(gpaw::Array_NDIM(C_xG_obj) == 2);
+    assert(gpaw::Array_NDIM(result_x_obj) == 1);
+    assert(gpaw::Array_NDIM(kin_G_obj) == 1);
 
     // Make sure dtypes are correct
     assert_corresponding_real(dtypenum, result_x_obj);
     assert_corresponding_real(dtypenum, kin_G_obj);
 
     // Make sure dimensions match
-    int nx = Array_DIM(result_x_obj, 0);
-    int nG = Array_DIM(C_xG_obj, 1);
-    assert(Array_DIM(kin_G_obj, 0) == nG);
-    assert(Array_DIM(C_xG_obj, 0) == nx);
+    int nx = gpaw::Array_DIM(result_x_obj, 0);
+    int nG = gpaw::Array_DIM(C_xG_obj, 1);
+    assert(gpaw::Array_DIM(kin_G_obj, 0) == nG);
+    assert(gpaw::Array_DIM(C_xG_obj, 0) == nx);
 
     if (PyErr_Occurred())
     {
@@ -424,16 +426,16 @@ PyObject* pw_amend_insert_realwf_gpu(PyObject* self, PyObject* args)
     if (!PyArg_ParseTuple(args, "Oii",
                           &array_nQ_obj, &n, &m))
         return NULL;
-    void *array_nQ = Array_DATA(array_nQ_obj);
-    if (Array_NDIM(array_nQ_obj) != 4)
+    void *array_nQ = gpaw::Array_DATA<void>(array_nQ_obj);
+    if (gpaw::Array_NDIM(array_nQ_obj) != 4)
     {
         PyErr_SetString(PyExc_RuntimeError, "array_nQ must be of (nb, NGx, NGy, NGz)-shape.");
         return NULL;
     }
-    int nb = Array_DIM(array_nQ_obj, 0);
-    int nx = Array_DIM(array_nQ_obj, 1);
-    int ny = Array_DIM(array_nQ_obj, 2);
-    int nz = Array_DIM(array_nQ_obj, 3);
+    int nb = gpaw::Array_DIM(array_nQ_obj, 0);
+    int nx = gpaw::Array_DIM(array_nQ_obj, 1);
+    int ny = gpaw::Array_DIM(array_nQ_obj, 2);
+    int nz = gpaw::Array_DIM(array_nQ_obj, 3);
     if (PyErr_Occurred())
     {
         return NULL;
@@ -454,11 +456,11 @@ PyObject* add_to_density_gpu(PyObject* self, PyObject* args)
         return NULL;
     int dtypenum = get_dtype(psit_nR_obj);
 
-    double *f_n = Array_DATA(f_n_obj);
-    void *psit_nR = (void*) Array_DATA(psit_nR_obj);
-    void *rho_R = (void*) Array_DATA(rho_R_obj);
-    int nb = Array_SIZE(f_n_obj);
-    int nR = Array_SIZE(psit_nR_obj) / nb;
+    double *f_n = gpaw::Array_DATA<double>(f_n_obj);
+    void *psit_nR = gpaw::Array_DATA<void>(psit_nR_obj);
+    void *rho_R = gpaw::Array_DATA<void>(rho_R_obj);
+    int nb = gpaw::Array_SIZE(f_n_obj);
+    int nR = gpaw::Array_SIZE(psit_nR_obj) / nb;
 
     // If running on same precision, then this should be the case
     // assert_corresponding_real(dtypenum, rho_R_obj);
@@ -480,14 +482,14 @@ PyObject* calculate_residual_gpu(PyObject* self, PyObject* args)
     if (!PyArg_ParseTuple(args, "OOO",
                           &residual_nG_obj, &eps_n_obj, &wf_nG_obj))
         return NULL;
-    void *residual_nG = Array_DATA(residual_nG_obj);
-    void* eps_n = Array_DATA(eps_n_obj);
-    void *wf_nG = Array_DATA(wf_nG_obj);
-    int nn = Array_DIM(residual_nG_obj, 0);
+    void *residual_nG = gpaw::Array_DATA<void>(residual_nG_obj);
+    void* eps_n = gpaw::Array_DATA<void>(eps_n_obj);
+    void *wf_nG = gpaw::Array_DATA<void>(wf_nG_obj);
+    int nn = gpaw::Array_DIM(residual_nG_obj, 0);
     int nG = 1;
-    for (int d=1; d<Array_NDIM(residual_nG_obj); d++)
+    for (int d=1; d<gpaw::Array_NDIM(residual_nG_obj); d++)
     {
-        nG *= Array_DIM(residual_nG_obj, d);
+        nG *= gpaw::Array_DIM(residual_nG_obj, d);
     }
     if (PyErr_Occurred())
     {
