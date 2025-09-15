494a88de0805689a5a59dc48363e09f837a688c2
Jens Jørgen Mortensen <jj@smoerhul.dk>
2025-09-15 16:01:10 +0200

    Also port SimpleBasis to GPU

--- gpaw/new/basis.py
+++ gpaw/new/basis.py
@@ -31,7 +31,7 @@ def create_basis(ibz: IBZ,
     kd.symmetry = ibz.symmetries._old_symmetry
     kd.set_communicator(kpt_comm)
     if GPAW_NO_C_EXTENSION:
-        return SimpleBasis(grid, setups, relpos_ac)
+        return SimpleBasis(grid, setups, relpos_ac, xp)
     basis_dtype = complex if \
         np.issubdtype(dtype, np.complexfloating) else float
     basis = BasisFunctions(grid._gd,
@@ -49,20 +49,27 @@ def create_basis(ibz: IBZ,
 
 
 class SimpleBasis:
+    """Quick hack for use without C-extensions."""
     def __init__(self,
                  grid: UGDesc,
                  setups,
-                 relpos_ac):
+                 relpos_ac,
+                 xp):
         self.grid = grid
         self.pw = PWDesc(cell=grid.cell,
                          ecut=min(12.5, grid.ekin_max()))
         self.phit_aIG = self.pw.atom_centered_functions(
             [setup.basis_functions_J for setup in setups],
             relpos_ac)
+        self.xp = xp
 
     def add_to_density(self,
                        nt_sR: np.ndarray,
                        f_asi):
+        if self.xp is not np:
+            _nt_sR = self.xp.asnumpy(nt_sR)
+        else:
+            _nt_sR = nt_sR
         nI = sum(f_si.shape[1] for f_si in f_asi.values())
         c_aiI = self.phit_aIG.empty(nI)
         c_aiI.data[:] = np.eye(nI)
@@ -72,6 +79,8 @@ class SimpleBasis:
         for f_si in f_asi.values():
             for f_s in f_si.T:
                 phit_R = phit_IG[I].ifft(grid=self.grid)
-                nt_sR += f_s[:, np.newaxis, np.newaxis, np.newaxis] * (
+                _nt_sR += f_s[:, np.newaxis, np.newaxis, np.newaxis] * (
                     phit_R.data**2)
                 I += 1
+        if self.xp is not np:
+            nt_sR[:] = self.xp.asarray(_nt_sR)
