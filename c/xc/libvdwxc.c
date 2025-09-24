fdf0558a54db0eecd2a9830633451f91a436afd0
the-ohm <mikhik@yahoo.dk>
2025-09-24 12:43:05 +0200

    TYPOS

--- c/xc/libvdwxc.c
+++ c/xc/libvdwxc.c
@@ -205,7 +205,7 @@ PyObject* libvdwxc_free(PyObject* self, PyObject* args)
 MPI_Comm unpack_gpaw_comm(PyObject* gpaw_mpi_obj)
 {
     //MPIObject* gpaw_comm = (MPIObject *)gpaw_mpi_obj;
-    return *((MPI_Comm*) PyLong_AsVoidPtr(comm_obj));
+    return *((MPI_Comm*) PyLong_AsVoidPtr(gpaw_mpi_obj));
 }
 #endif
 
