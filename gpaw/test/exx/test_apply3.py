ed378eab6e20ce2a70373aa07a9625d200993905
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-09-25 16:40:12 +0200

    Gamma ... WIP

--- gpaw/test/exx/test_apply3.py
+++ gpaw/test/exx/test_apply3.py
@@ -7,11 +7,11 @@ from gpaw.mpi import world
 from time import time
 
 
-def test_apply3(a=5.0, N=10):
+def test_apply3(a=5.0, N=10, dtype=float):
     c = 4.0
     grid = UGDesc.from_cell_and_grid_spacing([a, a, c, 90, 90, 120], 0.3,
-                                             dtype=complex)
-    pw = PWDesc(cell=grid.cell, ecut=grid.ekin_max() * 0.8)
+                                             dtype=dtype)
+    pw = PWDesc(cell=grid.cell, ecut=grid.ekin_max() * 0.8, dtype=dtype)
     relpos_ac = np.empty((N, 3))
     relpos_ac[:] = np.linspace(0, 1.0, N, False)[:, np.newaxis]
     xc = type('XC', (), {'exx_fraction': 0.25,
@@ -25,20 +25,26 @@ def test_apply3(a=5.0, N=10):
         kpt_comm=world,
         band_comm=world,
         comm=world)
-    # pw1 = pw.new(kpt=[0.5, 0.5, 0.0], dtype=complex)
-    pw2 = pw.new(kpt=[0.25, 0.25, 0.0], dtype=complex)
-    pw12 = pw.new(kpt=[-0.25, -0.25, 0.0], dtype=complex)
+    if dtype == float:
+        pw2 = pw
+        pw12 = pw
+        kpt1_c = np.zeros(3)
+    else:
+        kpt1_c = np.array([0.5, 0.5, 0.0])
+        kpt2_c = np.array([0.25, 0.25, 0.0])
+        pw2 = pw.new(kpt=[0.25, 0.25, 0.0], dtype=dtype)
+        pw12 = pw.new(kpt=kpt2_c - kpt1_c, dtype=dtype)
     n1 = 35
     n2 = 153
-    layout = AtomArraysLayout([13] * N, dtype=complex)
+    layout = AtomArraysLayout([13] * N, dtype=dtype)
     psit1 = Psit(
         ut_nR=grid.empty(n1),
         P_ani=layout.empty(n1),
         f_n=np.ones(n1),
-        kpt_c=np.array([0.6, 0.6, 0.6]),
-        Q_aniL={a: np.ones((n1, 13, 9), dtype=complex) for a in range(N)},
+        kpt_c=kpt1_c,
+        Q_aniL={a: np.ones((n1, 13, 9), dtype=dtype) for a in range(N)},
         spin=0)
-    v_G = 1.0 / pw12.ekin_G
+    v_G = 1.0 / (0.1 + pw12.ekin_G)
     ut2_nR = grid.empty(n2)
     P2_ani = layout.empty(n2)
     V2_ani = P2_ani.new()
@@ -56,10 +62,10 @@ def test_apply3(a=5.0, N=10):
         Htpsit2_nG,
         V2_ani,
         f2_n=np.ones(n2),
-        calculate_energy=not True)
+        calculate_energy=True)
     t = time() - t
     print(t)
 
 
 if __name__ == '__main__':
-    test_apply3(9.5, 20)
+    test_apply3(9.5, 20, dtype=complex)
