8ba3661e978b3cb20957a459061219f9b2e2b982
Mikael Kuisma <kuisma@dtu.dk>
2025-10-16 12:52:26 +0200

    maybe test runs now

--- gpaw/test/response/test_gw_lcao.py
+++ gpaw/test/response/test_gw_lcao.py
@@ -5,15 +5,20 @@ def test_lcao_gw(in_tmp_dir):
     from ase.build import bulk
     from gpaw import GPAW
     from gpaw.response.g0w0 import G0W0
-    atoms = bulk('C')
-    atoms.calc = GPAW(mode='lcao',
-                      basis='dzp',
-                      kpts={'gamma': True, 'size': (8,8,8)})
-    atoms.get_potential_energy()
-    atoms.calc.write('gs.gpw', mode='all')
+    if 1:
+        atoms = bulk('C')
+        atoms.calc = GPAW(mode='lcao',
+                          basis='dzp',
+                          nbands='nao',
+                          kpts={'gamma': True, 'size': (8,8,8)})
+        atoms.get_potential_energy()
+        atoms.calc.write('gs.gpw', mode='all')
     gw = G0W0('gs.gpw',
-              nbands=8, integrate_gamma='WS',
-              ecut=200,
-              eta=0.1, relbands=(0, 8))
+              nbands=((1 + 3) * 2 + 5) * 2, 
+              integrate_gamma='WS',
+              ecut=200, 
+              eta=0.1, bands=(0, 8))
     gw.calculate()
 
+if __name__ == "__main__":
+    test_lcao_gw(None)
