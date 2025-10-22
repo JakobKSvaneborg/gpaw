aa9862a5843b165f011466fa1855595c1949b56d
Elisabeth Johanna Keller <ejoke@sylg.fysik.dtu.dk>
2025-10-22 15:10:10 +0200

    Fix lint error

--- gpaw/test/response/test_diamond_absorption.py
+++ gpaw/test/response/test_diamond_absorption.py
@@ -24,7 +24,6 @@ def test_response_diamond_absorption(in_tmp_dir, eshift, mode):
                 kpts=(3, 3, 3),
                 nbands='nao' if mode == 'lcao' else None,
                 basis='dzp' if mode == 'lcao' else None,
-             #   convergence={'density': 1e-6},
                 eigensolver='rmm-diis' if mode == 'pw' else None,
                 occupations=FermiDirac(0.001), txt='out.txt')
 
