f6ff9b8f88bba4f0a68a956446c0b2397d8fa267
hamed <hamedgeramizadeh@gmail.com>
2025-10-24 16:01:50 +0000

    reworked er_localize as a general tool.

--- gpaw/new/etdm/tools.py
+++ gpaw/new/etdm/tools.py
@@ -8,21 +8,24 @@ from gpaw.new.etdm.edmiston_ruedenberg.obj_function import (
 from gpaw.new.etdm.skewherm_matrix import random_a
 
 
-def er_localize(ibzwfs, state2opt="all"):
-    for states in state2opt.split("-"):
-        for loct in ["paw", "pseudo"]:
-            objfunc = ER(ibzwfs, loct, states)
-            a_init = []
-            for a in objfunc.a_vec_u:
-                skmat = random_a((a.ndim, a.ndim), ibzwfs.dtype) * 0.01
-                skmat -= skmat.T.conj()
-                a_init.append(skmat[a.ind_up])
-            a_init = np.asarray(a_init)
-            world.broadcast(a_init, 0)
-            gtol = 1.0e-6
-            niter = 500
-            if loct == "paw":
-                gtol = 1.0e-12
-                niter = 2000
-            etdm = ETDM(objfunc, a_init, niter, gtol, True)
-            etdm.optimize()
+def er_localize(ibzwfs,
+                states="all"
+                loct="pseudo-paw"
+                gtol=1e-6,
+                niter=333,
+                seed=None
+    ):
+    objfunc = ER(ibzwfs, loct, states)
+    a_init = []
+    for a in objfunc.a_vec_u:
+        skmat = random_a((a.ndim, a.ndim),
+                          ibzwfs.dtype, seed=seed) * 0.01
+        skmat -= skmat.T.conj()
+        a_init.append(skmat[a.ind_up])
+    a_init = np.asarray(a_init)
+    world.broadcast(a_init, 0)
+
+    etdm = ETDM(objfunc, a_init, niter, gtol, True)
+    etdm.optimize()
+
+    return etdm
