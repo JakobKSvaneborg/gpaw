d6c4c65f0f87ec683268071d31fabfb80294bb28
Boris Neijzen <borne@fjorm.fysik.dtu.dk>
2025-10-27 16:00:29 +0100

    changed error type and more formatting

--- gpaw/symmetry.py
+++ gpaw/symmetry.py
@@ -517,8 +517,8 @@ def map_k_points_fast(bzk_kc, U_scc, time_reversal, comm=None, tol=1e-7):
 
         # This has to be true.
         if not (orders[0] < nbzkpts).all():
-            raise AttributeError("Duplicate kpoints detected, \
-                                 set symmetry='off'.")
+            raise ValueError("Duplicate kpoints detected, "
+                             "set symmetry='off'.")
         assert (orders[1] >= nbzkpts).all()
         bz2bz_ks[orders[1] - nbzkpts, s] = orders[0]
 
