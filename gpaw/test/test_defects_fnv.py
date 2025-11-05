2281375ba159325c249177408533aaa728fe63fc
Silvan Kretschmer <silvan_kretschmer@gmx.de>
2025-11-05 17:25:06 +0100

    Added cell test

--- gpaw/test/test_defects_fnv.py
+++ gpaw/test/test_defects_fnv.py
@@ -58,6 +58,10 @@ def test_fnv_2d():
 
 @pytest.mark.parametrize('modename', ['pw', 'fd'])
 def test_fnv_3d(modename):
+
+    E_corr_t = 23.55
+    E_uncorr_t = 18.31
+
     a0 = 5.628      # lattice parameter
     sigma = 2 / (2.0 * np.sqrt(2.0 * np.log(2.0)))
     epsilon = 12.7  # dielectric constant
@@ -71,23 +75,58 @@ def test_fnv_3d(modename):
               'kpts': {'size': (2, 2, 2), 'gamma': False},
               'occupations': {'name': 'fermi-dirac', 'width': 0.01}}
 
-    E_corr_t = 23.55
-    E_uncorr_t = 18.31
+    calc_charged = GPAW(charge=charge, **params)
+    calc_neutral = GPAW(charge=0, **params)
 
     pristine = bulk('GaAs', crystalstructure='zincblende', a=a0, cubic=True)
+    pristine.calc = calc_neutral
+    pristine.get_potential_energy()
 
     defect = pristine.copy()
     defect.pop(0)  # make a Ga vacancy
+    defect.calc = calc_charged
+    defect.get_potential_energy()
+
+    # need to convert Path -> str
+    elc = ElectrostaticCorrections(pristine=pristine.calc,
+                                   charged=defect.calc,
+                                   q=charge,
+                                   sigma=sigma,
+                                   dimensionality='3d')
+    elc.set_epsilons(epsilon)
+    E_corr = elc.calculate_corrected_formation_energy()
+    E_uncorr = elc.calculate_uncorrected_formation_energy()
+
+    assert E_corr == pytest.approx(E_corr_t, abs=2e-2)
+    assert E_uncorr == pytest.approx(E_uncorr_t, abs=2e-2)
+
+
+def test_fnv_cell():
+
+    E_corr_t = 23.55
+    E_uncorr_t = 18.31
+
+    a0 = 5.628      # lattice parameter
+    sigma = 2 / (2.0 * np.sqrt(2.0 * np.log(2.0)))
+    epsilon = 12.7  # dielectric constant
+    charge = -3     # defect charge
+
+    params = {'mode': {'name': 'pw', 'ecut': 400},
+              'xc': 'LDA',
+              'kpts': {'size': (2, 2, 2), 'gamma': False},
+              'occupations': {'name': 'fermi-dirac', 'width': 0.01}}
 
     calc_charged = GPAW(charge=charge, **params)
     calc_neutral = GPAW(charge=0, **params)
 
-    defect.calc = calc_charged
-    defect.get_potential_energy()
-
-    # pristine case
+    pristine = bulk('GaAs', crystalstructure='zincblende', a=a0, cubic=True)
     pristine.calc = calc_neutral
     pristine.get_potential_energy()
+
+    defect = pristine.copy()
+    defect.pop(0)  # make a Ga vacancy
+    defect.calc = calc_charged
+    defect.get_potential_energy()
 
     # need to convert Path -> str
     elc = ElectrostaticCorrections(pristine=pristine.calc,
