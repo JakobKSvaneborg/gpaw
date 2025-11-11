2bbf7a47cd63e9fffe04e1d38d28c082f5c7ced6
Mikael Kuisma <kuisma@dtu.dk>
2025-11-11 10:49:37 +0100

    xeon40 nodes of niflheim can't run GPAW because GPAW is being compiled for GPU (for another node with same CPU architechture), but xeon40 doesn't have cuda installed

--- gpaw/new/c.py
+++ gpaw/new/c.py
@@ -23,10 +23,10 @@ if not TYPE_CHECKING and not GPAW_NO_C_EXTENSION:
                                 pw_insert_gpu, pwlfc_expand_gpu,
                                 pw_norm_kinetic_gpu, pw_norm_gpu)
 
-        from cupy.cuda.stream import get_current_stream
         import functools
 
         def s(fun):
+            from cupy.cuda.stream import get_current_stream
             @functools.wraps(fun)
             def wrapper(*args):
                 return fun(*args, get_current_stream().ptr)
