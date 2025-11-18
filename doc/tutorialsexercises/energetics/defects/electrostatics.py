0f9ccaefdb3cda641c13f976d20aafe8bd6ed81b
SianK <silkr@dtu.dk>
2025-11-18 11:06:34 +0100

    Updated electrostatics.py to new fnv implementation, potential profile missing

--- doc/tutorialsexercises/energetics/defects/electrostatics.py
+++ doc/tutorialsexercises/energetics/defects/electrostatics.py
@@ -1,25 +1,37 @@
 import numpy as np
+from gpaw import GPAW
 from gpaw.defects import ElectrostaticCorrections
+from pathlib import Path
 
 sigma = 2 / (2.0 * np.sqrt(2.0 * np.log(2.0)))
-q = -3
+charge = -3
 epsilon = 12.7
 corrected = []
 uncorrected = []
-repeats = [1, 2, 3, 4]
-for repeat in repeats:
-    pristine = 'GaAs{0}{0}{0}.pristine.gpw'.format(repeat)
-    charged = 'GaAs{0}{0}{0}.Ga_vac_charged.gpw'.format(repeat)
-    elc = ElectrostaticCorrections(pristine=pristine,
-                                   charged=charged,
-                                   q=q,
+repeats = [1]
+for N in repeats:
+    label = f'GaAs_{N}x{N}x{N}'
+    prs_path = Path(f'{label}_prs.gpw')
+    def_path = Path(f'{label}_def.gpw')
+
+    pristine = GPAW(prs_path).get_atoms()
+
+    # defect position
+    r0 = pristine.positions[0, :]
+
+    elc = ElectrostaticCorrections(pristine=prs_path,
+                                   defect=def_path,
+                                   r0=r0,
+                                   charge=charge,
                                    sigma=sigma,
-                                   dimensionality='3d')
-    elc.set_epsilons(epsilon)
-    corrected.append(elc.calculate_corrected_formation_energy())
-    uncorrected.append(elc.calculate_uncorrected_formation_energy())
-    data = elc.collect_electrostatic_data()
-    np.savez('electrostatic_data_{0}{0}{0}.npz'.format(repeat), **data)
+                                   epsilon=epsilon,
+                                   method='full-planar')
+    E_corr = elc.calculate_corrected_formation_energy()
+    E_uncorr = elc.calculate_uncorrected_formation_energy()
+    E_fnv = E_corr - E_uncorr
+
+    corrected.append(E_corr)
+    uncorrected.append(E_uncorr)
 
 np.savez('formation_energies.npz',
          repeats=np.array(repeats),
