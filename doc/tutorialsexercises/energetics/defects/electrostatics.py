81aad1aad94d2b3e62dae2259d352fb5d8bbe562
SianK <silkr@dtu.dk>
2025-11-19 13:42:14 +0100

    Updated tutorial

--- doc/tutorialsexercises/energetics/defects/electrostatics.py
+++ doc/tutorialsexercises/energetics/defects/electrostatics.py
@@ -2,6 +2,7 @@ import numpy as np
 from ase.io.jsonio import write_json
 from gpaw import GPAW
 from gpaw.defects import ElectrostaticCorrections
+from gpaw.defects.electrostatics import gather_electrostatic_potential
 from pathlib import Path
 
 sigma = 2 / (2.0 * np.sqrt(2.0 * np.log(2.0)))
@@ -15,20 +16,30 @@ for N in repeats:
     prs_path = Path(f'{label}_prs.gpw')
     def_path = Path(f'{label}_def.gpw')
 
-    pristine = GPAW(prs_path).get_atoms()
+    calc_prs = GPAW(prs_path)
+    calc_def = GPAW(def_path)
+
+    atoms_prs = calc_prs.get_atoms()
+    rvR_prs, phi_prs = gather_electrostatic_potential(calc_prs)
+    rvR_def, phi_def = gather_electrostatic_potential(calc_def)
 
     # defect position
-    r0 = pristine.positions[0, :]
+    r0 = atoms_prs.positions[0, :]
 
-    elc = ElectrostaticCorrections(pristine=prs_path,
-                                   defect=def_path,
+    elc = ElectrostaticCorrections(atoms_prs=atoms_prs,
+                                   rphi_prs=(rvR_prs, phi_prs),
+                                   rphi_def=(rvR_def, phi_def),
                                    r0=r0,
                                    charge=charge,
                                    sigma=sigma,
                                    epsilon=epsilon,
                                    method='full-planar')
-    E_corr = elc.calculate_corrected_formation_energy()
-    E_uncorr = elc.calculate_uncorrected_formation_energy()
+    E_fnv = elc.calculate_correction()
+
+    E_0 = calc_prs.get_potential_energy()
+    E_X = calc_def.get_potential_energy()
+    E_uncorr = E_X - E_0
+    E_corr = E_uncorr + E_fnv
 
     if N == 2:
         profile = elc.calculate_potential_profile()
