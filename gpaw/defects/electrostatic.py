f3131e088989e8ed5debb0cf85b8d21bf4260fee
Silvan Kretschmer <silvan_kretschmer@gmx.de>
2025-11-20 16:10:47 +0100

    Removed OldElectrostaticCorrections and corresponding tests. Clean-up rest of tests.

--- gpaw/defects/electrostatic.py
+++ gpaw/defects/electrostatic.py
@@ -28,6 +28,31 @@ def gather_electrostatic_potential(calc):
     return phi_R.gather(broadcast=True)
 
 
+def plot_potential(profile):
+    from matplotlib import pyplot as plt
+
+    z = profile['z']
+    V_m = profile['model']
+    dV_defprs = profile['def'] - profile['prs']
+    dV = V_m - dV_defprs
+    dphi_avg = profile['dphi']
+
+    plt.plot(z, dV, '-', label=r'$\Delta V(z)$')
+    plt.plot(z, V_m, '-', label='$V(z)$')
+    plt.plot(z, dV_defprs, '-',
+             label=(r'$[V^{V_\mathrm{Ga}^{-3}}_\mathrm{el}(z) -'
+                    r'V^{0}_\mathrm{el}(z) ]$'))
+
+    plt.axhline(dphi_avg, ls='dashed')
+    plt.axhline(0.0, ls='-', color='grey')
+    plt.xlabel(r'$z\enspace (\mathrm{\AA})$', fontsize=18)
+    plt.ylabel('Planar averages (eV)', fontsize=18)
+    plt.legend(loc='upper right')
+    plt.xlim((z[0], z[-1]))
+
+    plt.show()
+
+
 class ElectrostaticCorrections():
     """
     Calculate the electrostatic corrections for charged defects.
--- gpaw/test/test_defects_fnv.py
+++ gpaw/test/test_defects_fnv.py
@@ -2,14 +2,12 @@ import numpy as np
 import pytest
 from ase import Atoms
 from ase.units import Bohr, Hartree
-from ase.build import bulk, graphene
-from ase.build.supercells import make_supercell
+from ase.build import bulk
 
 from gpaw import GPAW
 from gpaw.defects import ElectrostaticCorrections
-from gpaw.defects.old_electrostatic import OldElectrostaticCorrections
 from gpaw.defects.electrostatic import (gather_electrostatic_potential,
-                                        build_ugarray)
+                                        build_ugarray, plot_potential)
 from scipy.special import erf
 from pathlib import Path
 
@@ -93,125 +91,10 @@ def test_fnv_model(method):
     E_fnv = elc.calculate_correction()
 
     if 0:
-        from matplotlib import pyplot as plt
         profile = elc.calculate_potential_profile()
-
-        z = profile['z']
-        V_m = profile['model']
-        dV_defprs = profile['def'] - profile['prs']
-        dV = V_m - dV_defprs
-        dphi_avg = profile['dphi']
-
-        plt.plot(z, dV, '-', label=r'$\Delta V(z)$')
-        plt.plot(z, V_m, '-', label='$V(z)$')
-        plt.plot(z, dV_defprs, '-',
-                 label=(r'$[V^{V_\mathrm{Ga}^{-3}}_\mathrm{el}(z) -'
-                        r'V^{0}_\mathrm{el}(z) ]$'))
-
-        plt.axhline(dphi_avg, ls='dashed')
-        plt.axhline(0.0, ls='-', color='grey')
-        plt.xlabel(r'$z\enspace (\mathrm{\AA})$', fontsize=18)
-        plt.ylabel('Planar averages (eV)', fontsize=18)
-        plt.legend(loc='upper right')
-        plt.xlim((z[0], z[-1]))
-
-        plt.show()
+        plot_potential(profile)
 
     assert E_fnv == pytest.approx(E_fnv_t[method], abs=1e-2)
-
-
-@pytest.mark.serial
-def test_fnv_2d():
-
-    E_corr_t = 4.892
-    E_uncorr_t = 9.349
-
-    sigma = 1.0
-    charge = +1
-    epsilons = [1.9, 1.15]
-    a0 = 2.51026699
-    c0 = 15.0
-
-    params = {'mode': {'name': 'pw', 'ecut': 400},
-              'xc': 'PBE',
-              'kpts': {'size': (4, 4, 1)},
-              'occupations': {'name': 'fermi-dirac', 'width': 0.01}}
-
-    calc_charged = GPAW(charge=charge, **params)
-    calc_neutral = GPAW(charge=0, **params)
-
-    atoms = graphene('N2', a=a0, vacuum=c0 / 2)
-    atoms.symbols[0] = 'B'
-    atoms.set_pbc(True)
-    atoms.center()
-
-    # transformation to orthogonal cell
-    P = np.array([[1, 0, 0], [1, 2, 0], [0, 0, 1]])
-    pristine = make_supercell(atoms, P)
-    pristine.calc = calc_neutral
-    pristine.get_potential_energy()
-
-    defect = pristine.copy()
-    # C_B substitution
-    defect[0].symbol = 'C'
-    defect[1].magmom = 1
-    defect.calc = calc_charged
-    defect.get_potential_energy()
-
-    # defect position
-    r0 = pristine.positions[0, :]
-
-    elc = OldElectrostaticCorrections(pristine=pristine.calc,
-                                      charged=defect.calc,
-                                      r0=r0,
-                                      q=charge,
-                                      sigma=sigma,
-                                      dimensionality='2d')
-    elc.set_epsilons(epsilons)
-    E_corr = elc.calculate_corrected_formation_energy()
-    E_uncorr = elc.calculate_uncorrected_formation_energy()
-
-    assert E_corr == pytest.approx(E_corr_t, abs=2e-2)
-    assert E_uncorr == pytest.approx(E_uncorr_t, abs=2e-2)
-
-
-def test_fnv_data():
-
-    sigma = 2 / (2.0 * np.sqrt(2.0 * np.log(2.0)))
-
-    from ase.io.jsonio import read_json
-    # from ase.io.jsonio import write_json
-    # data = {'E_corr': E_corr_t, 'E_uncorr': E_uncorr_t,
-    #         'epsilon': epsilon, 'charge': charge, 'r0': r0,
-    #         'atoms_prs': atoms_prs, 'rvR': rvR_prs,
-    #         'phi_prs': phi_prs, 'phi_def': phi_def,
-    #         'method': 'full-planar'}
-    # write_json('test_fnv_3d.json', data)
-
-    data = read_json('test_fnv_3d.json')
-    E_fnv_t = data['E_corr'] - data['E_uncorr']
-    epsilon = data['epsilon']
-    charge = data['charge']
-    r0 = data['r0']
-    atoms = data['atoms_prs']
-    phi_prs = data['phi_prs']
-    phi_def = data['phi_def']
-    method = data['method']
-
-    phi_def_R = build_ugarray(atoms, phi_def)
-    phi_prs_R = build_ugarray(atoms, phi_prs)
-
-    elc = ElectrostaticCorrections(phi_pristine=phi_prs_R,
-                                   phi_defect=phi_def_R,
-                                   r0=r0,
-                                   charge=charge,
-                                   sigma=sigma,
-                                   epsilon=epsilon,
-                                   method=method,
-                                   atoms_pristine=atoms)
-    E_fnv = elc.calculate_correction()
-
-    assert E_fnv == pytest.approx(E_fnv_t, abs=3e-2)
 
 
 def test_fnv_3d(in_tmp_dir):
@@ -268,83 +151,15 @@ def test_fnv_3d(in_tmp_dir):
     E_X = defect.calc.get_potential_energy()
     E_uncorr = E_X - E_0
     E_corr = E_uncorr + E_fnv
-
     print(E_uncorr, E_corr, E_fnv)
+
+    if 0:
+        profile = elc.calculate_potential_profile()
+        plot_potential(profile)
+
     assert E_fnv == pytest.approx(E_fnv_t, abs=3e-2)
     assert E_corr == pytest.approx(E_corr_t, abs=2e-2)
     assert E_uncorr == pytest.approx(E_uncorr_t, abs=2e-2)
-
-
-@pytest.mark.parametrize('P', [[[1, 0, 0], [1, 1, 0], [0, 0, 1]]])
-# [[1, 0, 0], [1, -1, 0], [0, 0, 1]] passes
-def test_fnv_cell(P, in_tmp_dir, gpaw_new):
-
-    if gpaw_new:
-        pytest.skip('Transformed cell [90, 90, 45] not supported by GPAW new')
-
-    P = np.array(P)
-
-    E_corr_t = 23.55
-    E_uncorr_t = 18.31
-    E_fnv_t = E_corr_t - E_uncorr_t
-
-    prs_path = Path('prs.gpw')
-    def_path = Path('def.gpw')
-
-    a0 = 5.628      # lattice parameter
-    sigma = 2 / (2.0 * np.sqrt(2.0 * np.log(2.0)))
-    epsilon = 12.7  # dielectric constant
-    charge = -3     # defect charge
-
-    params = {'mode': {'name': 'pw', 'ecut': 400},
-              'xc': 'LDA',
-              # avoid warning about grid symmetrization
-              'gpts': [40, 40, 40],
-              'kpts': {'size': (2, 2, 2), 'gamma': False},
-              'occupations': {'name': 'fermi-dirac', 'width': 0.01}}
-
-    calc_charged = GPAW(charge=charge, **params)
-    calc_neutral = GPAW(charge=0, **params)
-
-    pristine = bulk('GaAs', crystalstructure='zincblende', a=a0, cubic=True)
-    pristine = make_supercell(pristine, P)
-    pristine.calc = calc_neutral
-    pristine.get_potential_energy()
-    pristine.calc.write(prs_path)
-
-    defect = pristine.copy()
-    defect.pop(0)  # make a Ga vacancy
-    defect.calc = calc_charged
-    defect.get_potential_energy()
-    defect.calc.write(def_path)
-
-    phiR_prs = gather_electrostatic_potential(pristine.calc)
-    phiR_def = gather_electrostatic_potential(defect.calc)
-
-    # defect position
-    r0 = pristine.positions[0, :]
-
-    elc = ElectrostaticCorrections(phi_pristine=phiR_prs,
-                                   phi_defect=phiR_def,
-                                   r0=r0,
-                                   charge=charge,
-                                   sigma=sigma,
-                                   epsilon=epsilon,
-                                   method='full-planar',
-                                   atoms_pristine=pristine)
-    E_fnv = elc.calculate_correction()
-
-    E_0 = pristine.calc.get_potential_energy()
-    E_X = defect.calc.get_potential_energy()
-    E_uncorr = E_X - E_0
-    E_corr = E_uncorr + E_fnv
-
-    # changed tolerance to pass ortho-rhombic case
-    # switching symmetry off does not help to improve accuracy
-    print(E_uncorr, E_corr, E_fnv)
-    assert E_fnv == pytest.approx(E_fnv_t, abs=4e-2)
-    assert E_corr == pytest.approx(E_corr_t, abs=2e-1)
-    assert E_uncorr == pytest.approx(E_uncorr_t, abs=2e-1)
 
 
 if __name__ == "__main__":
