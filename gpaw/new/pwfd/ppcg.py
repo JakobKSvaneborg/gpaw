43ff28de35813f80f000dcc95a97e8b4de781dc3
SianK <silkr@dtu.dk>
2025-11-24 14:05:10 +0100

    No block-size limitations needed for hybrids.

--- gpaw/new/pwfd/ppcg.py
+++ gpaw/new/pwfd/ppcg.py
@@ -93,13 +93,6 @@ class PPCG(PWFDEigensolver):
             hamiltonian,
             converge_bands)
 
-        if not hamiltonian.band_local:
-            # assert band_comm.size == 1
-            blocksize = 100000
-            # raise NotImplementedError(
-            #     'PPCG only implemented for band local XCs,'
-            #     'use davidson instead')
-
         self.nbands = nbands
         self.wf_grid = wf_grid
         self.band_comm = band_comm
