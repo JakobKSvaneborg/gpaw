5e8b938828166b30ba2f18a3e3437fc9b46b3ff7
Luigi <luigibrusaferri@dhcp-10-196-164-142.fys.clients.local>
2025-11-25 14:47:48 +0100

    Updated Calculations and Positions

--- doc/tutorialsexercises/electronic/berry/gs_BaTiO3.py
+++ doc/tutorialsexercises/electronic/berry/gs_BaTiO3.py
@@ -3,22 +3,25 @@ from ase.optimize import BFGS
 from gpaw import GPAW, PW
 from ase.filters import FrechetCellFilter
 
-a = Atoms('BaTiO3',
-          cell=[4.00, 4.00, 4.00 * 1.054],
-          pbc=True,
-          scaled_positions=[[0.0, 0.0, 0.0],
-                            [0.5, 0.5, 0.5 + 0.018],
-                            [0.5, 0.5, 0.0],
-                            [0.5, 0.0, 0.5],
-                            [0.0, 0.5, 0.5]])
+params = {'mode': {'name': 'pw', 'ecut': 800},
+         'xc': 'PBE',
+         'kpts': {'size': (3, 3, 3), 'gamma': True},
+	 'convergence': {'forces': 1e-3, 'density': 1e-6},
+	 'txt': 'relax.txt'}
 
-a.calc = GPAW(mode=PW(800),
-              xc='PBE',
-              kpts={'size': (3, 3, 3), 'gamma': True},
-	      convergence={'forces': 1e-3, 'density': 1e-6},
-              txt='relax.txt')
+atoms = Atoms('BaTiO3',
+              cell=[3.97624117, 3.97624117, 4.29519778],
+              pbc=True,
+              scaled_positions=[[0.0, 0.0, 0.0292438349],
+                                [0.5, 0.5, 0.546102840],
+                                [0.5, 0.5, 0.965876790],
+                                [0.5, 0.0, 0.488398303],
+                                [0.0, 0.5, 0.488398303]])
 
-uf = FrechetCellFilter(a, mask=[1, 1, 1, 0, 0, 0])
+calc = GPAW(**params)
+atoms.calc = calc
+
+uf = FrechetCellFilter(atoms, mask=[1, 1, 1, 0, 0, 0])
 opt = BFGS(uf)
 opt.run(fmax=0.01)
-a.calc.write('BaTiO3.gpw')
+atoms.calc.write('BaTiO3.gpw')
