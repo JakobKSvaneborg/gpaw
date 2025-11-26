648ab3b05eeb9e57716a617e014d8990f0ad910d
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-11-26 12:39:27 +0100

    make it possible for parallel decorator to work on keywords with other names than comm

--- gpaw/mpi.py
+++ gpaw/mpi.py
@@ -851,7 +851,7 @@ def verify_ase_world():
 verify_ase_world()
 
 
-def parallel(func):
+def parallel(func=None, *, name='comm'):
     """Decorator for functions that take comm=world.
 
     We want this decorator so as to control access to world and prevent
@@ -860,15 +860,22 @@ def parallel(func):
     import functools
     from gpaw.mpi import world
 
+    if func is None:
+        def decorator(func):
+            return parallel(func, name=name)
+        return decorator
+
     @functools.wraps(func)
-    def wrapper(*args, comm=None, **kwargs):
+    def wrapper(*args, **kwargs):
+        comm = kwargs.get(name)
         if comm is None:
-            if _NO_TOUCH_WORLD:
-                raise DontDoThat(
-                    'Must call method with keyword comm=<communicator>'
-                )
             comm = world
-        return func(*args, comm=comm, **kwargs)
+            if comm is world and _NO_TOUCH_WORLD:
+                raise DontDoThat(
+                    f'Must call method with keyword {name}=<communicator> '
+                    'and communicator must not be gpaw.mpi.world')
+            kwargs[name] = comm
+        return func(*args, **kwargs)
 
     return wrapper
 
