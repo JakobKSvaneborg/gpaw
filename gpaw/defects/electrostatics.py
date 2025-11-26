6ee522940fcbd7d6684d2fec79ea40e3724ebc21
Silvan Kretschmer <silvan_kretschmer@gmx.de>
2025-11-26 15:56:13 +0100

    Replaced None -> np.newaxis, renamed to s0_c

--- gpaw/defects/electrostatics.py
+++ gpaw/defects/electrostatics.py
@@ -219,9 +219,8 @@ class ElectrostaticCorrections():
         # assumes self.r_vR being on a regular grid
         # evaluate grid index of cartesian vector
         # convert to reduced (fractional) coordinates
-        s0_v = np.linalg.solve(self.cell_cv.T, r0_v)
-        s0_v = self.atoms_prs.get_scaled_positions()[0, :]
-        return np.array(np.round(ng_v * s0_v, 0), dtype=int) % ng_v
+        s0_c = np.linalg.solve(self.cell_cv.T, r0_v)
+        return np.array(np.round(ng_v * s0_c, 0), dtype=int) % ng_v
 
     def bulk_atom_average(self):
         # in pristine obtain atom positions closest to the defect_site
@@ -297,8 +296,9 @@ class ElectrostaticCorrections():
         iy = np.linspace(0, ny - 1, nsample, dtype=int)
         igx, igy = np.meshgrid(ix, iy)
 
-        z_vR = self.rc_vR[:, igx, igy, :] - self.r0[:, None, None, None]
-        phi_model = self.calculate_model_potential(z_vR)
+        z0_vR = self.rc_vR[:, igx, igy, :]
+        zr0_vR = z0_vR - self.r0[(...,) + (np.newaxis,) * 3]
+        phi_model = self.calculate_model_potential(zr0_vR)
         phiz_model = np.mean(phi_model, axis=(0, 1))
 
         zaxis = self.rc_vR[2, 0, 0, :]
@@ -333,8 +333,8 @@ class ElectrostaticCorrections():
         r_vR = self.rc_vR[:, ix, iy, iz]
 
         # get model potential inside the averaging region
-        r_vR = r_vR - self.r0[:, None, None, None]
-        phi_model = self.calculate_model_potential(r_vR)
+        rr0_vR = r_vR - self.r0[(...,) + (np.newaxis,) * 3]
+        phi_model = self.calculate_model_potential(rr0_vR)
 
         dphi = phi_model - (phi_def - phi_prs)
         dphi_avg = np.average(dphi)
