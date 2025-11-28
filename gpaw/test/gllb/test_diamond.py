a93b90e8287778772addb2958066f715debebf1a
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-11-28 15:22:56 +0100

    Stop testing deprecated GLLB-stuff

--- gpaw/test/gllb/test_diamond.py
+++ gpaw/test/gllb/test_diamond.py
@@ -14,13 +14,11 @@ from ase.build import bulk
 
 from gpaw import GPAW, Davidson, Mixer
 from gpaw.mpi import world
-from gpaw.old.calculator import DeprecatedParameterWarning
 
 
 @pytest.mark.gllb
 @pytest.mark.libxc
-@pytest.mark.parametrize('deprecated_syntax', [False, True])
-def test_gllb_diamond(in_tmp_dir, deprecated_syntax):
+def test_gllb_diamond(in_tmp_dir):
     xc = 'GLLBSC'
     KS_gap_ref = 4.180237125868162
     QP_gap_ref = 5.469387490357182
@@ -46,18 +44,11 @@ def test_gllb_diamond(in_tmp_dir, deprecated_syntax):
     atoms.calc = calc
     atoms.get_potential_energy()
 
-    if deprecated_syntax:
-        with pytest.warns(DeprecationWarning):
-            # Calculate the discontinuity directly
-            response = calc.hamiltonian.xc.xcs['RESPONSE']
-            response.calculate_delta_xc()
-            KS_gap, dxc = response.calculate_delta_xc_perturbation()
-    else:
-        # Calculate the discontinuity directly
-        homo, lumo = calc.get_homo_lumo()
-        response = calc.hamiltonian.xc.response
-        dxc_pot = response.calculate_discontinuity_potential(homo, lumo)
-        KS_gap, dxc = response.calculate_discontinuity(dxc_pot)
+    # Calculate the discontinuity directly
+    homo, lumo = calc.get_homo_lumo()
+    response = calc.hamiltonian.xc.response
+    dxc_pot = response.calculate_discontinuity_potential(homo, lumo)
+    KS_gap, dxc = response.calculate_discontinuity(dxc_pot)
 
     QP_gap = KS_gap + dxc
     print('KS gap direct', KS_gap)
@@ -76,44 +67,20 @@ def test_gllb_diamond(in_tmp_dir, deprecated_syntax):
     # Get the accurate KS-band gap
     homo, lumo = bs_calc.get_homo_lumo()
 
-    if deprecated_syntax:
-        with pytest.warns(DeprecationWarning):
-            from ase.units import Ha
+    # Calculate the discontinuity potential with accurate band gap
+    response = calc.hamiltonian.xc.response
+    dxc_pot = response.calculate_discontinuity_potential(homo, lumo)
 
-            from gpaw.xc.gllb.c_response import ResponsePotential
+    # Calculate the discontinuity using the band structure calculator
+    bs_response = bs_calc.hamiltonian.xc.response
+    KS_gap, dxc = bs_response.calculate_discontinuity(dxc_pot)
 
-            # Calculate the discontinuity potential with accurate band gap
-            response = calc.hamiltonian.xc.xcs['RESPONSE']
-            response.calculate_delta_xc(homolumo=homolumo / Ha)
-
-            # Calculate the discontinuity using the band structure calculator
-            bs_response = bs_calc.hamiltonian.xc.xcs['RESPONSE']
-            # Copy response potential to avoid recalculation
-            pot = ResponsePotential(response,
-                                    response.Dxc_vt_sG,
-                                    None,
-                                    response.Dxc_D_asp,
-                                    response.Dxc_Dresp_asp)
-            pot.redistribute(bs_response)
-            bs_response.Dxc_vt_sG = pot.vt_sG
-            bs_response.Dxc_D_asp = pot.D_asp
-            bs_response.Dxc_Dresp_asp = pot.Dresp_asp
-            KS_gap, dxc = bs_response.calculate_delta_xc_perturbation()
-    else:
-        # Calculate the discontinuity potential with accurate band gap
-        response = calc.hamiltonian.xc.response
-        dxc_pot = response.calculate_discontinuity_potential(homo, lumo)
-
-        # Calculate the discontinuity using the band structure calculator
-        bs_response = bs_calc.hamiltonian.xc.response
-        KS_gap, dxc = bs_response.calculate_discontinuity(dxc_pot)
-
-        # Test alternative (testing) implementation
-        if world.size == 1:
-            with pytest.warns(Warning):
-                dxc_ave = bs_response.calculate_discontinuity_from_average(
-                    dxc_pot, 0, True)
-            assert dxc_ave == pytest.approx(dxc_ave_ref, abs=1e-4)
+    # Test alternative (testing) implementation
+    if world.size == 1:
+        with pytest.warns(Warning):
+            dxc_ave = bs_response.calculate_discontinuity_from_average(
+                dxc_pot, 0, True)
+        assert dxc_ave == pytest.approx(dxc_ave_ref, abs=1e-4)
 
     QP_gap = KS_gap + dxc
     print('KS gap', KS_gap)
