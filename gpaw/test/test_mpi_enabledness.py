f31769797f62ea3235336af637b2b57b39356846
Ask Hjorth Larsen <asklarsen@gmail.com>
2025-12-08 11:05:57 +0100

    add a test to verify that MPI is enabled or not under the intended circumstances

---
+++ gpaw/test/test_mpi_enabledness.py
@@ -0,0 +1,43 @@
+"""Test that MPI is initialized if and only if we run "gpaw -P<n> python."""
+
+import subprocess
+import sys
+
+import pytest
+
+
+@pytest.fixture(autouse=True)
+def skip_if_mpi_initialized(comm):
+    from gpaw.mpi import SerialCommunicator
+
+    if not isinstance(comm, SerialCommunicator):
+        pytest.skip('Cannot create non-MPI process from MPI process')
+
+
+def test_ordinary_python():
+    assert subprocess.call([sys.executable, __file__]) == MPI_MISSING
+
+
+def test_gpaw_python():
+    # Note: gpaw -P1 does not actually enable MPI.  Is that the best?
+    assert subprocess.call(['gpaw', '-P2', 'python', __file__]) == MPI_PRESENT
+
+
+MPI_MISSING = 234
+MPI_PRESENT = 235
+
+
+if __name__ == '__main__':
+    # MPI was called if we did it in _broadcast_imports,
+    # or if someone initialized mpi4py (which we don't know unless we did it,
+    # but if we did it, it was also in _broadcast_imports.
+    #
+    # We cannot completely guarantee that nobody created a _gpaw.Communicator()
+    # which is what actually causes GPAW to initialize MPI, but this should
+    # be the best point to intercept the initialization.
+    from gpaw._broadcast_imports import world
+
+    if world is None:
+        sys.exit(MPI_MISSING)
+    else:
+        sys.exit(MPI_PRESENT)
