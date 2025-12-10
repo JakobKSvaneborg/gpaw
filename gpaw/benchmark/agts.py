567fdcd4d84dae0beaa79b2c7663a0950015a7bc
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-12-10 11:53:12 +0100

    AGTS fix

--- gpaw/benchmark/agts.py
+++ gpaw/benchmark/agts.py
@@ -15,5 +15,5 @@ def workflow():
                 (dir / 'params.json').write_text(
                     '{"mode": "fd", "h": 0.15}\n')
         os.chdir(dir)
-        wf(skip=['ErGe-2M', 'Fe8O8-3M'], mode=mode)
+        wf(skip=['ErGe-2M', 'Fe8O8-3M'])
         os.chdir('..')
