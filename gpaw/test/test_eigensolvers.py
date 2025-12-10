28ff6f27c921300950ca5aadc84df44131fba22a
Silvan Kretschmer <silvan_kretschmer@gmx.de>
2025-12-10 12:13:58 +0100

    Made one test for all eigensolvers

---
+++ gpaw/test/test_eigensolvers.py
@@ -0,0 +1,49 @@
+import pytest
+from ase import Atoms
+from ase.build import bulk
+
+from gpaw import GPAW
+
+@pytest.mark.parametrize('element', ['Al', 'Si'])
+@pytest.mark.parametrize('eigensolver', ['davidson', 'ppcg', 'dir_opt'])
+def test_eigensolver(element, eigensolver, gpaw_new):
+    if not gpaw_new:
+        pytest.skip('Only implemented for new GPAW')
+
+    # element = 'Si'
+    # eigensolver = 'davidson'
+    # eigensolver = 'ppcg'
+    # eigensolver = 'dir_opt'
+    energy_tolerance = 5e-5
+    eig_tolerance = 1e-3
+
+    if element == 'Si':
+        a = 5.431
+        atoms = bulk('Si', 'diamond', a=a)
+        e0_t = -11.7125397
+        # occupied eigenvalues
+        nocc = 4
+        eig_t = [-4.08139256, -1.23190614, 1.59863588, 2.95648716]
+    else:
+        a = 4.05
+        d = a / 2**0.5
+        atoms = Atoms('Al2', positions=[[0, 0, 0], [.5, .5, .5]], pbc=True)
+        atoms.set_cell((d, d, a), scale_atoms=True)
+        e0_t = -6.9786673
+        nocc = 3
+        eig_t = [-1.36400629,  2.97388703,  6.63549518]
+
+    params = {'mode': {'name': 'pw', 'ecut': 400},
+              'nbands': 2 * 8,
+              'kpts': {'size': [2, 2, 2]},
+              'spinpol': False,
+              'convergence': {'eigenstates': 1e-8,
+                              'energy': 1e-5}}
+
+    calc = GPAW(**params)
+    atoms.calc = calc
+    e0 = atoms.get_potential_energy()
+    eig = atoms.calc.get_eigenvalues()[:nocc]
+
+    assert e0 == pytest.approx(e0_t, abs=energy_tolerance)
+    assert eig == pytest.approx(eig_t, abs=eig_tolerance)
