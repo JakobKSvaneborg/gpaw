a66d43cf0a3b98ac0342b8dd14b7fdb08e0fb263
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-12-11 10:31:23 +0100

    Revert to old way

--- gpaw/test/utilities/test_makebadge.py
+++ gpaw/test/utilities/test_makebadge.py
@@ -2,7 +2,7 @@ from gpaw.doctools.makebadge import main
 
 
 def test_coverage_badge(capsys):
-    main(['(ignored)', 'coverage', '42'])
+    main(['(ignored)', '42'])
     svg, _ = capsys.readouterr()
     assert 'Coverage' in svg
     assert '42%' in svg
