0565409378b93a73b3b4fc96e7a518c0f954d391
Jens Jørgen Mortensen <jjmo@dtu.dk>
2025-12-12 07:29:44 +0100

    Add benchmark check task to AGTS

--- gpaw/benchmark/agts.py
+++ gpaw/benchmark/agts.py
@@ -4,6 +4,7 @@ import os
 
 
 def workflow():
+    from myqueue.workflow import run
     for mode in ['pw', 'lcao', 'fd']:
         dir = Path(mode)
         if not dir.is_dir():
@@ -15,5 +16,10 @@ def workflow():
                 (dir / 'params.json').write_text(
                     '{"mode": "fd", "h": 0.15}\n')
         os.chdir(dir)
-        wf(skip=['ErGe-2M', 'Fe8O8-3M'])
+        handles = wf(skip=['ErGe-2M', 'Fe8O8-3M'])
         os.chdir('..')
+        run(function=check, args=[mode], deps=handles)
+
+
+def check(mode: str) -> None:
+    assert True
