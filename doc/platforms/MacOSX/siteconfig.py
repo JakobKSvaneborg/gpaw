532f15c0ac39ef9335373deebee97dc3daeff769
Jakob Schiotz <schiotz@fysik.dtu.dk>
2026-01-12 13:29:14 +0100

    Update installation instructions for MacOS (Homebrew).

--- doc/platforms/MacOSX/siteconfig.py
+++ doc/platforms/MacOSX/siteconfig.py
@@ -1,2 +1,17 @@
 fftw = True
-libraries = ['xc', 'fftw3']
+scalapack = True
+libraries = ['xc', 'openblas', 'fftw3', 'scalapack']
+
+library_dirs = ['/usr/local/lib']
+include_dirs = ['/usr/local/include']
+
+# OpenBLAS
+library_dirs += ['/usr/local/opt/openblas/lib']
+include_dirs += ['/usr/local/opt/openblas/include']
+
+# FFTW
+library_dirs += ['/usr/local/opt/fftw/lib']
+include_dirs += ['/usr/local/opt/fftw/include']
+
+# Scalapack
+library_dirs += ['/usr/local/opt/scalapack/lib']
