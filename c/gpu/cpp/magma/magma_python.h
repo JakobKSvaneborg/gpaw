217f1154bc8b7740fa29b8457ae1d6728f664b51
Lauri Niemi <lauri.niemi@csc.fi>
2026-01-21 15:31:38 +0200

    Move also magma init/finalize functions to _gpaw.gpu.magma, and call them from Python side instead of doing them in old C code

--- c/gpu/cpp/magma/magma_python.h
+++ c/gpu/cpp/magma/magma_python.h
@@ -6,9 +6,3 @@
 
 // Binds MAGMA wrappers as a submodule (_gpaw.gpu.magma)
 bool bind_magma_submodule(pybind11::module_ gpu_module);
-
-/* Initializes MAGMA library. Must be called come AFTER any calls to cudaSetValidDevices
-* and cudaSetDeviceFlags. Call only if GPUs are available.
-*/
-GPAW_GPU_LINKAGE void gpaw_magma_init();
-GPAW_GPU_LINKAGE void gpaw_magma_finalize();
