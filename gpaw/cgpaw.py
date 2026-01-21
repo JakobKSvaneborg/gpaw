c286741727c67abdbe7fbaecbc49f7e9159dff4c
Lauri Niemi <lauri.niemi@csc.fi>
2026-01-21 15:17:40 +0200

    make mypy happy

--- gpaw/cgpaw.py
+++ gpaw/cgpaw.py
@@ -1,13 +1,12 @@
 from gpaw import GPAW_NO_C_EXTENSION
 
 if GPAW_NO_C_EXTENSION:
-    have_magma = False
     import gpaw.purepython as _gpaw
 else:
     import _gpaw  # type: ignore[no-redef]
 
 try:
-    import _gpaw.gpu.magma
+    import _gpaw.gpu.magma  # type: ignore[no-redef]
     have_magma = True
 except ImportError:
     have_magma = False
