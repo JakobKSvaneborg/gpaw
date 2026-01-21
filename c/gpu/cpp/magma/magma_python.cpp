84e8fb41dff43af2f1dd0416f21bddf32cda94ae
Lauri Niemi <lauri.niemi@csc.fi>
2026-01-21 11:51:16 +0200

    Rewrote magma wrapper for the version that takes Numpy arrays as input/output, and bound it to the new magma submodule

--- c/gpu/cpp/magma/magma_python.cpp
+++ c/gpu/cpp/magma/magma_python.cpp
@@ -1,9 +1,13 @@
+#include "python_utils.h"
 #include "magma_python.h"
-#include "gpu/cpp/pyarray_utils.hpp"
 #include "magma_gpaw.hpp"
+#include "magma_solvers.hpp"
+#include "gpu/cpp/pyarray_utils.hpp"
 #include "gpaw_utils.h"
 
 #include <cassert>
+
+namespace py = pybind11;
 
 namespace gpaw
 {
@@ -19,13 +23,166 @@ static MagmaStaticData magma_static_data;
 This also takes care of convention difference between Numpy/Cupy arrays (C-style, row major)
 and Magma arrays (Fortran/LAPACK style, column major).
 So 'L' in Python conventions actually means MagmaUpper. */
-static magma_uplo_t get_magma_uplo(char* in_uplo_str)
+static magma_uplo_t get_magma_uplo(const std::string& uplo)
 {
-    assert((strcmp(in_uplo_str, "L") == 0 || strcmp(in_uplo_str, "U") == 0)
-        && "Invalid UPLO");
+    if (!(uplo == "L" || uplo == "U"))
+    {
+        throw std::invalid_argument("Invalid UPLO, must be 'L' or 'U'");
+    }
 
-    return strcmp(in_uplo_str, "L") == 0 ? MagmaUpper : MagmaLower;
+    // Handle array convention difference
+    return uplo == "L" ? MagmaUpper : MagmaLower;
 }
+
+static void check_gpu_request(int64_t requested_num_gpus)
+{
+    if (requested_num_gpus <= 0)
+    {
+        throw std::invalid_argument("requested_num_gpus <= 0");
+    }
+
+    if (requested_num_gpus > (int64_t)magma_static_data.num_gpus)
+    {
+        char error_msg[256];
+        snprintf(error_msg, sizeof(error_msg),
+            "Requested %ld GPUs to a MAGMA routine but MAGMA only sees %ld.\n",
+           (long)requested_num_gpus, (long)(magma_static_data.num_gpus)
+        );
+
+        throw std::invalid_argument(error_msg);
+    }
+}
+
+/* Checks that the given matrix and eigenvalue arrays have matching dtypes (symmetric/Hermitian solver).
+* If matrix dtype is complex<T> -> eigenvalue dtype must be T.
+* If matrix dtype is T -> eigenvalue dtype must be T.
+* Throws on failure. Note that integer dtypes may pass this.
+*/
+static void ensure_dtype_match_eigh(py::array matrix, py::array eigvals)
+{
+    const py::dtype matrix_dtype = matrix.dtype();
+    const py::dtype eigval_dtype = eigvals.dtype();
+
+    // Complex case
+    if (matrix_dtype.kind() == 'c')
+    {
+        if (matrix_dtype.is(py::dtype::of<std::complex<double>>()))
+        {
+            if (!eigval_dtype.is(py::dtype::of<double>()))
+            {
+                throw std::invalid_argument("Matrix/eigenvalue dtype mismatch (complex double-precision)");
+            }
+        }
+        else if (matrix_dtype.is(py::dtype::of<std::complex<float>>()))
+        {
+            if (!eigval_dtype.is(py::dtype::of<float>()))
+            {
+                throw std::invalid_argument("Matrix/eigenvalue dtype mismatch (complex single-precision)");
+            }
+        }
+    }
+    // Real case
+    else if (!eigval_dtype.is(matrix_dtype))
+    {
+        throw std::invalid_argument("Matrix/eigenvalue mismatch (real matrix)");
+    }
+}
+
+/* We could use the templated py::array_t<T> type here to enforce matching dtypes for the matrix and eigenvalue arrays.
+* However pybind11 does implicit conversions, meaning array copies, if the input is non-conforming.
+* This is bad for performance and is also not what we want, since we need to mutate the input arrays directly.
+* Therefore the following uses type-erased py::array objects as inputs, which is essentially just PyArrayObject*.
+*/
+
+static void eigh_magma_numpy(
+    py::array inout_matrix,
+    py::array inout_eigvals,
+    const py::str& uplo,
+    int64_t num_gpus)
+{
+    check_gpu_request(num_gpus);
+
+    if (inout_matrix.ndim() != 2 || inout_matrix.shape()[0] != inout_matrix.shape()[1])
+    {
+        throw std::invalid_argument("Input matrix must be an N x N matrix");
+    }
+    const py::ssize_t matrix_size = inout_matrix.shape()[0];
+
+    if (inout_eigvals.ndim() != 1 || inout_eigvals.shape()[0] != matrix_size)
+    {
+        throw std::invalid_argument("Input array for eigenvalues must be 1D and have length N (if the matrix is N x N)");
+    }
+
+    if (!gpaw::is_c_contiguous(inout_matrix) || inout_eigvals.strides(0) != inout_eigvals.itemsize())
+    {
+        throw std::invalid_argument("Input matrix and eigenvalue arrays must be C-contiguous");
+    }
+
+    ensure_dtype_match_eigh(inout_matrix, inout_eigvals);
+
+    MagmaEighContext solver_context;
+    solver_context.matrix_size = static_cast<magma_int_t>(matrix_size);
+    solver_context.matrix_lda = solver_context.matrix_size;
+    solver_context.uplo = get_magma_uplo(uplo.cast<std::string>());
+    solver_context.jobz = MagmaVec; // Always do eigenvectors
+
+    const py::dtype dtype = inout_matrix.dtype();
+    magma_int_t status;
+
+    switch (dtype.normalized_num())
+    {
+        case py::dtype::num_of<float>():
+            status = magma_symmetric_solver_host<float>(
+                solver_context,
+                static_cast<float*>(inout_matrix.mutable_data()),
+                static_cast<float*>(inout_eigvals.mutable_data())
+            );
+            break;
+        case py::dtype::num_of<double>():
+            status = magma_symmetric_solver_host<double>(
+                solver_context,
+                static_cast<double*>(inout_matrix.mutable_data()),
+                static_cast<double*>(inout_eigvals.mutable_data())
+            );
+            break;
+        case py::dtype::num_of<std::complex<float>>():
+            status = magma_hermitian_solver_host<float>(
+                solver_context,
+                static_cast<magmaComplex<float>*>(inout_matrix.mutable_data()),
+                static_cast<float*>(inout_eigvals.mutable_data())
+            );
+            break;
+        case py::dtype::num_of<std::complex<double>>():
+            status = magma_hermitian_solver_host<double>(
+                solver_context,
+                static_cast<magmaComplex<double>*>(inout_matrix.mutable_data()),
+                static_cast<double*>(inout_eigvals.mutable_data())
+            );
+            break;
+    default:
+        throw std::invalid_argument("Unsupported matrix dtype");
+    }
+
+    if (status < 0)
+    {
+        throw std::runtime_error("Invalid input to MAGMA solver at position " + std::to_string(-status));
+    }
+    else if (status > 0)
+    {
+        PyErr_WarnEx(PyExc_RuntimeWarning,
+            "MAGMA eigensolver failed to converge",
+            1
+        );
+    }
+}
+
+static void eigh_magma_cupy(
+    PyDeviceArray& inout_matrix,
+    PyDeviceArray& inout_eigvals,
+    const py::str& uplo)
+{
+}
+
 
 } // namespace gpaw
 
@@ -48,7 +205,6 @@ void gpaw_magma_finalize()
 
 bool bind_magma_submodule(pybind11::module_ gpu_module)
 {
-    namespace py = pybind11;
 
     if (!gpu_module || gpu_module == Py_None)
     {
@@ -60,8 +216,23 @@ bool bind_magma_submodule(pybind11::module_ gpu_module)
     {
         return false;
     }
-
     py::module_ submodule = m.def_submodule("magma", "MAGMA bindings for GPAW");
+
+    submodule.def("eigh_magma_numpy", &gpaw::eigh_magma_numpy,
+    py::arg("inout_matrix"), py::arg("inout_eigenvalues"),
+    py::arg("uplo"), py::arg("num_gpus"),
+    R"(
+        Solves eigensystem on the GPU with Numpy input/output. This is an in-place solver:
+        the input matrix will be overwritten with resulting eigenvectors. The input eigenvalue array
+        must already be allocated to correct size (its contents don't matter).
+        Input matrix is in Numpy/Scipy conventions. Output is still in MAGMA (Fortran) convention,
+        so you will need to conjugate transpose to get back to Numpy conventions for eigenvectors.
+
+        Passing num_gpus > 1 will instruct the solver to utilize multiple GPUs. This can be beneficial for large matrices (N >= 10k).
+        Note though that the GPUs must be directly reachable from the same Cuda/HIP context, ie. this is a single-node solver without Scalapack-like support.
+    )");
+
+    submodule.def("eigh_magma_cupy", &gpaw::eigh_magma_cupy);
 
     return true;
 }
