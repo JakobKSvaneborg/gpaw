327f9cb4323ef44a8b90c10d58e7552702ae6e64
Lauri Niemi <lauri.niemi@csc.fi>
2026-01-23 09:18:13 +0200

    Catch also ModuleNotFoundError when importing stuff from C++ extension.
    This is currently needed because if GPAW_GPU is not enabled during build, _gpaw will actually be a simple module instead of a package. So from _gpaw.gpu import XXX will raise ModuleNotFoundError instead of ImportError.

--- gpaw/cgpaw/gpu/__init__.py
+++ gpaw/cgpaw/gpu/__init__.py
@@ -1,6 +1,4 @@
-from gpaw import GPAW_NO_C_EXTENSION
-
-if GPAW_NO_C_EXTENSION:
-    raise ImportError("No purepython versions of GPU extensions")
-else:
+try:
     from _gpaw.gpu import *  # noqa: F401, F403
+except (ImportError, ModuleNotFoundError):
+    pass
--- gpaw/cgpaw/gpu/magma.py
+++ gpaw/cgpaw/gpu/magma.py
@@ -3,5 +3,5 @@ have_magma = False
 try:
     from _gpaw.gpu.magma import *  # noqa: F401, F403
     have_magma = True
-except ImportError:
+except (ImportError, ModuleNotFoundError):
     pass
