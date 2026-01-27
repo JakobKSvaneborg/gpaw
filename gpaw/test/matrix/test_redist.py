b99d4c4c0494415bb2ab43613b69c55965901681
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-01-27 21:07:24 +0100

    Check redistributed numbers

--- gpaw/test/matrix/test_redist.py
+++ gpaw/test/matrix/test_redist.py
@@ -1,6 +1,7 @@
 import pytest
 from gpaw.core.matrix import Matrix
 from gpaw.mpi import world
+import numpy as np
 
 
 @pytest.mark.skipif(world.size == 1, reason='Only run in parallel')
@@ -13,16 +14,17 @@ def test_redist():
     band_comm = world.new_communicator(range(r, world.size, 2))
     n = 8
     M = Matrix(n, n, dist=band_comm)
-    M.data[:] = world.rank
-    print('M', world.rank, band_comm.rank, domain_comm.rank, M)
+    M.data[:] = np.arange(*M.dist.my_row_range())[:, np.newaxis]
     M2 = Matrix(n, n,
                 dist=(world, B, D, n // B, n),
                 data=M.data if domain_comm.rank == 0 else None)
-    print(world.rank, band_comm.rank, domain_comm.rank, M2.data.flat[:])
     M3 = Matrix(n, n, dist=(world, B, D, 2, 2))
-    print('M3', world.rank, band_comm.rank, domain_comm.rank, M3)
     M2.redist(M3)
-    print(M3.data)
+    M4 = Matrix(n, n, dist=(world, 1, 1))
+    M3.redist(M4)
+    print(M4.data)
+    if world.rank == 0:
+        assert (M4.data.T == np.arange(n)).all()
 
 
 if __name__ == '__main__':
