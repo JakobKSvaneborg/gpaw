26cd30df729c08ec42cfffc356016022e44ba3cc
Martin Ovesen <martin.ovesen@outlook.dk>
2026-02-01 00:45:40 +0100

    Remove usage of mpi fixture in one more test

--- gpaw/test/symmetry/test_optimal_mp_grid.py
+++ gpaw/test/symmetry/test_optimal_mp_grid.py
@@ -3,7 +3,7 @@ from gpaw.bztools import optimal_monkhorst_pack_grid
 from ase import Atoms
 
 
-def test_optimal_monkhorst_pack_grid(gpw_files, mpi):
+def test_optimal_monkhorst_pack_grid(gpw_files):
     atoms = Atoms("H", cell=[6, 3, 8], pbc=True)
 
     kpts_dict = optimal_monkhorst_pack_grid(atoms, kptdensity=3.0)
@@ -26,7 +26,7 @@ def test_optimal_monkhorst_pack_grid(gpw_files, mpi):
     assert kpts_dict['gamma'] is False
 
     for system, kpts in [('si_pw', (16, 16, 16)), ('mos2_pw', (18, 18, 1))]:
-        atoms = mpi.GPAW(gpw_files[system]).atoms
+        atoms = GPAW(gpw_files[system]).atoms
         kpts_dict = optimal_monkhorst_pack_grid(atoms,
                                                 kptdensity=8.0,
                                                 force_gamma=True,
@@ -36,7 +36,7 @@ def test_optimal_monkhorst_pack_grid(gpw_files, mpi):
         assert (kpts_dict['size'] == kpts).all()
 
     for system, kpts in [('si_pw', (8, 8, 8)), ('mos2_pw', (12, 12, 1))]:
-        atoms = mpi.GPAW(gpw_files[system]).atoms
+        atoms = GPAW(gpw_files[system]).atoms
         print(system)
         kpts_dict = optimal_monkhorst_pack_grid(atoms,
                                                 kptdensity=4.0,
