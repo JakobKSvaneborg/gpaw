2de95cc936f26e0f972152fe106849c5c4ee3e28
Ask Hjorth Larsen <asklarsen@gmail.com>
2026-02-01 22:30:02 +0100

    use another communicator than world with gpwfiles fixture

--- gpaw/test/test_generate_gpwfiles.py
+++ gpaw/test/test_generate_gpwfiles.py
@@ -2,10 +2,13 @@ import pytest
 
 
 @pytest.mark.generate_gpw_files
-def test_generate_gpwfiles(all_gpw_files):
+def test_generate_gpwfiles(no_touch_world, all_gpw_files):
     """Dummy test which results in the creation of all gpw file fixtures.
 
     This test triggers a parametrized fixture which results in
     all gpw files of the gpw_files fixture being accessed.
 
     See the all_gpw_files fixture."""
+
+    from gpaw.mpi import world
+    world.sum_scalar(42)
