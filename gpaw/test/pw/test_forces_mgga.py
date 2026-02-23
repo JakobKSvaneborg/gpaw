a82f5423e1553bfec075957521ad2b3f988dbffd
Aku Lempelto <11915395-lempelto@users.noreply.gitlab.com>
2026-02-23 18:29:21 +0200

    Modify test for MGGA forces

--- gpaw/test/pw/test_forces_mgga.py
+++ gpaw/test/pw/test_forces_mgga.py
@@ -11,22 +11,23 @@ def test_pw_forces_mgga(gpaw_new):
     a = 4.05
     d = a / 2**0.5
     bulk = Atoms([Atom('Al', (0, 0, 0)),
-                  Atom('Al', (0.49, 0.48, 0.47))], pbc=True)
+                  Atom('Al', (0.48, 0.48, 0.48))], pbc=True)
     bulk.set_cell((d, d, a), scale_atoms=True)
-    bulk.calc = GPAW(mode='pw',
+
+    bulk.calc = GPAW(mode={'name': 'pw', 'ecut': 800},
                      xc='revTPSS',
-                     nbands=2 * 8,
-                     kpts=(2, 2, 2))
+                     convergence={'forces': 1e-5},
+                     kpts=(3, 3, 3),
+                     symmetry='off')
 
     f_test = bulk.get_forces()
     parprint('Forces:\n', f_test)
     assert pytest.approx(f_test) != [0., 0., 0.]
-    # Pre-calculated forces:
-    f_revTPSS = np.array([[-0.10154195, -0.20316854, -0.17164669],
-                          [0.10132630, 0.20300123, 0.17154444]])
 
+    # Pre-calculated forces (ase.calculators.fd)
+    f_revTPSS = np.array([[-0.12277113, -0.12294365, -0.13208065],
+                          [0.12294729, 0.12294381, 0.13207365]])
     f_err = f_test - f_revTPSS
 
-    # New GPAW and old GPAW disagree by ca. ± 7e-4 so I'll set treshold to 1e-3
-    assert np.all(abs(f_err) < 1e-3)
+    assert np.all(abs(f_err) < 1e-2)
     parprint('Error in forces:\n', f_err)
