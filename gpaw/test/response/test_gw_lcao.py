0eea42d03fc3c74e012bc5dd3f35719aa6934467
Amalie Sondersted <amhso@dtu.dk>
2026-02-24 10:12:02 +0100

    LCAO to PW conversion before GW calculation

--- gpaw/test/response/test_gw_lcao.py
+++ gpaw/test/response/test_gw_lcao.py
@@ -1,16 +1,24 @@
 import pytest
 from gpaw.response.g0w0 import G0W0
+from gpaw.dft import GPAW
 
 
 @pytest.mark.ci
 @pytest.mark.response
 @pytest.mark.old_gpaw_only
 def test_lcao_gw(in_tmp_dir, gpw_files):
-    gw = G0W0(gpw_files['diamond_lcao'],
+
+    dft = GPAW(gpw_files['diamond_lcao']).dft
+    dft.change(eigensolver={})  # remove SCS solver which PW-mode doesn't like
+    dft.change_mode('pw')
+    dft.ase_calculator().write('diamond_pw.gpw', 'all')
+
+    gw = G0W0('diamond_pw.gpw',
               integrate_gamma='WS',
               ecut=100,
               eta=0.1,
               bands=(0, 8))
+
     res = gw.calculate()
 
     qp = res['qp']
@@ -35,5 +43,5 @@ def test_lcao_gw(in_tmp_dir, gpw_files):
 
     assert eps[0][0][4] == pytest.approx(17.5784, abs=0.01)
     assert f[0][0][4] == pytest.approx(0.0, abs=0.01)
-    assert qp[0][0][4] == pytest.approx(21.522, abs=0.01)
-    assert Z[0][0][4] == pytest.approx(2.046, abs=0.01)
+    assert qp[0][0][4] == pytest.approx(20.370, abs=0.01)
+    assert Z[0][0][4] == pytest.approx(1.339, abs=0.01)
