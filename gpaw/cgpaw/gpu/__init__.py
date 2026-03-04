66e77cce79a42855d4ccfa07e6de8d887a185393
Lauri Niemi <lauri.niemi@csc.fi>
2026-03-04 14:28:24 +0200

    Guard magma imports with 'not GPAW_NO_C_EXTENSION' so that magma will effectively not be available if that envvar is enabled

--- gpaw/cgpaw/gpu/__init__.py
+++ gpaw/cgpaw/gpu/__init__.py
@@ -1,4 +1,14 @@
-try:
-    from _gpaw.gpu import *  # noqa: F401, F403
-except (ImportError, ModuleNotFoundError):
+from gpaw import GPAW_NO_C_EXTENSION
+
+
+_ok = True
+if not GPAW_NO_C_EXTENSION:
+    try:
+        from _gpaw.gpu import *  # noqa: F401, F403
+    except (ImportError, ModuleNotFoundError):
+        _ok = False
+
+
+if not GPAW_NO_C_EXTENSION or not _ok:
+    # ... purepython stuff ...
     pass
--- gpaw/cgpaw/gpu/magma.py
+++ gpaw/cgpaw/gpu/magma.py
@@ -1,7 +1,19 @@
-have_magma = False
+from gpaw import GPAW_NO_C_EXTENSION
 
-try:
-    from _gpaw.gpu.magma import *  # noqa: F401, F403
-    have_magma = True
-except (ImportError, ModuleNotFoundError):
-    pass
+_ok = True
+
+if not GPAW_NO_C_EXTENSION:
+
+    have_magma = False
+
+    try:
+        from _gpaw.gpu.magma import *  # noqa: F401, F403
+        have_magma = True
+    except (ImportError, ModuleNotFoundError):
+        _ok  = False
+
+
+if GPAW_NO_C_EXTENSION or not _ok:
+
+    def available() -> bool:
+        return False
