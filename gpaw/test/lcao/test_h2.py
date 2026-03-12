5f8ec105aed860bb661cbfd9c172792febc21075
Jens Jørgen Mortensen <jjmo@dtu.dk>
2026-03-12 11:49:57 +0100

    Finish new H2-DFT test

--- gpaw/test/lcao/test_h2.py
+++ gpaw/test/lcao/test_h2.py
@@ -1,16 +1,19 @@
-from gpaw.dft import DFT
+import pytest
 from ase.build import molecule
+from gpaw.dft import DFT
 
 
+@pytest.mark.serial
 def test_h2(new=True):
     atoms = molecule('H2', cell=[3, 3, 3])
     atoms.center()
     atoms.pbc = True
     dft = DFT(atoms,
               mode='lcao',
-              parallel={'gpu': False},
               experimental={'new_basis': new})
     dft.converge()
+    e = dft.calculate_energy()
+    assert e == pytest.approx(-6.172722, abs=1e-6)
 
 
 if __name__ == '__main__':
